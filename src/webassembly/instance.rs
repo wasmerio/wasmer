4ab26226f9a5b71d270876881175492ce7919fde
Syrus Akbary <me@syrusakbary.com>
2018-10-22 18:56:29 +0200

    Revert "Improved function pointers"
    This reverts commit a625382158d72471b9da47d23c99ae3e62b33cf4.

--- src/webassembly/instance.rs
+++ src/webassembly/instance.rs
@@ -49,7 +49,7 @@ fn get_function_addr(
     func_index: &FuncIndex,
     import_functions: &Vec<*const u8>,
     functions: &Vec<Vec<u8>>,
-) -> *const () {
+) -> *const u8 {
     let index = func_index.index();
     let len = import_functions.len();
     let func_pointer = if index < len {
@@ -57,7 +57,7 @@ fn get_function_addr(
     } else {
         (&functions[func_index.index() - len]).as_ptr()
     };
-    func_pointer as _
+    func_pointer
 }
 
 // pub fn get_function_addr(
@@ -468,8 +468,7 @@ impl Instance {
     pub fn memories(&self) -> Arc<Vec<LinearMemory>> {
         self.memories.clone()
     }
-
-    pub fn get_function_pointer(&self, func_index: FuncIndex) -> *const () {
+    pub fn get_function_pointer(&self, func_index: FuncIndex) -> *const u8 {
         get_function_addr(&func_index, &self.import_functions, &self.functions)
     }
 
