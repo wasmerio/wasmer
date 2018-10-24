bcad000cc2d4f4a8557571f26c0bd01abb5e9841
Syrus Akbary <me@syrusakbary.com>
2018-10-24 11:44:33 +0200

    Improved utility functions docs

--- src/webassembly/utils.rs
+++ src/webassembly/utils.rs
@@ -1,3 +1,5 @@
+//! Utility functions for the webassembly library
+
 /// Detect if a provided binary is a WASM file
 pub fn is_wasm_binary(binary: &Vec<u8>) -> bool {
     binary.starts_with(&[b'\0', b'a', b's', b'm'])
