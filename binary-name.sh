e7f7c12485251af6d5990f734126d8f805936294
Syrus Akbary <me@syrusakbary.com>
2018-11-13 10:28:50 -0800

    Trying to autoplublish releases

---
+++ binary-name.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+
+initArch() {
+    ARCH=$(uname -m)
+    if [ -n "$WASMER_ARCH" ]; then
+        ARCH="$WASMER_ARCH"
+    fi
+    case $ARCH in
+        amd64) ARCH="amd64";;
+        x86_64) ARCH="amd64";;
+        i386) ARCH="386";;
+        *) echo "Architecture ${ARCH} is not supported by this installation script"; exit 1;;
+    esac
+}
+
+initOS() {
+    OS=$(uname | tr '[:upper:]' '[:lower:]')
+    if [ -n "$WASMER_OS" ]; then
+        echo "Using WASMER_OS"
+        OS="$WASMER_OS"
+    fi
+    case "$OS" in
+        darwin) OS='darwin';;
+        linux) OS='linux';;
+        freebsd) OS='freebsd';;
+        mingw*) OS='windows';;
+        msys*) OS='windows';;
+        *) echo "OS ${OS} is not supported by this installation script"; exit 1;;
+    esac
+}
+
+# identify platform based on uname output
+initArch
+initOS
+
+# determine install directory if required
+BINARY="wasmer-${OS}-${ARCH}"
+    
+# add .exe if on windows
+if [ "$OS" = "windows" ]; then
+    BINARY="$BINARY.exe"
+fi
+
+echo "${BINARY}"
