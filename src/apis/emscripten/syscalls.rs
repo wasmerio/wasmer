2486ac96a30ec64d0e4d39e24cec957ec6538cce
Steve Akinyemi <appcypher@outlook.com>
2018-11-22 18:00:05 +0100

    Add existing syscalls impl

--- src/apis/emscripten/syscalls.rs
+++ src/apis/emscripten/syscalls.rs
@@ -1,51 +1,77 @@
 /// NOTE: TODO: These syscalls only support wasm_32 for now because they take u32 offset
 /// Syscall list: https://www.cs.utexas.edu/~bismith/test/syscalls/syscalls32.html
-use libc::{c_int, c_void, ssize_t, write};
+use libc::{
+    c_int, c_void, utsname,
+    ssize_t, write, exit, read,
+    open, close, ioctl,
+    uname,
+};
 #[macro_use]
 use macros;
 use crate::webassembly::Instance;
 
+/// sys_exit
+pub extern "C" fn ___syscall1(_which: c_int, varargs: c_int, instance: &mut Instance) {
+    debug!("emscripten::___syscall1");
+    let status = vararg!(i32, instance, varargs);
+    unsafe { exit(status); }
+}
 
 /// sys_read
-pub extern "C" fn ___syscall3(_which: c_int, mut _varargs: c_int, _instance: &mut Instance) -> ssize_t {
+pub extern "C" fn ___syscall3(_which: c_int, varargs: c_int, instance: &mut Instance) -> ssize_t {
     debug!("emscripten::___syscall3");
-    0
+    let fd = vararg!(i32, instance, varargs);
+    let buf = vararg!(u32, instance, varargs);
+    let count = vararg!(usize, instance, varargs);
+    debug!("fd: {}, buf_offset: {}, count: {}", fd, buf, count);
+    let buf_addr = instance.memory_offset_addr(0, buf as usize) as *mut c_void;
+    unsafe { read(fd, buf_addr, count) }
 }
 
 /// sys_write
-pub extern "C" fn ___syscall4(_which: c_int, mut varargs: c_int, instance: &mut Instance) -> c_int {
+pub extern "C" fn ___syscall4(_which: c_int, varargs: c_int, instance: &mut Instance) -> c_int {
     debug!("emscripten::___syscall4");
-    let fd = vararg!(fd, i32, instance, varargs);
-    let buf_ptr = vararg!(buf_ptr, u32, instance, varargs);
-    let count = vararg!(count, u32, instance, varargs);
-    debug!("fd: {}, buf_ptr: {}, count: {}", fd, buf_ptr, count);
-    let buf = instance.memory_offset_addr(0, buf_ptr as usize) as *const c_void;
-    unsafe { write(fd, buf, count as usize) as i32 }
+    let fd = vararg!(i32, instance, varargs);
+    let buf = vararg!(u32, instance, varargs);
+    let count = vararg!(u32, instance, varargs);
+    debug!("fd: {}, buf: {}, count: {}", fd, buf, count);
+    let buf_addr = instance.memory_offset_addr(0, buf as usize) as *const c_void;
+    unsafe { write(fd, buf_addr, count as usize) as i32 }
 }
 
 /// sys_open
-pub extern "C" fn ___syscall5(_which: c_int, mut varargs: c_int, instance: &mut Instance) -> c_int {
+pub extern "C" fn ___syscall5(_which: c_int, varargs: c_int, instance: &mut Instance) -> c_int {
     debug!("emscripten::___syscall5");
-    let pathname = vararg!(pathname, u32, instance, varargs);
-    let flags = vararg!(flags, u32, instance, varargs);
-    let mode = vararg!(mode, u32, instance, varargs);
+    let pathname = vararg!(u32, instance, varargs);
+    let flags = vararg!(i32, instance, varargs);
+    let mode = vararg!(u32, instance, varargs);
     debug!("pathname: {}, flags: {}, mode: {}", pathname, flags, mode);
-    -2
+    let pathname_addr = instance.memory_offset_addr(0, pathname as usize) as *const i8;
+    unsafe { open(pathname_addr, flags, mode) }
+}
+
+/// sys_close
+pub extern "C" fn ___syscall6(_which: c_int, varargs: c_int, instance: &mut Instance) -> c_int {
+    debug!("emscripten::___syscall1");
+    let fd = vararg!(i32, instance, varargs);
+    debug!("fd: {}", fd);
+    unsafe { close(fd) }
 }
 
 // sys_ioctl
-pub extern "C" fn ___syscall54(_which: c_int, mut varargs: c_int, instance: &mut Instance) -> c_int {
+pub extern "C" fn ___syscall54(_which: c_int, varargs: c_int, instance: &mut Instance) -> c_int {
     debug!("emscripten::___syscall54");
-    let stream = vararg!(stream, u32, instance, varargs);
-    let op = vararg!(op, u32, instance, varargs);
-    debug!("stream: {}, op: {}", stream, op);
-    0
+    let fd = vararg!(i32, instance, varargs);
+    let request = vararg!(u64, instance, varargs);
+    debug!("fd: {}, op: {}", fd, request);
+    unsafe { ioctl(fd, request) }
 }
 
 // sys_newuname
-pub extern "C" fn ___syscall122(_which: c_int, mut varargs: c_int, instance: &mut Instance) -> c_int {
+pub extern "C" fn ___syscall122(_which: c_int, varargs: c_int, instance: &mut Instance) -> c_int {
     debug!("emscripten::___syscall122");
-    let buf = vararg!(buf, u32, instance, varargs);
+    let buf = vararg!(u32, instance, varargs);
     debug!("buf: {}", buf);
-    0
+    let buf_addr = instance.memory_offset_addr(0, buf as usize) as *mut utsname;
+    unsafe { uname(buf_addr) } // TODO: Fix implementation
 }
