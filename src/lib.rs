3815eaf13ae2fec8f3bc97abdc4c5218b955adc4
losfair <zhy20000919@hotmail.com>
2018-11-28 13:15:33 +0800

    Allow using wasmer as a library.

---
+++ src/lib.rs
@@ -0,0 +1,27 @@
+#[macro_use]
+extern crate error_chain;
+extern crate cranelift_codegen;
+extern crate cranelift_entity;
+extern crate cranelift_native;
+extern crate cranelift_wasm;
+extern crate libc;
+extern crate memmap;
+extern crate region;
+extern crate structopt;
+extern crate wabt;
+extern crate wasmparser;
+#[macro_use]
+extern crate target_lexicon;
+pub extern crate nix; // re-exported for usage in macros
+extern crate rayon;
+
+#[macro_use]
+mod macros;
+#[macro_use]
+pub mod recovery;
+pub mod apis;
+pub mod common;
+pub mod sighandler;
+#[cfg(test)]
+mod spectests;
+pub mod webassembly;
