1fbb88dcff7aaf16302a735d55bf5c9cd075ce7d
Lachlan Sneff <lachlan.sneff@gmail.com>
2018-12-15 13:25:29 -0500

    Fix formatting

--- src/common/mmap/windows.rs
+++ src/common/mmap/windows.rs
@@ -7,7 +7,7 @@ use std::ptr;
 use std::slice;
 use std::string::String;
 use winapi::um::memoryapi::{VirtualAlloc, VirtualFree};
-use winapi::um::winnt::{MEM_COMMIT, MEM_RESERVE, PAGE_NOACCESS, MEM_RELEASE};
+use winapi::um::winnt::{MEM_COMMIT, MEM_RELEASE, MEM_RESERVE, PAGE_NOACCESS};
 
 use super::common::round_up_to_page_size;
 
