db93d2693d4d866a7183addd01bf4f975a2e62c5
Syrus <me@syrusakbary.com>
2018-12-18 23:21:18 -0800

    Make setjmp/longjmp more secure

--- src/apis/emscripten/jmp.rs
+++ src/apis/emscripten/jmp.rs
@@ -1,12 +1,21 @@
 use crate::webassembly::Instance;
 use libc::{c_int, c_void};
+use std::cell::UnsafeCell;
 
 /// setjmp
 pub extern "C" fn __setjmp(env_addr: u32, instance: &mut Instance) -> c_int {
     debug!("emscripten::__setjmp (setjmp)");
     unsafe {
-        let env = instance.memory_offset_addr(0, env_addr as usize) as *mut c_void;
-        setjmp(env)
+        // Rather than using the env as the holder of the jump address,
+        // we obscure that id so we are in complete control of it
+        let obscure_env = instance.memory_offset_addr(0, env_addr as usize) as *mut i8;
+        let jmp_buf: UnsafeCell<[c_int; 27]> = UnsafeCell::new([0; 27]);
+        let mut jumps = &mut instance.emscripten_data.as_mut().unwrap().jumps;
+        let result = setjmp(jmp_buf.get() as _);
+        *obscure_env = jumps.len() as _;
+        jumps.push(jmp_buf);
+        // We use the index of the jump as the jump buffer (env)
+        result
     }
 }
 
@@ -14,8 +23,10 @@ pub extern "C" fn __setjmp(env_addr: u32, instance: &mut Instance) -> c_int {
 pub extern "C" fn __longjmp(env_addr: u32, val: c_int, instance: &mut Instance) -> ! {
     debug!("emscripten::__longjmp (longjmp) {}", val);
     unsafe {
-        let env = instance.memory_offset_addr(0, env_addr as usize) as *mut c_void;
-        longjmp(env, val)
+        let obscure_env = instance.memory_offset_addr(0, env_addr as usize) as *mut i8;
+        let mut jumps = &mut instance.emscripten_data.as_mut().unwrap().jumps;
+        let mut real_env = &jumps[*obscure_env as usize];
+        longjmp(real_env.get() as _, val)
     };
 }
 
