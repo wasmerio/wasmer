4a7560dcd32ada592b46eefdec0de5a0e5871f6f
Brandon Fish <brandon.j.fish@gmail.com>
2018-12-28 16:09:10 -0600

    Document Compiler trait

--- src/runtime/backend.rs
+++ src/runtime/backend.rs
@@ -2,5 +2,6 @@ use crate::runtime::module::Module;
 use std::sync::Arc;
 
 pub trait Compiler {
+    /// Compiles a `Module` from WebAssembly binary format
     fn compile(&self, wasm: &[u8]) -> Result<Arc<Module>, String>;
 }
