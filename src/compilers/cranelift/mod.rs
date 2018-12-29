8a19b042f8afdee919719c09bdc62e3b5f35b368
Steve Akinyemi <appcypher@outlook.com>
2018-12-29 19:40:25 +0100

    Add change in mod.rs

--- src/compilers/cranelift/mod.rs
+++ src/compilers/cranelift/mod.rs
@@ -4,13 +4,26 @@ use crate::runtime::{
     module::Module,
     backend::Compiler,
 };
+
 use std::sync::Arc;
+
+use self::codegen::{
+    CraneliftModule,
+    converter,
+};
 
 struct Cranelift {}
 
 impl Compiler for Cranelift {
     // Compiles towasm byte to module
     fn compile(&self, wasm: &[u8]) -> Result<Arc<Module>, String> {
-        Ok(Arc::new(codegen::CraneliftModuleTrait::from_bytes(wasm)?))
+        // Generate a Cranlift module from wasm binary
+        let cranelift_module = CraneliftModule::from_bytes(wasm)?;
+
+        // Convert Cranelift module to wasmer module
+        let wasmer_module = converter::generate_wasmer_module(cranelift_module);
+
+        // Return new wasmer module
+        Ok(Arc::new(wasmer_module))
     }
 }
