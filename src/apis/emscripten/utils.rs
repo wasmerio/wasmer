01a89dd3476c30b9ca7501a5b43487397d1b5e48
Brandon Fish <brandon.j.fish@gmail.com>
2018-12-29 17:01:04 -0600

    Fix is_emscripten_module references

--- src/apis/emscripten/utils.rs
+++ src/apis/emscripten/utils.rs
@@ -126,13 +126,13 @@ mod tests {
     fn should_detect_emscripten_files() {
         let wasm_bytes = include_wast2wasm_bytes!("tests/is_emscripten_true.wast");
         let module = compile(&wasm_bytes).expect("Not compiled properly");
-        assert!(is_emscripten_module(module));
+        assert!(is_emscripten_module(&module));
     }
 
     #[test]
     fn should_detect_non_emscripten_files() {
         let wasm_bytes = include_wast2wasm_bytes!("tests/is_emscripten_false.wast");
         let module = compile(&wasm_bytes).expect("Not compiled properly");
-        assert!(!is_emscripten_module(module));
+        assert!(!is_emscripten_module(&module));
     }
 }
