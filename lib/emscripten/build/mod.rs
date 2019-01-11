34869ff3c80aaac87bdae5b1bfb0d79154ad6d18
Syrus <me@syrusakbary.com>
2019-01-10 21:52:14 -0800

    Fixed emscripten emtests env var name

--- lib/emscripten/build/mod.rs
+++ lib/emscripten/build/mod.rs
@@ -2,10 +2,10 @@ use std::env;
 
 mod emtests;
 
-static SPECTESTS_ENV_VAR: &str = "WASM_EMSCRIPTEN_GENERATE_EMTESTS";
+static EMTESTS_ENV_VAR: &str = "WASM_EMSCRIPTEN_GENERATE_EMTESTS";
 
 fn main() {
-    if env::var(SPECTESTS_ENV_VAR).unwrap_or("0".to_string()) == "1" {
+    if env::var(EMTESTS_ENV_VAR).unwrap_or("0".to_string()) == "1" {
         emtests::build();
     }
 }
