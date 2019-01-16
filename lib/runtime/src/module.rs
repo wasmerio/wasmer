aa01fd50e95ddf1be9acd1ade6c7eb3b27189beb
Steve Akinyemi <appcypher@outlook.com>
2019-01-16 12:41:06 +0100

    Remove memory_offset_addr impl

--- lib/runtime/src/module.rs
+++ lib/runtime/src/module.rs
@@ -39,7 +39,7 @@ pub struct ModuleInner {
     pub sig_registry: SigRegistry,
 }
 
-pub struct Module(pub Rc<ModuleInner>);
+pub struct Module(Rc<ModuleInner>);
 
 impl Module {
     pub(crate) fn new(inner: Rc<ModuleInner>) -> Self {
