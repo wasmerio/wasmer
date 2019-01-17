71e50a0272bd7bf269e00d2be7806b94bf446f3e
Brandon Fish <brandon.j.fish@gmail.com>
2019-01-17 00:11:54 -0600

    Add data and data finalizer to Ctx

--- lib/runtime/src/vm.rs
+++ lib/runtime/src/vm.rs
@@ -1,6 +1,7 @@
 use crate::backing::ImportBacking;
 pub use crate::backing::LocalBacking;
 use std::{mem, ptr};
+use std::ffi::c_void;
 
 #[derive(Debug)]
 #[repr(C)]
@@ -28,12 +29,29 @@ pub struct Ctx {
 
     /// The parent instance.
     pub local_backing: *mut LocalBacking,
+
+    /// Host data
+    pub data: *mut c_void,
+
+    /// Host data finalizer
+    pub data_finalizer: extern "C" fn(data: *mut c_void),
+
+}
+
+extern "C" fn empty_finalizer(_data: *mut c_void){
+
+}
+
+impl Drop for Ctx {
+    fn drop(&mut self) {
+        (self.data_finalizer)(self.data);
+    }
 }
 
 impl Ctx {
     pub unsafe fn new(
         local_backing: &mut LocalBacking,
-        import_backing: &mut ImportBacking,
+        import_backing: &mut ImportBacking
     ) -> Self {
         Self {
             memories: local_backing.vm_memories.as_mut_ptr(),
@@ -46,6 +64,30 @@ impl Ctx {
             imported_funcs: import_backing.functions.as_mut_ptr(),
 
             local_backing,
+            data: ptr::null_mut(),
+            data_finalizer: empty_finalizer,
+        }
+    }
+
+    pub unsafe fn new_with_data(
+        local_backing: &mut LocalBacking,
+        import_backing: &mut ImportBacking,
+        data: *mut c_void,
+        data_finalizer: extern "C" fn(data: *mut c_void)
+    ) -> Self {
+        Self {
+            memories: local_backing.vm_memories.as_mut_ptr(),
+            tables: local_backing.vm_tables.as_mut_ptr(),
+            globals: local_backing.vm_globals.as_mut_ptr(),
+
+            imported_memories: import_backing.memories.as_mut_ptr(),
+            imported_tables: import_backing.tables.as_mut_ptr(),
+            imported_globals: import_backing.globals.as_mut_ptr(),
+            imported_funcs: import_backing.functions.as_mut_ptr(),
+
+            local_backing,
+            data,
+            data_finalizer,
         }
     }
 
@@ -422,4 +464,60 @@ mod vm_offset_tests {
             offset_of!(Anyfunc => sig_id).get_byte_offset(),
         );
     }
+}
+
+
+#[cfg(test)]
+mod vm_ctx_tests {
+    use super::{
+        Ctx, LocalBacking, ImportBacking,
+    };
+    use crate::structures::{Map};
+    use std::ffi::c_void;
+
+    struct TestData {
+        x: u32,
+        y: bool,
+        str: String,
+    }
+
+    extern "C" fn test_data_finalizer(data: *mut c_void){
+        let test_data: &mut TestData = unsafe { &mut *(data as *mut TestData) };
+        assert_eq!(test_data.x, 10);
+        assert_eq!(test_data.y, true);
+        assert_eq!(test_data.str, "Test".to_string());
+        println!("hello from finalizer");
+        drop(test_data);
+    }
+
+    #[test]
+    fn test_callback_on_drop(){
+        let mut data = TestData { x: 10, y: true, str: "Test".to_string() };
+        let mut local_backing = LocalBacking {
+            memories: Map::new().into_boxed_map(),
+            tables: Map::new().into_boxed_map(),
+            vm_memories: Map::new().into_boxed_map(),
+            vm_tables: Map::new().into_boxed_map(),
+            vm_globals:Map::new().into_boxed_map()
+        };
+        let mut import_backing = ImportBacking {
+            functions: Map::new().into_boxed_map(),
+            memories: Map::new().into_boxed_map(),
+            tables: Map::new().into_boxed_map(),
+            globals: Map::new().into_boxed_map()
+        };
+        let data = &mut data as *mut _ as *mut c_void;
+        let ctx = unsafe { Ctx::new_with_data(&mut local_backing, &mut import_backing, data, test_data_finalizer) };
+        let ctx_test_data = cast_test_data(ctx.data);
+        assert_eq!(ctx_test_data.x, 10);
+        assert_eq!(ctx_test_data.y, true);
+        assert_eq!(ctx_test_data.str, "Test".to_string());
+        drop(ctx);
+    }
+
+    fn cast_test_data(data: *mut c_void) -> &'static mut TestData {
+        let test_data: &mut TestData = unsafe { &mut *(data as *mut TestData) };
+        test_data
+    }
+
 }
