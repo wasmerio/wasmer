5daefa496dd64a40394a1acf8d78fa129c0211e8
Brandon Fish <brandon.j.fish@gmail.com>
2019-01-21 23:48:05 -0600

    Make default compiler dependency optional

--- lib/runtime/src/lib.rs
+++ lib/runtime/src/lib.rs
@@ -1,10 +1,17 @@
+#[cfg(feature = "wasmer-clif-backend")]
 extern crate wasmer_clif_backend;
+
 extern crate wasmer_runtime_core;
 
 pub use wasmer_runtime_core::*;
 
+#[cfg(feature = "wasmer-clif-backend")]
 use wasmer_clif_backend::CraneliftCompiler;
 
+/// Compiles a WebAssembly module
 pub fn compile(wasm: &[u8]) -> error::CompileResult<module::Module> {
+    #[cfg(not(feature = "wasmer-clif-backend"))]
+    panic!("compile/1 is not available when default compiler is disabled");
+    #[cfg(feature = "wasmer-clif-backend")]
     wasmer_runtime_core::compile(&wasm[..], &CraneliftCompiler::new())
 }
