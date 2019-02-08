9a56680281069e8fdf1c3e615029105131cad82f
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-02-08 13:51:29 -0800

    fix windows linking and appveyor build (#166)

--- lib/clif-backend/src/signal/windows.rs
+++ lib/clif-backend/src/signal/windows.rs
@@ -4,3 +4,7 @@ use wasmer_runtime_core::error::RuntimeResult;
 pub fn call_protected<T>(handler_data: &HandlerData, f: impl FnOnce() -> T) -> RuntimeResult<T> {
     unimplemented!("TODO");
 }
+
+pub unsafe fn trigger_trap() -> ! {
+    unimplemented!("TODO");
+}
