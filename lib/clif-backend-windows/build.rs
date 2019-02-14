b87bf6ccf1af41fc008f3bfd77e8129c4fee6b29
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-02-13 18:36:39 -0800

    none of that nonsense

--- lib/clif-backend-windows/build.rs
+++ lib/clif-backend-windows/build.rs
@@ -1,7 +1,8 @@
 use cmake::Config;
 
 fn main() {
-    #[cfg(target_os = "windows")] {
+    #[cfg(target_os = "windows")]
+    {
         let project_name = "exception_handling";
         let dst = Config::new(project_name).build();
         println!("cargo:rustc-link-search=native={}", dst.display());
