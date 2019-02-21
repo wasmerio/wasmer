c78d4a8674d9fb147ba12f0a48e1f1b35598098c
Lachlan Sneff <lachlan.sneff@gmail.com>
2019-02-21 12:54:53 -0800

    Fix test compilation 2

--- lib/runtime-core/src/vm.rs
+++ lib/runtime-core/src/vm.rs
@@ -497,7 +497,7 @@ mod vm_ctx_tests {
         use crate::backend::{
             sys::Memory, Backend, CacheGen, FuncResolver, ProtectedCaller, Token, UserTrapper,
         };
-        use crate::cache::{Error as CacheError, WashHash};
+        use crate::cache::{Error as CacheError, WasmHash};
         use crate::error::RuntimeResult;
         use crate::types::{FuncIndex, LocalFuncIndex, Value};
         use hashbrown::HashMap;
