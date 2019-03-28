e3a6b7c9d8ca9ded52b45a531bc098b415e762b2
Lachlan Sneff <lachlan.sneff@gmail.com>
2019-03-28 11:44:31 -0700

    Add skeleton for wasi abi

--- lib/wasi/src/lib.rs
+++ lib/wasi/src/lib.rs
@@ -1,1 +1,18 @@
 
+use wasmer_runtime_core::{
+    import::ImportObject,
+    imports,
+    func,
+};
+
+pub fn generate_import_object() -> ImportObject {
+    imports! {
+        // This generates the wasi state.
+        || {
+            // returns (pointer to state, function that can destruct the state).
+        },
+        "wasi_unstable" => {
+            
+        },
+    }
+}
