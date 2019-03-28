b7254ce1f5a8c3e331c50aed15e6f7c08b590833
Mark McCaskey <mark@wasmer.io>
2019-03-28 11:54:22 -0700

    add stubs and dispatch

---
+++ lib/wasi/src/syscalls/mod.rs
@@ -0,0 +1,137 @@
+use wasmer_runtime_core::vm::Ctx;
+
+pub fn __wasi_args_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_args_sizes_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_clock_res_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_clock_time_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_environ_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_environ_sizes_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_advise(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_allocate(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_close(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_datasync(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_fdstat_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_fdstat_set_flags(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_fdstat_set_rights(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_filestat_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_filestat_set_size(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_filestat_set_times(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_pread(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_prestat_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_prestat_dir_name(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_pwrite(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_read(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_readdir(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_renumber(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_seek(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_sync(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_tell(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_fd_write(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_create_directory(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_filestat_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_filestat_set_times(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_link(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_open(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_readlink(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_remove_directory(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_rename(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_symlink(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_path_unlink_file(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_poll_oneoff(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_proc_exit(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_proc_raise(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_random_get(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_sched_yield(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_sock_recv(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_sock_send(ctx: &mut Ctx) {
+    unimplemented!()
+}
+pub fn __wasi_sock_shutdown(ctx: &mut Ctx) {
+    unimplemented!()
+}
