2b1052d983dc82c92b02c1b6dd2753e64ab3ee7a
Mackenzie Clark <mackenzie.a.z.c@gmail.com>
2019-04-03 00:18:22 -0700

    fix warnings and lint

--- lib/emscripten/src/invoke.rs
+++ lib/emscripten/src/invoke.rs
@@ -1,7 +1,5 @@
-use wasmer_runtime_core::{Module, compile_with_config};
-use wasmer_runtime_core::import::{ImportObject, Namespace};
-use wasmer_runtime_core::export::Export;
 use wasmer_runtime_core::backend::CompilerConfig;
+use wasmer_runtime_core::{compile_with_config, Module};
 
 struct Sig {
     pub args: Vec<InvokeArgType>,
@@ -18,7 +16,8 @@ pub fn create_invoke_module(invoke_function_names: &Vec<String>) -> Module {
 }
 
 fn create_wat(invoke_function_names: &Vec<String>) -> String {
-    let types_and_funcs_wat = invoke_function_names.iter()
+    let types_and_funcs_wat = invoke_function_names
+        .iter()
         .map(|s| &**s) // &String -> &str
         .map(invoke_name_to_signature)
         .filter_map(|r| r.ok())
@@ -29,7 +28,11 @@ fn create_wat(invoke_function_names: &Vec<String>) -> String {
         })
         .collect::<Vec<_>>()
         .join("\n");
-    format!("(module (table {} anyfunc)\n {})", invoke_function_names.len(), types_and_funcs_wat)
+    format!(
+        "(module (table {} anyfunc)\n {})",
+        invoke_function_names.len(),
+        types_and_funcs_wat
+    )
 }
 
 fn invoke_name_to_signature(invoke_name: &str) -> Result<(&str, Sig), InvokeError> {
@@ -40,12 +43,10 @@ fn invoke_name_to_signature(invoke_name: &str) -> Result<(&str, Sig), InvokeErro
     let mut chars = chars.skip(7);
     // 'if next is a 'v' then this does not return
     match chars.next() {
-        Some('v') => {},
-        Some(c) => {
-            match InvokeArgType::from_char(c) {
-                Ok(i) => rets.push(i),
-                Err(_) => return Err(InvokeError::InvalidInvokeFunctionParameterCharacter),
-            }
+        Some('v') => {}
+        Some(c) => match InvokeArgType::from_char(c) {
+            Ok(i) => rets.push(i),
+            Err(_) => return Err(InvokeError::InvalidInvokeFunctionParameterCharacter),
         },
         None => {}
     };
@@ -57,30 +58,31 @@ fn invoke_name_to_signature(invoke_name: &str) -> Result<(&str, Sig), InvokeErro
         }
     }
 
-    let sig = Sig {
-        args,
-        rets,
-    };
+    let sig = Sig { args, rets };
     Ok((invoke_name, sig))
 }
 
 fn sig_to_type_wat(func_name: &str, sig: &Sig) -> String {
-    let x= sig.args.iter()
-        .map(|i| i.to_type_str())
-        .collect::<Vec<_>>();
+    let x = sig.args.iter().map(|i| i.to_type_str()).collect::<Vec<_>>();
     let params_string = match &x[..] {
         [] => "".to_string(),
-        args => format!(" (param {})", x.join(" ")),
+        _args => format!(" (param {})", x.join(" ")),
     };
     let result_string = match sig.rets.get(0) {
         Some(ret) => format!(" (result {})", ret.to_type_str()),
         None => "".to_string(),
     };
-    format!("(type $emscripten_{} (func{}{}))", func_name, params_string, result_string)
+    format!(
+        "(type $emscripten_{} (func{}{}))",
+        func_name, params_string, result_string
+    )
 }
 
 fn sig_to_func_wat(func_name: &str, sig: &Sig) -> String {
-    let x = sig.args.iter().zip(1..)
+    let x = sig
+        .args
+        .iter()
+        .zip(1..)
         .map(|(a, i)| format!("(param $p{} {})", i, a.to_type_str()))
         .collect::<Vec<_>>();
     let params_string = match &x[..] {
@@ -91,10 +93,13 @@ fn sig_to_func_wat(func_name: &str, sig: &Sig) -> String {
         Some(ret) => format!(" (result {})", ret.to_type_str()),
         None => "".to_string(),
     };
-    let get_locals_vec = sig.args.iter().zip((1..))
+    let get_locals_vec = sig
+        .args
+        .iter()
+        .zip(1..)
         .map(|(_, i)| format!("get_local $p{}", i))
         .collect::<Vec<_>>();
-//    get_locals_vec.reverse();
+    //    get_locals_vec.reverse();
     let mut get_locals_string = get_locals_vec.join("\n");
     if get_locals_string.len() > 0 {
         get_locals_string = get_locals_string + "\n";
@@ -115,7 +120,7 @@ enum InvokeArgType {
 }
 
 impl InvokeArgType {
-    pub fn from_char(c :char) -> Result<InvokeArgType, InvokeError> {
+    pub fn from_char(c: char) -> Result<InvokeArgType, InvokeError> {
         match c {
             'd' => Ok(InvokeArgType::D),
             'i' => Ok(InvokeArgType::I),
@@ -135,15 +140,21 @@ impl InvokeArgType {
 
 #[cfg(test)]
 mod test {
-    use crate::invoke::{invoke_name_to_signature, InvokeArgType, sig_to_type_wat, Sig, sig_to_func_wat, create_invoke_module};
+    use crate::invoke::{
+        create_invoke_module, invoke_name_to_signature, sig_to_func_wat, sig_to_type_wat,
+        InvokeArgType, Sig,
+    };
+    use crate::{generate_emscripten_env, EmscriptenGlobals};
     use wasmer_runtime_core::import::{ImportObject, LikeNamespace};
     use wasmer_runtime_core::Instance;
-    use crate::{EmscriptenGlobals, generate_emscripten_env};
 
     #[test]
     fn create_type_wat_for_invoke_ii() {
         let expected_wat = "(type $emscripten_invoke_ii (func (param i32) (result i32)))";
-        let sig = Sig { args: vec![InvokeArgType::I], rets: vec![InvokeArgType::I] };
+        let sig = Sig {
+            args: vec![InvokeArgType::I],
+            rets: vec![InvokeArgType::I],
+        };
         let func_name = "invoke_ii";
         let actual_wat = sig_to_type_wat(func_name, &sig);
         assert_eq!(expected_wat, actual_wat);
@@ -151,7 +162,10 @@ mod test {
     #[test]
     fn create_type_wat_for_invoke_i() {
         let expected_wat = "(type $emscripten_invoke_i (func (result i32)))";
-        let sig = Sig { args: vec![], rets: vec![InvokeArgType::I] };
+        let sig = Sig {
+            args: vec![],
+            rets: vec![InvokeArgType::I],
+        };
         let func_name = "invoke_i";
         let actual_wat = sig_to_type_wat(func_name, &sig);
         assert_eq!(expected_wat, actual_wat);
@@ -159,7 +173,10 @@ mod test {
     #[test]
     fn create_type_wat_for_invoke_vi() {
         let expected_wat = "(type $emscripten_invoke_vi (func (param i32)))";
-        let sig = Sig { args: vec![InvokeArgType::I], rets: vec![] };
+        let sig = Sig {
+            args: vec![InvokeArgType::I],
+            rets: vec![],
+        };
         let func_name = "invoke_vi";
         let actual_wat = sig_to_type_wat(func_name, &sig);
         assert_eq!(expected_wat, actual_wat);
@@ -167,11 +184,15 @@ mod test {
 
     #[test]
     fn create_func_wat_for_invoke_ii() {
-        let expected_wat = r#"(func (export "invoke_ii") (param $p0 i32) (param $p1 i32) (result i32)
+        let expected_wat =
+            r#"(func (export "invoke_ii") (param $p0 i32) (param $p1 i32) (result i32)
 get_local $p1
 get_local $p0
 call_indirect (type $emscripten_invoke_ii))"#;
-        let sig = Sig { args: vec![InvokeArgType::I], rets: vec![InvokeArgType::I] };
+        let sig = Sig {
+            args: vec![InvokeArgType::I],
+            rets: vec![InvokeArgType::I],
+        };
         let func_name = "invoke_ii";
         let actual_wat = sig_to_func_wat(func_name, &sig);
         assert_eq!(expected_wat, actual_wat);
@@ -185,7 +206,10 @@ get_local $p2
 get_local $p3
 get_local $p0
 call_indirect (type $emscripten_invoke_iiii))"#;
-        let sig = Sig { args: vec![InvokeArgType::I, InvokeArgType::I, InvokeArgType::I], rets: vec![InvokeArgType::I] };
+        let sig = Sig {
+            args: vec![InvokeArgType::I, InvokeArgType::I, InvokeArgType::I],
+            rets: vec![InvokeArgType::I],
+        };
         let func_name = "invoke_iiii";
         let actual_wat = sig_to_func_wat(func_name, &sig);
         assert_eq!(expected_wat, actual_wat);
