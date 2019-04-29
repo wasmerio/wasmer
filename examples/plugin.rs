4f4473c84eacf7182fbf7650a6ce54d3e89e4514
Mark McCaskey <mark@wasmer.io>
2019-04-29 11:27:19 -0700

    update from feedback

--- examples/plugin.rs
+++ examples/plugin.rs
@@ -2,7 +2,7 @@ use wasmer_runtime::{func, imports, instantiate};
 use wasmer_runtime_core::vm::Ctx;
 use wasmer_wasi::generate_import_object;
 
-static PLUGIN_WASM: &'static [u8] = include_bytes!("plugin-for-example.wasm");
+static PLUGIN_LOCATION: &'static str = "examples/plugin-for-example.wasm";
 
 fn it_works(_ctx: &mut Ctx) -> i32 {
     println!("Hello from outside WASI");
@@ -10,6 +10,11 @@ fn it_works(_ctx: &mut Ctx) -> i32 {
 }
 
 fn main() {
+    let wasm_bytes = std::fs::read(PLUGIN_LOCATION).expect(&format!(
+        "Could not read in WASM plugin at {}",
+        PLUGIN_LOCATION
+    ));
+
     // WASI imports
     let mut base_imports = generate_import_object(vec![], vec![], vec![]);
     // env is the default namespace for extern functions
@@ -20,7 +25,7 @@ fn main() {
     };
     base_imports.extend(custom_imports);
     let instance =
-        instantiate(PLUGIN_WASM, &base_imports).expect("failed to instantiate wasm module");
+        instantiate(&wasm_bytes[..], &base_imports).expect("failed to instantiate wasm module");
 
     let entry_point = instance.func::<(i32), i32>("plugin_entrypoint").unwrap();
     let result = entry_point.call(2).expect("failed to execute plugin");
