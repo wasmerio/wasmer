8e0d71b2bcad8ad5301d4ad9cdb9ddbcfb066d11
losfair <zhy20000919@hotmail.com>
2019-05-17 01:32:41 +0800

    Fix missing `internals` in test.

--- lib/runtime-core/src/vm.rs
+++ lib/runtime-core/src/vm.rs
@@ -699,6 +699,8 @@ mod vm_ctx_tests {
 
             dynamic_sigindices: Map::new().into_boxed_map(),
             local_functions: Map::new().into_boxed_map(),
+
+            internals: crate::backing::Internals([0; 256]),
         };
         let mut import_backing = ImportBacking {
             memories: Map::new().into_boxed_map(),
