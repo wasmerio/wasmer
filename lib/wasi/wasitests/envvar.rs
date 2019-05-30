6a9515d13ddacaa579f15b2eca94ac651e3bcf02
Mark McCaskey <mark@wasmer.io>
2019-05-30 12:25:14 -0700

    improve wasi envvar test

--- lib/wasi/wasitests/envvar.rs
+++ lib/wasi/wasitests/envvar.rs
@@ -4,6 +4,17 @@
 
 use std::env;
 
+fn get_env_var(var_name: &str) -> Result<String, env::VarError> {
+    #[cfg(not(target = "wasi"))]
+    match var_name {
+        "DOG" => Ok("1".to_string()),
+        "CAT" => Ok("2".to_string()),
+        _ => Err(env::VarError::NotPresent),
+    }
+    #[cfg(target = "wasi")]
+    env::var(var_name)
+}
+
 fn main() {
     #[cfg(not(target = "wasi"))]
     let mut env_vars = vec!["DOG=1".to_string(), "CAT=2".to_string()];
@@ -18,4 +29,10 @@ fn main() {
     for e in env_vars {
         println!("{}", e);
     }
+
+    env::set_var("WASI_ENVVAR_TEST", "HELLO");
+
+    println!("DOG {:?}", get_env_var("DOG"));
+    println!("DOG_TYPE {:?}", get_env_var("DOG_TYPE"));
+    println!("SET VAR {:?}", env::var("WASI_ENVVAR_TEST"));
 }
