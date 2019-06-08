61786a370a78fbea3125538615d08459df5e704e
losfair <zhy20000919@hotmail.com>
2019-06-08 22:56:13 +0800

    Fix typo in `machine`.

--- lib/singlepass-backend/src/machine.rs
+++ lib/singlepass-backend/src/machine.rs
@@ -427,11 +427,11 @@ mod test {
 
     #[test]
     fn test_release_locations_keep_state_nopanic() {
-        let mut mechine = Machine::new();
+        let mut machine = Machine::new();
         let mut assembler = Assembler::new().unwrap();
-        let locs = mechine.acquire_locations(&mut assembler, &[WpType::I32; 10], false);
+        let locs = machine.acquire_locations(&mut assembler, &[WpType::I32; 10], false);
 
-        mechine.release_locations_keep_state(&mut assembler, &locs);
+        machine.release_locations_keep_state(&mut assembler, &locs);
     }
 
 }
