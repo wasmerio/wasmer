d4634dfdb92bf2c762ace746c89a5a43665d99fe
Syrus <me@syrusakbary.com>
2019-06-25 01:40:15 -0700

    Removed unused llvm file

--- lib/emscripten/src/llvm.rs
+++
@@ -1,9 +0,0 @@
-extern crate libm;
-extern crate wasmer_runtime_core;
-
-use libm::fma;
-use wasmer_runtime_core::vm::Ctx;
-
-pub fn fma_f64(_ctx: &mut Ctx, x: f64, y: f64, z: f64) -> f64 {
-    fma(x, y, z)
-}
