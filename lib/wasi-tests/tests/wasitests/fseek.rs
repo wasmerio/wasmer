53d7ecba2caa84b707b3bf608267fa8bec179a5d
Mark McCaskey <mark@wasmer.io>
2019-07-15 10:36:12 -0700

    improve wasitest infra and fix fseek test

--- lib/wasi-tests/tests/wasitests/fseek.rs
+++ lib/wasi-tests/tests/wasitests/fseek.rs
@@ -3,10 +3,7 @@ fn test_fseek() {
     assert_wasi_output!(
         "../../wasitests/fseek.wasm",
         "fseek",
-        vec![(
-            ".".to_string(),
-            ::std::path::PathBuf::from("wasitests/test_fs/hamlet")
-        ),],
+        vec![(".".to_string(), ::std::path::PathBuf::from("wasitests/test_fs/hamlet")),],
         vec![],
         "../../wasitests/fseek.out"
     );
