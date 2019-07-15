affb9465d5faa3a8bde162596b5bdf12005903e1
Mark McCaskey <mark@wasmer.io>
2019-07-15 10:43:30 -0700

    update rustfmt.toml to ignore and fmt

--- lib/wasi-tests/tests/wasitests/fseek.rs
+++ lib/wasi-tests/tests/wasitests/fseek.rs
@@ -3,7 +3,10 @@ fn test_fseek() {
     assert_wasi_output!(
         "../../wasitests/fseek.wasm",
         "fseek",
-        vec![(".".to_string(), ::std::path::PathBuf::from("wasitests/test_fs/hamlet")),],
+        vec![(
+            ".".to_string(),
+            ::std::path::PathBuf::from("wasitests/test_fs/hamlet")
+        ),],
         vec![],
         "../../wasitests/fseek.out"
     );
