9471f643fd071f569607f42cddb26c0af0730a33
losfair <zhy20000919@hotmail.com>
2019-08-15 19:03:39 -0700

    Update iterative_hash example.

--- examples/iterative_hash/src/main.rs
+++ examples/iterative_hash/src/main.rs
@@ -1,7 +1,12 @@
 use blake2::{Blake2b, Digest};
+use std::time::{Duration, SystemTime};
 
 fn main() {
     let mut data: Vec<u8> = b"test".to_vec();
+    let now = SystemTime::now();
+
+    let mut last_millis: u128 = 0;
+    let mut round_count: usize = 0;
 
     for i in 0.. {
         let mut hasher = Blake2b::new();
@@ -9,8 +14,11 @@ fn main() {
         let out = hasher.result();
         data = out.to_vec();
 
-        if i % 1000000 == 0 {
-            println!("Round {}: {:?}", i, data);
+        if i % 100000 == 0 {
+            let millis = now.elapsed().unwrap().as_millis();
+            println!("{} rounds in last second", (i - round_count) as f64 / (millis - last_millis) as f64);
+            last_millis = millis;
+            round_count = i;
         }
     }
 }
