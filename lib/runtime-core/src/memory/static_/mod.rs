0eba1532322d460a44b8582c1a528b7cb1401353
Nick Lewycky <nick@wasmer.io>
2019-08-19 15:18:22 -0700

    Implement shared static memory similarly to unshared static memory.

--- lib/runtime-core/src/memory/static_/mod.rs
+++ lib/runtime-core/src/memory/static_/mod.rs
@@ -3,8 +3,6 @@ pub const SAFE_STATIC_HEAP_SIZE: usize = 1 << 32; // 4 GiB
 #[doc(hidden)]
 pub const SAFE_STATIC_GUARD_SIZE: usize = 1 << 31; // 2 GiB
 
-mod shared;
 mod unshared;
 
-pub use self::shared::SharedStaticMemory;
 pub use self::unshared::StaticMemory;
