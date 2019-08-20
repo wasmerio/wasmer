70d561a1ddf0fcc21490a439b61f976a86af9a6a
Nick Lewycky <nick@wasmer.io>
2019-08-20 11:27:43 -0700

    Remove SharedStaticMemory and simplify surrounding code.

--- lib/runtime-core/src/memory/static_/mod.rs
+++
@@ -1,8 +0,0 @@
-#[doc(hidden)]
-pub const SAFE_STATIC_HEAP_SIZE: usize = 1 << 32; // 4 GiB
-#[doc(hidden)]
-pub const SAFE_STATIC_GUARD_SIZE: usize = 1 << 31; // 2 GiB
-
-mod unshared;
-
-pub use self::unshared::StaticMemory;
