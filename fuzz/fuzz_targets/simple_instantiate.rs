4ce2e2c56d687f9a071ecc735ec8dc12fc3fc8d9
Nick Lewycky <nick@wasmer.io>
2019-09-03 15:20:36 -0700

    cargo fmt + adjust comment.

--- fuzz/fuzz_targets/simple_instantiate.rs
+++ fuzz/fuzz_targets/simple_instantiate.rs
@@ -1,11 +1,9 @@
 #![no_main]
-#[macro_use] extern crate libfuzzer_sys;
+#[macro_use]
+extern crate libfuzzer_sys;
 extern crate wasmer_runtime;
 
-use wasmer_runtime::{
-    instantiate,
-    imports,
-};
+use wasmer_runtime::{imports, instantiate};
 
 fuzz_target!(|data: &[u8]| {
     let import_object = imports! {};
