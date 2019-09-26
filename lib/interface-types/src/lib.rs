87f61ab226618fbc7f0af38deb892cf0c7781a0d
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-09-26 15:02:20 +0200

    feat(interface-types) Add attributes.

--- lib/interface-types/src/lib.rs
+++ lib/interface-types/src/lib.rs
@@ -1,3 +1,15 @@
+#![deny(
+    dead_code,
+    nonstandard_style,
+    unused_imports,
+    unused_mut,
+    unused_variables,
+    unused_unsafe,
+    unreachable_patterns
+)]
+#![doc(html_favicon_url = "https://wasmer.io/static/icons/favicon.ico")]
+#![doc(html_logo_url = "https://github.com/wasmerio.png")]
+
 pub mod ast;
 #[macro_use]
 mod macros;
