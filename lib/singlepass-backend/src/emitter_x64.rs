8e63d54fdb962b25ea4153f5b893c7ffd66eddcd
Nick Lewycky <nick@wasmer.io>
2019-09-28 22:08:10 -0700

    cargo fmt

--- lib/singlepass-backend/src/emitter_x64.rs
+++ lib/singlepass-backend/src/emitter_x64.rs
@@ -800,7 +800,10 @@ impl Emitter for Assembler {
             (Size::S16, Location::Memory(src, disp), Size::S64, Location::GPR(dst)) => {
                 dynasm!(self ; movzx Rq(dst as u8), WORD [Rq(src as u8) + disp]);
             }
-            _ => panic!("singlepass can't emit MOVZX {:?} {:?} {:?} {:?}", sz_src, src, sz_dst, dst),
+            _ => panic!(
+                "singlepass can't emit MOVZX {:?} {:?} {:?} {:?}",
+                sz_src, src, sz_dst, dst
+            ),
         }
     }
     fn emit_movsx(&mut self, sz_src: Size, src: Location, sz_dst: Size, dst: Location) {
@@ -835,7 +838,10 @@ impl Emitter for Assembler {
             (Size::S32, Location::Memory(src, disp), Size::S64, Location::GPR(dst)) => {
                 dynasm!(self ; movsx Rq(dst as u8), DWORD [Rq(src as u8) + disp]);
             }
-            _ => panic!("singlepass can't emit MOVSX {:?} {:?} {:?} {:?}", sz_src, src, sz_dst, dst),
+            _ => panic!(
+                "singlepass can't emit MOVSX {:?} {:?} {:?} {:?}",
+                sz_src, src, sz_dst, dst
+            ),
         }
     }
 
@@ -895,7 +901,10 @@ impl Emitter for Assembler {
             (Size::S64, Location::GPR(src), Location::Memory(dst, disp)) => {
                 dynasm!(self ; lock xadd [Rq(dst as u8) + disp], Rq(src as u8));
             }
-            _ => panic!("singlepass can't emit LOCK XADD {:?} {:?} {:?}", sz, src, dst),
+            _ => panic!(
+                "singlepass can't emit LOCK XADD {:?} {:?} {:?}",
+                sz, src, dst
+            ),
         }
     }
 
@@ -913,7 +922,10 @@ impl Emitter for Assembler {
             (Size::S64, Location::GPR(src), Location::Memory(dst, disp)) => {
                 dynasm!(self ; lock cmpxchg [Rq(dst as u8) + disp], Rq(src as u8));
             }
-            _ => panic!("singlepass can't emit LOCK CMPXCHG {:?} {:?} {:?}", sz, src, dst),
+            _ => panic!(
+                "singlepass can't emit LOCK CMPXCHG {:?} {:?} {:?}",
+                sz, src, dst
+            ),
         }
     }
 
