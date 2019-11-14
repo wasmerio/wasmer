987b0765c0ea4c675af6f32203f27e2e13f1e5f1
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2019-11-14 14:37:55 +0100

    chore(runtime-c-api) Update header files.

--- lib/runtime-c-api/wasmer.hh
+++ lib/runtime-c-api/wasmer.hh
@@ -1,18 +1,20 @@
 
 #if !defined(WASMER_H_MACROS)
-  #define WASMER_H_MACROS
-  #if defined(MSVC)
-    #if defined(_M_AMD64)
-      #define ARCH_X86_64
-    #endif
-  #endif
+#define WASMER_H_MACROS
 
-  #if defined(GCC) || defined(__clang__)
-    #if defined(__x86_64__)
-      #define ARCH_X86_64
-    #endif
-  #endif
+#if defined(MSVC)
+#if defined(_M_AMD64)
+#define ARCH_X86_64
 #endif
+#endif
+
+#if defined(GCC) || defined(__clang__)
+#if defined(__x86_64__)
+#define ARCH_X86_64
+#endif
+#endif
+
+#endif // WASMER_H_MACROS
 
 
 #ifndef WASMER_H
