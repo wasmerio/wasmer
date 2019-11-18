23f03f555b321aed1d09542501063996ab0d9c83
losfair <zhy20000919@hotmail.com>
2019-11-18 23:10:49 +0800

    Fix compilation failure in tiering.rs.

--- lib/runtime-core/src/tiering.rs
+++ lib/runtime-core/src/tiering.rs
@@ -1,6 +1,6 @@
 //! The tiering module supports switching between code compiled with different optimization levels
 //! as runtime.
-use crate::backend::{Compiler, CompilerConfig};
+use crate::backend::{Backend, Compiler, CompilerConfig};
 use crate::compile_with_config;
 use crate::fault::{
     catch_unsafe_unwind, ensure_sighandler, pop_code_version, push_code_version, with_ctx,
