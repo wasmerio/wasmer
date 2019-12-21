ad82beffb4ee4a1d8d53899b8935e26b667e8d69
Syrus <me@syrusakbary.com>
2019-12-20 21:11:55 -0800

    Fixed tests

--- lib/runtime-core/src/vm.rs
+++ lib/runtime-core/src/vm.rs
@@ -975,6 +975,7 @@ mod vm_ctx_tests {
     use crate::module::{ModuleInfo, ModuleInner, StringTable};
     use crate::structures::Map;
     use std::ffi::c_void;
+    use std::sync::Arc;
 
     struct TestData {
         x: u32,
