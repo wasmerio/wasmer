469e43312ada7bbd1c08a5df9aeb9fefadcfa3a4
Mark McCaskey <mark@wasmer.io>
2020-03-03 16:16:29 -0800

    Improve `WasmPtr` documentation

--- lib/runtime-core/src/memory/ptr.rs
+++ lib/runtime-core/src/memory/ptr.rs
@@ -1,4 +1,4 @@
-//! A reusable pointer abstraction for getting memory from the guest's memory.
+//! Types for a reusable pointer abstraction for accessing Wasm linear memory.
 //!
 //! This abstraction is safe: it ensures the memory is in bounds and that the pointer
 //! is aligned (avoiding undefined behavior).
@@ -12,18 +12,36 @@ use crate::{
 };
 use std::{cell::Cell, fmt, marker::PhantomData, mem};
 
-/// Array.
+/// The `Array` marker type. This type can be used like `WasmPtr<T, Array>`
+/// to get access to methods
 pub struct Array;
-/// Item.
+/// The `Item` marker type. This is the default and does not usually need to be
+/// specified.
 pub struct Item;
 
-/// A pointer to a Wasm item.
+/// A zero-cost type that represents a pointer to something in Wasm linear
+/// memory.
+///
+/// This type can be used directly in the host function arguments:
+/// ```
+/// # use wasmer_runtime_core::vm::Ctx;
+/// # use wasmer_runtime_core::memory::ptr::WasmPtr;
+/// pub fn host_import(ctx: &mut Ctx, ptr: WasmPtr<u32>) {
+///     let memory = ctx.memory(0);
+///     let derefed_ptr = ptr.deref(memory).expect("pointer in bounds");
+///     let inner_val: u32 = derefed_ptr.get();
+///     println!("Got {} from Wasm memory address 0x{:X}", inner_val, ptr.offset());
+///     // update the value being pointed to
+///     derefed_ptr.set(inner_val + 1);
+/// }
+/// ```
 #[repr(transparent)]
 pub struct WasmPtr<T: Copy, Ty = Item> {
     offset: u32,
     _phantom: PhantomData<(T, Ty)>,
 }
 
+/// Methods relevant to all types of `WasmPtr`.
 impl<T: Copy, Ty> WasmPtr<T, Ty> {
     /// Create a new `WasmPtr` at the given offset.
     #[inline]
@@ -34,7 +52,7 @@ impl<T: Copy, Ty> WasmPtr<T, Ty> {
         }
     }
 
-    /// Get the offset for this `WasmPtr`.
+    /// Get the offset into Wasm linear memory for this `WasmPtr`.
     #[inline]
     pub fn offset(self) -> u32 {
         self.offset
@@ -48,8 +66,16 @@ fn align_pointer(ptr: usize, align: usize) -> usize {
     ptr & !(align - 1)
 }
 
+/// Methods for `WasmPtr`s to data that can be dereferenced, namely to types
+/// that implement [`ValueType`], meaning that they're valid for all possible
+/// bit patterns.
 impl<T: Copy + ValueType> WasmPtr<T, Item> {
-    /// Dereference this `WasmPtr`.
+    /// Dereference the `WasmPtr` getting access to a `&Cell<T>` allowing for
+    /// reading and mutating of the inner value.
+    ///
+    /// This method is unsound to use with unsynchronized shared memory.
+    /// If you're unsure what that means, it likely does not apply to you.
+    /// This invariant will be enforced in the future.
     #[inline]
     pub fn deref<'a>(self, memory: &'a Memory) -> Option<&'a Cell<T>> {
         if (self.offset as usize) + mem::size_of::<T>() >= memory.size().bytes().0 {
@@ -64,7 +90,13 @@ impl<T: Copy + ValueType> WasmPtr<T, Item> {
         }
     }
 
-    /// Mutable dereference this `WasmPtr`.
+    /// Mutably dereference this `WasmPtr` getting a `&mut Cell<T>` allowing for
+    /// direct access to a `&mut T`.
+    ///
+    /// # Safety
+    /// - This method does not do any aliasing checks: it's possible to create
+    ///  `&mut T` that point to the same memory. You should ensure that you have
+    ///   exclusive access to Wasm linear memory before calling this method.
     #[inline]
     pub unsafe fn deref_mut<'a>(self, memory: &'a Memory) -> Option<&'a mut Cell<T>> {
         if (self.offset as usize) + mem::size_of::<T>() >= memory.size().bytes().0 {
@@ -78,8 +110,16 @@ impl<T: Copy + ValueType> WasmPtr<T, Item> {
     }
 }
 
+/// Methods for `WasmPtr`s to arrays of data that can be dereferenced, namely to
+/// types that implement [`ValueType`], meaning that they're valid for all
+/// possible bit patterns.
 impl<T: Copy + ValueType> WasmPtr<T, Array> {
-    /// Dereference this `WasmPtr`.
+    /// Dereference the `WasmPtr` getting access to a `&[Cell<T>]` allowing for
+    /// reading and mutating of the inner values.
+    ///
+    /// This method is unsound to use with unsynchronized shared memory.
+    /// If you're unsure what that means, it likely does not apply to you.
+    /// This invariant will be enforced in the future.
     #[inline]
     pub fn deref(self, memory: &Memory, index: u32, length: u32) -> Option<&[Cell<T>]> {
         // gets the size of the item in the array with padding added such that
@@ -102,7 +142,13 @@ impl<T: Copy + ValueType> WasmPtr<T, Array> {
         }
     }
 
-    /// Mutable dereference this `WasmPtr`.
+    /// Mutably dereference this `WasmPtr` getting a `&mut [Cell<T>]` allowing for
+    /// direct access to a `&mut [T]`.
+    ///
+    /// # Safety
+    /// - This method does not do any aliasing checks: it's possible to create
+    ///  `&mut T` that point to the same memory. You should ensure that you have
+    ///   exclusive access to Wasm linear memory before calling this method.
     #[inline]
     pub unsafe fn deref_mut(
         self,
@@ -128,7 +174,11 @@ impl<T: Copy + ValueType> WasmPtr<T, Array> {
         Some(cell_ptrs)
     }
 
-    /// Get a UTF-8 string representation of this `WasmPtr` with the given length.
+    /// Get a UTF-8 string from the `WasmPtr` with the given length.
+    ///
+    /// Note that this method returns a reference to Wasm linear memory. The
+    /// underlying data can be mutated if the Wasm is allowed to execute or
+    /// an aliasing `WasmPtr` is used to mutate memory.
     pub fn get_utf8_string(self, memory: &Memory, str_len: u32) -> Option<&str> {
         if self.offset as usize + str_len as usize > memory.size().bytes().0 {
             return None;
@@ -138,9 +188,14 @@ impl<T: Copy + ValueType> WasmPtr<T, Array> {
         std::str::from_utf8(slice).ok()
     }
 
-    /// Get a UTF-8 string representation of this `WasmPtr`, where the string is nul-terminated.
+    /// Get a UTF-8 string from the `WasmPtr`, where the string is nul-terminated.
+    ///
     /// Note that this does not account for UTF-8 strings that _contain_ nul themselves,
     /// [`get_utf8_string`] has to be used for those.
+    ///
+    /// Also note that this method returns a reference to Wasm linear memory. The
+    /// underlying data can be mutated if the Wasm is allowed to execute or
+    /// an aliasing `WasmPtr` is used to mutate memory.
     pub fn get_utf8_string_with_nul(self, memory: &Memory) -> Option<&str> {
         memory.view::<u8>()[(self.offset as usize)..]
             .iter()
