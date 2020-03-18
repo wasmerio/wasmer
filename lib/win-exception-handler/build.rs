4de7deb568a82ac7f9e67bae551f366fce43bf49
Jake Shadle <jake.shadle@embark-studios.com>
2020-03-18 20:05:05 +0100

    Replace cmake with cc

--- lib/win-exception-handler/build.rs
+++ lib/win-exception-handler/build.rs
@@ -7,4 +7,8 @@ fn main() {
         println!("cargo:rustc-link-search=native={}", dst.display());
         println!("cargo:rustc-link-lib=static={}", project_name);
     }
+    cc::Build::new()
+        .include("exception_handling")
+        .file("exception_handling/exception_handling.c")
+        .compile("exception_handling");
 }
