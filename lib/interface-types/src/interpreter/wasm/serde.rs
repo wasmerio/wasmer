be66ac8aa4f342aae2d03a23543fd9736f4d0150
Ivan Enderlin <ivan.enderlin@hoa-project.net>
2020-04-02 16:41:03 +0200

    doc(interface-types) Improve `serde` module documentation.

--- lib/interface-types/src/interpreter/wasm/serde.rs
+++ lib/interface-types/src/interpreter/wasm/serde.rs
@@ -1,6 +1,6 @@
 //! Serde is not necessary to use WIT. It only provides a nicer API
-//! for the end-user to rebuild its complex types from WIT values,
-//! like `record`.
+//! for the end-user to send or receive its complex types to/from WIT
+//! values, like `record` for instance.
 
 use crate::{
     ast::InterfaceType,
