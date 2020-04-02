976bf9b6a360a9705c8261bb34bbec3a2c965edb
Mark McCaskey <mark@wasmer.io>
2020-04-01 17:09:51 -0700

    Move integration tests to `tests` dir in workspace root

---
+++ build.rs
@@ -0,0 +1,9 @@
+//! A kind of meta-build.rs that can be configured to do different things.
+//!
+//! Please try to keep this file as clean as possible.
+
+use generate_wasi_tests;
+
+fn main() {
+    generate_wasi_tests::build();
+}
