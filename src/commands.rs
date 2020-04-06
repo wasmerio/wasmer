5572ebd9903cc500acbb03c49c186f9ae3456cde
Syrus <me@syrusakbary.com>
2020-04-06 16:06:49 -0700

    Refactored bin commands into separate files

---
+++ src/commands.rs
@@ -0,0 +1,6 @@
+mod cache;
+mod run;
+mod selfupdate;
+mod validate;
+
+pub use {cache::*, run::*, selfupdate::*, validate::*};
--- src/lib.rs
+++ src/lib.rs
@@ -10,13 +10,8 @@
 #![doc(html_favicon_url = "https://wasmer.io/static/icons/favicon.ico")]
 #![doc(html_logo_url = "https://avatars3.githubusercontent.com/u/44205449?s=200&v=4")]
 
-extern crate wasmer_runtime_core;
-#[macro_use]
-extern crate log;
-
-#[macro_use]
-pub mod update;
+pub mod commands;
+pub mod common;
 #[cfg(feature = "debug")]
 pub mod logging;
 pub mod utils;
-pub mod webassembly;
