ca1b0ee5652bbaa17c6625f516f952b86408cb13
Syrus <me@syrusakbary.com>
2020-04-30 01:58:27 -0700

    Make SourceLoc transparent

--- lib/compiler/src/sourceloc.rs
+++ lib/compiler/src/sourceloc.rs
@@ -12,6 +12,7 @@ use serde::{Deserialize, Serialize};
 /// The default source location uses the all-ones bit pattern `!0`. It is used for instructions
 /// that can't be given a real source location.
 #[derive(Clone, Copy, Debug, PartialEq, Eq, Serialize, Deserialize)]
+#[serde(transparent)]
 pub struct SourceLoc(u32);
 
 impl SourceLoc {
