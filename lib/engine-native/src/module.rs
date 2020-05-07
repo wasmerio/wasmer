9b8665906e8a27d4b5d6becd290335aab9b388f4
Syrus <me@syrusakbary.com>
2020-05-07 11:36:21 -0700

    Improvements on the native compilation

--- lib/engine-native/src/module.rs
+++ lib/engine-native/src/module.rs
@@ -3,10 +3,17 @@
 
 use crate::engine::{NativeEngine, NativeEngineInner};
 use crate::serialize::ModuleMetadata;
+use faerie::{ArtifactBuilder, Decl, Link, Reloc, SectionKind};
+use libloading::Library;
 use serde::{Deserialize, Serialize};
 use std::any::Any;
+use std::error::Error;
 use std::ffi::c_void;
+use std::fmt::Debug;
+use std::fs::File;
+use std::path::PathBuf;
 use std::sync::{Arc, Mutex};
+use tempfile::NamedTempFile;
 use wasm_common::entity::{BoxedSlice, EntityRef, PrimaryMap};
 use wasm_common::{
     DataInitializer, LocalFunctionIndex, LocalGlobalIndex, LocalMemoryIndex, LocalTableIndex,
@@ -24,17 +31,21 @@ use wasmer_runtime::{
     InstanceHandle, LinearMemory, Module, SignatureRegistry, Table, VMFunctionBody,
     VMGlobalDefinition, VMSharedSignatureIndex,
 };
-
 use wasmer_runtime::{MemoryPlan, TablePlan};
 
 /// A compiled wasm module, ready to be instantiated.
 pub struct NativeModule {
+    tempfile_path: PathBuf,
     metadata: ModuleMetadata,
     finished_functions: BoxedSlice<LocalFunctionIndex, *mut [VMFunctionBody]>,
     signatures: BoxedSlice<SignatureIndex, VMSharedSignatureIndex>,
 }
 
 type Handle = *mut c_void;
+
+fn to_compile_error(err: impl Error) -> CompileError {
+    CompileError::Codegen(format!("{}", err))
+}
 
 impl NativeModule {
     /// Compile a data buffer into a `NativeModule`, which may then be instantiated.
@@ -91,6 +102,82 @@ impl NativeModule {
             .collect::<Vec<_>>()
             .into_boxed_slice();
 
+        let target = compiler.target().triple().clone();
+        let mut obj = ArtifactBuilder::new(target)
+            .name("module".to_string())
+            .library(true)
+            .finish();
+
+        // first we declare our symbolic references;
+        // it is a runtime error to define a symbol _without_ declaring it first
+        // let declarations: Vec<(&'static str, Decl)> = vec![
+        //     ("deadbeef", Decl::function().into()),
+        //     // ("main", Decl::function().global().into()),
+        //     // ("str.1", Decl::cstring().into()),
+        //     // ("DEADBEEF", Decl::data_import().into()),
+        //     // ("STATIC_REF", Decl::data().global().writable().with_align(Some(64)).into()),
+        //     // ("printf", Decl::function_import().into()),
+        // ];
+        // obj.declarations(declarations.into_iter()).map_err(to_compile_error)?;
+        // obj.write(file)?;
+
+        let metadata = ModuleMetadata {
+            features: compiler.features().clone(),
+            module: Arc::new(translation.module),
+            data_initializers,
+            memory_plans,
+            table_plans,
+        };
+
+        let serialized_data = bincode::serialize(&metadata).map_err(to_compile_error)?;
+
+        obj.declare("WASMER_METADATA", Decl::data().global().read_only())
+            .map_err(to_compile_error)?;
+        obj.define("WASMER_METADATA", serialized_data)
+            .map_err(to_compile_error)?;
+
+        let function_bodies = compilation.get_function_bodies();
+        let function_relocations = compilation.get_relocations();
+
+        for i in 0..metadata.module.num_imported_funcs {
+            let imported_function_name = format!("wasmer_imported_function_{}", i);
+            obj.declare(imported_function_name, Decl::function_import())
+                .map_err(to_compile_error)?;
+        }
+
+        for (function_local_index, function) in function_bodies.iter() {
+            let function_name = format!("wasmer_function_{}", function_local_index.index());
+            obj.declare(&function_name, Decl::function().global())
+                .map_err(to_compile_error)?;
+            obj.define(&function_name, function.body.clone())
+                .map_err(to_compile_error)?;
+        }
+
+        let file = NamedTempFile::new().map_err(to_compile_error)?;
+
+        // Re-open it.
+        let mut file2 = file.reopen().map_err(to_compile_error)?;
+        let (file, filepath) = file.keep().map_err(to_compile_error)?;
+        obj.write(file).map_err(to_compile_error)?;
+
+        // Compute indices into the shared signature table.
+        let signatures = {
+            let signature_registry = engine_inner.signatures();
+            metadata
+                .module
+                .signatures
+                .values()
+                .map(|sig| signature_registry.register(sig))
+                .collect::<PrimaryMap<_, _>>()
+        };
+        let lib = Library::new(&filepath).unwrap();
+
+        Ok(Self {
+            tempfile_path: filepath,
+            metadata,
+            finished_functions: PrimaryMap::new().into_boxed_slice(),
+            signatures: signatures.into_boxed_slice(),
+        })
         // let frame_infos = compilation
         //     .get_frame_info()
         //     .values()
@@ -113,7 +200,7 @@ impl NativeModule {
         //     memory_plans,
         //     table_plans,
         // };
-        unimplemented!();
+        // unimplemented!();
         // Self::from_parts(&mut engine_inner, metadata, )
     }
 
@@ -127,10 +214,11 @@ impl NativeModule {
 
     /// Serialize a NativeModule
     pub fn serialize(&self) -> Result<Vec<u8>, SerializeError> {
+        std::fs::read(&self.tempfile_path).map_err(|e| SerializeError::Generic(format!("{:?}", e)))
         // let mut s = flexbuffers::FlexbufferSerializer::new();
         // self.serializable.serialize(&mut s).map_err(|e| SerializeError::Generic(format!("{:?}", e)));
         // Ok(s.take_buffer())
-        unimplemented!();
+        // unimplemented!("cant' serialize yet");
         // bincode::serialize(&self.serializable)
         //     .map_err(|e| SerializeError::Generic(format!("{:?}", e)))
     }
