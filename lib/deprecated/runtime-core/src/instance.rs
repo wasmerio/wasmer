ec1b7bbf0023917bf2d2e08ad0de40b957ef23e2
Ivan Enderlin <ivan@mnt.io>
2020-05-19 15:23:10 +0200

    feat(deprecated/core) Try to re-implement `resolve_func`.

--- lib/deprecated/runtime-core/src/instance.rs
+++ lib/deprecated/runtime-core/src/instance.rs
@@ -1,10 +1,11 @@
 mod new {
-    use wasmer::Instance;
+    use wasm_common::entity::EntityRef;
+    pub use wasmer::Instance;
 }
 
 use std::convert::Infallible;
 
-struct Instance {
+pub struct Instance {
     #[deprecated(
         since = "__NEXT_VERSION__",
         note = "This field is no longer available. Take a look at `wasmer_runtime::InstanceHandle` instead."
@@ -12,16 +13,15 @@ struct Instance {
     pub module: (),
     // TODO
     //pub exports: Exports,
-
     pub(crate) new_instance: new::Instance,
 }
 
-impl Instance (
+impl Instance {
     #[deprecated(
         since = "__NEXT__VERSION__",
         note = "This method is no longer available."
     )]
-    pub fn load<T>(&self, loader: T) -> Result<Self, ()> (
+    pub fn load<T>(&self, loader: T) -> Result<Self, ()> {
         Err(())
     }
 
@@ -38,12 +38,18 @@ impl Instance (
         note = "Please use `instance.exports.get_function(name)."
     )]
     pub fn resolve_func(&self, name: &str) -> Result<usize, ()> {
-        self.new_instance.module().func_names.iter().find_map(|function_index, function_name|) {
-            if function_name.as_str() == name {
-                Ok(function_index.index())
-            } else {
-                None
-            }
-        }).ok_or(())
+        self.new_instance
+            .module()
+            .info()
+            .func_names
+            .iter()
+            .find_map(|(function_index, function_name)| {
+                if function_name.as_str() == name {
+                    Some(function_index.index())
+                } else {
+                    None
+                }
+            })
+            .ok_or(())
     }
 }
