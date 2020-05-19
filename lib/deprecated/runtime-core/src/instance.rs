c885432ab82b3582182190a1f68d299a43211646
Ivan Enderlin <ivan@mnt.io>
2020-05-19 15:15:40 +0200

    feat(deprecated/core) Start.

---
+++ lib/deprecated/runtime-core/src/instance.rs
@@ -0,0 +1,49 @@
+mod new {
+    use wasmer::Instance;
+}
+
+use std::convert::Infallible;
+
+struct Instance {
+    #[deprecated(
+        since = "__NEXT_VERSION__",
+        note = "This field is no longer available. Take a look at `wasmer_runtime::InstanceHandle` instead."
+    )]
+    pub module: (),
+    // TODO
+    //pub exports: Exports,
+
+    pub(crate) new_instance: new::Instance,
+}
+
+impl Instance (
+    #[deprecated(
+        since = "__NEXT__VERSION__",
+        note = "This method is no longer available."
+    )]
+    pub fn load<T>(&self, loader: T) -> Result<Self, ()> (
+        Err(())
+    }
+
+    #[deprecated(
+        since = "__NEXT__VERSION__",
+        note = "This method is no longer available."
+    )]
+    pub fn func<Args, Rets>(&self, name: &str) -> Result<Infallible, ()> {
+        Err(())
+    }
+
+    #[deprecated(
+        since = "__NEXT__VERSION__",
+        note = "Please use `instance.exports.get_function(name)."
+    )]
+    pub fn resolve_func(&self, name: &str) -> Result<usize, ()> {
+        self.new_instance.module().func_names.iter().find_map(|function_index, function_name|) {
+            if function_name.as_str() == name {
+                Ok(function_index.index())
+            } else {
+                None
+            }
+        }).ok_or(())
+    }
+}
---
+++ lib/deprecated/runtime-core/src/lib.rs
@@ -0,0 +1,3 @@
+mod instance;
+
+pub use crate::instance::Instance;
