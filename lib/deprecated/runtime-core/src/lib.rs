2e3c14d24d068701fa765259b2c38d16f5fbecc2
Ivan Enderlin <ivan@mnt.io>
2020-05-22 14:57:47 +0200

    feat(deprecated) Fix porting units.

--- lib/deprecated/runtime-core/src/lib.rs
+++ lib/deprecated/runtime-core/src/lib.rs
@@ -4,6 +4,7 @@ mod functional_api;
 pub mod instance;
 pub mod module;
 pub mod types;
+pub mod units;
 
 pub(crate) mod new {
     pub use wasm_common;
--- lib/deprecated/runtime-core/src/units.rs
+++ lib/deprecated/runtime-core/src/units.rs
@@ -10,6 +10,6 @@ pub use new::wasm_common::{
 // ```rust
 // pub use new::wasm_common::{WASM_MAX_PAGES, WASM_MIN_PAGES, WASM_PAGE_SIZE};
 // ```
-const WASM_MAX_PAGES = 0x10000;
-const WASM_MIN_PAGES = 0x100;
-const WASM_PAGE_SIZE = 0x10000;
+pub const WASM_MAX_PAGES: u32 = 0x10000;
+pub const WASM_MIN_PAGES: u32 = 0x100;
+pub const WASM_PAGE_SIZE: u32 = 0x10000;
