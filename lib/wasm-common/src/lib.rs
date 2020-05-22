540e453cd1428ce8fc67a407124fabc33d925055
Ivan Enderlin <ivan@mnt.io>
2020-05-22 14:24:10 +0200

    feat(wasm-common) Expose the `WASM_*` units.

--- lib/wasm-common/src/lib.rs
+++ lib/wasm-common/src/lib.rs
@@ -48,7 +48,7 @@ pub use crate::native::{
     WithoutEnv,
 };
 pub use crate::r#ref::{AnyRef, HostInfo, HostRef};
-pub use crate::units::{Bytes, Pages};
+pub use crate::units::{Bytes, Pages, WASM_MAX_PAGES, WASM_MIN_PAGES, WASM_PAGE_SIZE};
 pub use crate::values::Value;
 pub use types::{
     ExportType, ExternType, FunctionType, GlobalInit, GlobalType, ImportType, MemoryType,
