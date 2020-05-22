0bcbaafeab5f9b4fee3b8074c5db59fcc610c223
Ivan Enderlin <ivan@mnt.io>
2020-05-22 15:29:58 +0200

    feat(deprecated) Port `Module::imports` and `Module::exports`.

--- lib/deprecated/runtime-core/src/module.rs
+++ lib/deprecated/runtime-core/src/module.rs
@@ -23,4 +23,12 @@ impl Module {
     pub fn info(&self) -> &new::wasmer_runtime::ModuleInfo {
         &self.new_module.info()
     }
+
+    pub fn imports(&self) -> Vec<crate::types::ImportDescriptor> {
+        self.new_module.imports().collect()
+    }
+
+    pub fn exports(&self) -> Vec<crate::types::ExportDescriptor> {
+        self.new_module.exports().collect()
+    }
 }
