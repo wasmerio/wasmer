d911e377aa011755a85cd61d999647c696944830
Ivan Enderlin <ivan@mnt.io>
2020-05-22 11:26:05 +0200

    feat(deprecated) Optimize visiblity.

--- lib/deprecated/runtime-core/src/lib.rs
+++ lib/deprecated/runtime-core/src/lib.rs
@@ -1,9 +1,9 @@
 mod instance;
 pub mod types;
 
-mod new {
-    pub(crate) use wasm_common;
-    pub(crate) use wasmer;
+pub(crate) mod new {
+    pub use wasm_common;
+    pub use wasmer;
 }
 
 pub use crate::instance::Instance;
