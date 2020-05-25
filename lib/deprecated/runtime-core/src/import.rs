7263f61b5a0dcf77c16881d72ab901ec7daccf24
Ivan Enderlin <ivan@mnt.io>
2020-05-25 11:04:01 +0200

    feat(deprecated) Port `Namespace` and `LikeNamespace`.

---
+++ lib/deprecated/runtime-core/src/import.rs
@@ -0,0 +1,30 @@
+use crate::new;
+
+pub use new::wasmer::{imports, ImportObject, ImportObjectIterator, LikeNamespace};
+
+pub struct Namespace {
+    exports: new::wasmer::Exports,
+}
+
+impl Namespace {
+    pub fn new() -> Self {
+        Self {
+            exports: new::wasmer::Exports::new(),
+        }
+    }
+
+    pub fn insert<N, V>(&mut self, name: N, value: V)
+    where
+        N: Into<String>,
+        V: Into<new::wasmer::Extern> + Send + 'static,
+    {
+        self.exports.insert(name, value);
+    }
+
+    pub fn contains_key<N>(&mut self, name: N) -> bool
+    where
+        N: Into<String>,
+    {
+        self.exports.contains(name)
+    }
+}
--- lib/deprecated/runtime-core/src/lib.rs
+++ lib/deprecated/runtime-core/src/lib.rs
@@ -7,10 +7,8 @@ pub(crate) mod new {
 }
 
 mod functional_api;
+pub mod import;
 pub mod instance;
-pub mod import {
-    pub use crate::new::wasmer::{imports, ImportObject, ImportObjectIterator};
-}
 pub mod module;
 pub mod types;
 pub mod units;
@@ -19,3 +17,7 @@ pub use crate::instance::Instance;
 pub use crate::module::Module;
 pub use crate::units::{Bytes, Pages, WASM_MAX_PAGES, WASM_MIN_PAGES, WASM_PAGE_SIZE};
 pub use functional_api::{compile_with, compile_with_config, validate};
+
+pub mod prelude {
+    pub use crate::import::{ImportObject, Namespace};
+}
