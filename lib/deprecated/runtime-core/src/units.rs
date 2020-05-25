30d20d40f21f9d9fb5ad8006c7bfb2a1842aa51d
Ivan Enderlin <ivan@mnt.io>
2020-05-25 10:31:12 +0200

    feat(deprecated) #47 has been merged, update accordingly.

--- lib/deprecated/runtime-core/src/units.rs
+++ lib/deprecated/runtime-core/src/units.rs
@@ -4,12 +4,7 @@ pub use new::wasm_common::{
     //
     Bytes,
     Pages,
+    WASM_MAX_PAGES,
+    WASM_MIN_PAGES,
+    WASM_PAGE_SIZE,
 };
-
-// Once https://github.com/wasmerio/wasmer-reborn/pull/47 got merged, we can replace the following lines by:
-// ```rust
-// pub use new::wasm_common::{WASM_MAX_PAGES, WASM_MIN_PAGES, WASM_PAGE_SIZE};
-// ```
-pub const WASM_MAX_PAGES: u32 = 0x10000;
-pub const WASM_MIN_PAGES: u32 = 0x100;
-pub const WASM_PAGE_SIZE: u32 = 0x10000;
