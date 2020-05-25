17f02e21bd4e9854b69245023537c6d1138c0621
Ivan Enderlin <ivan@mnt.io>
2020-05-25 14:23:51 +0200

    feat(deprecated) Port `MemoryDescriptor` and `MemoryType`.

--- lib/deprecated/runtime-core/src/memory.rs
+++ lib/deprecated/runtime-core/src/memory.rs
@@ -1,3 +1,6 @@
 pub mod ptr {
     pub use crate::new::wasmer::{Array, Item, WasmPtr};
 }
+
+pub use crate::new::wasm_common::MemoryType as MemotyDescriptor;
+pub use crate::new::wasmer_runtime::MemoryStyle as MemoryType;
