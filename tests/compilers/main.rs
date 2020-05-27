4cda8a29f78d36373c0694b0e27657a0a7893891
Syrus <me@syrusakbary.com>
2020-05-27 11:55:45 -0700

    Moved wast test into compiler tests

--- tests/compilers/main.rs
+++ tests/compilers/main.rs
@@ -1,3 +1,8 @@
+//! This test suite does all the tests that involve any compiler
+//! implementation, such as: singlepass, cranelift or llvm depending
+//! on what's available on the target.
+
 #[macro_use]
 mod macros;
 mod trampolines;
+mod wast;
