62fb4f79c1c5a3961efb37567ed8789a1f29c143
Syrus <me@syrusakbary.com>
2020-05-27 11:55:21 -0700

    Moved misc test into api test

--- lib/api/tests/externals.rs
+++ lib/api/tests/externals.rs
@@ -152,15 +152,41 @@ fn memory_new() -> Result<()> {
 #[test]
 fn memory_grow() -> Result<()> {
     let store = Store::default();
-    let memory_type = MemoryType {
-        shared: false,
-        minimum: Pages(0),
-        maximum: Some(Pages(10)),
-    };
-    let memory = Memory::new(&store, memory_type)?;
-    let growed = memory.grow(5)?;
-    assert_eq!(growed, Pages(0));
-    assert_eq!(memory.size(), Pages(5));
+
+    let desc = MemoryType::new(Pages(10), Some(Pages(16)), false);
+    let memory = Memory::new(&store, desc)?;
+    assert_eq!(memory.size(), Pages(10));
+
+    let result = memory.grow(Pages(2)).unwrap();
+    assert_eq!(result, Pages(10));
+    assert_eq!(memory.size(), Pages(12));
+
+    let result = memory.grow(Pages(10));
+    assert_eq!(
+        result,
+        Err(MemoryError::CouldNotGrow {
+            current: 12.into(),
+            attempted_delta: 10.into(),
+        })
+    );
+
+    let bad_desc = MemoryType::new(Pages(15), Some(Pages(10)), false);
+    let bad_result = Memory::new(&store, bad_desc);
+
+    // due to stack overflow with a modern nightly, we can't update CI to use a version of nightly which will make this work
+    /*assert!(matches!(
+        bad_result,
+        Err(MemoryError::InvalidMemoryPlan { .. })
+    ));*/
+
+    assert!(
+        if let Err(MemoryError::InvalidMemoryPlan { .. }) = bad_result {
+            true
+        } else {
+            false
+        }
+    );
+
     Ok(())
 }
 
