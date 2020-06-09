7ff30d5a6fb7d497d79bc9269de2e8b9d73f35c6
Ivan Enderlin <ivan@mnt.io>
2020-06-09 13:54:12 +0200

    chore(wasm-common) Deny `clippy::all`.

--- lib/wasm-common/src/lib.rs
+++ lib/wasm-common/src/lib.rs
@@ -4,7 +4,7 @@
 //! This crate provides common structures such as `Type` or `Value`, type indexes
 //! and native function wrappers with `Func`.
 
-#![deny(missing_docs, unused_extern_crates)]
+#![deny(missing_docs, unused_extern_crates, clippy::all)]
 #![warn(unused_import_braces)]
 #![cfg_attr(feature = "std", deny(unstable_features))]
 #![cfg_attr(feature = "clippy", plugin(clippy(conf_file = "../../clippy.toml")))]
