6f2f611dc0416357c5c10b274cd99e2255c8af9d
Ivan Enderlin <ivan@mnt.io>
2020-06-09 15:21:15 +0200

    chore(engine-native) Declare variables as unused.

--- lib/engine-native/src/artifact.rs
+++ lib/engine-native/src/artifact.rs
@@ -489,7 +489,7 @@ impl NativeArtifact {
 
     /// Compile a data buffer into a `NativeArtifact`, which may then be instantiated.
     #[cfg(not(feature = "compiler"))]
-    pub fn new(engine: &NativeEngine, data: &[u8]) -> Result<Self, CompileError> {
+    pub fn new(_engine: &NativeEngine, _data: &[u8]) -> Result<Self, CompileError> {
         Err(CompileError::Codegen(
             "Compilation is not enabled in the engine".to_string(),
         ))
