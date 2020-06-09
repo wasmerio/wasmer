d57a7173fc24e8618731aa3cc5137c494cb2cf97
Ivan Enderlin <ivan@mnt.io>
2020-06-09 10:18:51 +0200

    feat(deprecated) Re-implement the `imports!` macro.

--- lib/deprecated/runtime-core/src/import.rs
+++ lib/deprecated/runtime-core/src/import.rs
@@ -1,6 +1,6 @@
 use crate::new;
 
-pub use new::wasmer::{imports, namespace, ImportObject, ImportObjectIterator, LikeNamespace};
+pub use new::wasmer::{namespace, ImportObject, ImportObjectIterator, LikeNamespace};
 
 pub struct Namespace {
     exports: new::wasmer::Exports,
@@ -44,3 +44,88 @@ impl LikeNamespace for Namespace {
     note = "Please use the `Exportable` trait instead."
 )]
 pub trait IsExport {}
+
+/// Generate an `ImportObject` easily with the `imports!` macro.
+///
+/// # Usage
+///
+/// ```
+/// # use wasmer_runtime_core::{imports, func};
+///
+/// let import_object = imports! {
+///     "env" => {
+///         "foo" => func!(foo)
+///     },
+/// };
+///
+/// fn foo(n: i32) -> i32 {
+///     n
+/// }
+/// ```
+///
+/// or by passing a state creator for the import object:
+///
+/// ```
+/// # use wasmer_runtime_core::{imports, func};
+///
+/// let import_object = imports! {
+///     || (0 as _, |_a| {}),
+///     "env" => {
+///         "foo" => func!(foo)
+///     },
+/// };
+///
+/// # fn foo(n: i32) -> i32 {
+/// #     n
+/// # }
+/// ```
+#[macro_export]
+macro_rules! imports {
+    ( $( $namespace_name:expr => $namespace:tt ),* $(,)? ) => {
+        {
+            let mut import_object = $crate::import::ImportObject::new();
+
+            $({
+                let namespace = $crate::import_namespace!($namespace);
+
+                import_object.register($namespace_name, namespace);
+            })*
+
+            import_object
+        }
+    };
+
+    ($state_creator:expr, $( $namespace_name:expr => $namespace:tt ),* $(,)? ) => {
+        {
+            let mut import_object = $crate::import::ImportObject::new_with_data($state_creator);
+
+            $({
+                let namespace = $crate::import_namespace!($namespace);
+
+                import_object.register($namespace_name, namespace);
+            })*
+
+            import_object
+        }
+    };
+}
+
+#[macro_export]
+#[doc(hidden)]
+macro_rules! import_namespace {
+    ( { $( $import_name:expr => $import_item:expr ),* $(,)? } ) => {
+        {
+            let mut namespace = $crate::instance::Exports::new();
+
+            $(
+                namespace.insert($import_name, $import_item);
+            )*
+
+            namespace
+        }
+    };
+
+    ($ns:ident) => {
+        $ns
+    };
+}
