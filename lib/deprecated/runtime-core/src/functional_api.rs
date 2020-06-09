c9fb7a9439b9e7f5e0ea2b12bd44265935415510
Ivan Enderlin <ivan@mnt.io>
2020-06-09 16:37:10 +0200

    feat(deprecated) Remove `deprecated` attribute.

--- lib/deprecated/runtime-core/src/functional_api.rs
+++ lib/deprecated/runtime-core/src/functional_api.rs
@@ -9,20 +9,12 @@ pub fn load_cache_with(cache: Artifact) -> Result<Module, Infallible> {
     Ok(cache.module())
 }
 
-#[deprecated(
-    since = "__NEXT_VERSION__",
-    note = "Please use `Module::new(&store, bytes)` instead."
-)]
 pub fn compile_with(bytes: &[u8], _compiler: ()) -> Result<Module, Box<dyn Error>> {
     let store = Default::default();
 
     Ok(Module::new(new::wasmer::Module::new(&store, bytes)?))
 }
 
-#[deprecated(
-    since = "__NEXT_VERSION__",
-    note = "Please use `Module::new(&store, bytes)` instead."
-)]
 pub fn compile_with_config(
     bytes: &[u8],
     _compiler: (),
@@ -33,10 +25,6 @@ pub fn compile_with_config(
     Ok(Module::new(new::wasmer::Module::new(&store, bytes)?))
 }
 
-#[deprecated(
-    since = "__NEXT_VERSION__",
-    note = "Please use `Module::validate(&store, bytes)` instead."
-)]
 pub fn validate(bytes: &[u8]) -> bool {
     let store = Default::default();
 
