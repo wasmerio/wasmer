1002a2765c248b283bfa5cc199ec5de5977fb29b
Syrus <me@syrusakbary.com>
2020-06-18 01:34:11 -0700

    Improved default store

--- lib/api/src/store.rs
+++ lib/api/src/store.rs
@@ -37,47 +37,57 @@ impl PartialEq for Store {
 }
 
 // We only implement default if we have assigned a default compiler and engine
-#[cfg(all(feature = "compiler", feature = "engine"))]
+#[cfg(all(feature = "default-compiler", feature = "default-engine"))]
 impl Default for Store {
     fn default() -> Store {
         // We store them on a function that returns to make
         // sure this function doesn't emit a compile error even if
         // more than one compiler is enabled.
         #[allow(unreachable_code)]
-        fn get_config() -> Box<dyn CompilerConfig + Send + Sync> {
-            #[cfg(feature = "cranelift")]
-            return Box::new(wasmer_compiler_cranelift::CraneliftConfig::default());
-
-            #[cfg(feature = "llvm")]
-            return Box::new(wasmer_compiler_llvm::LLVMConfig::default());
-
-            #[cfg(feature = "singlepass")]
-            return Box::new(wasmer_compiler_singlepass::SinglepassConfig::default());
+        fn get_config() -> impl CompilerConfig + Send + Sync {
+            cfg_if::cfg_if! {
+                if #[cfg(any(
+                    all(feature = "default-llvm", any(feature = "default-cranelift", feature = "default-singlepass")),
+                    all(feature = "default-cranelift", feature = "default-singlepass")
+                ))] {
+                    compile_error!("Only one compiler can be the default")
+                } else if #[cfg(feature = "default-cranelift")] {
+                    wasmer_compiler_cranelift::CraneliftConfig::default()
+                } else if #[cfg(feature = "default-llvm")] {
+                    wasmer_compiler_llvm::LLVMConfig::default()
+                } else if #[cfg(feature = "default-singlepass")] {
+                    wasmer_compiler_singlepass::SinglepassConfig::default()
+                } else {
+                    compile_error!("No compiler chosen")
+                }
+            }
         }
 
         #[allow(unreachable_code)]
-        fn get_engine(
-            config: Box<dyn CompilerConfig + Send + Sync>,
-        ) -> Box<dyn Engine + Send + Sync> {
-            #[cfg(feature = "jit")]
-            return Box::new(
-                wasmer_engine_jit::JIT::new(&*config)
-                    .tunables(Tunables::for_target)
-                    .engine(),
-            );
-
-            // #[cfg(feature = "native")]
-            // return Arc::new(wasmer_engine_native::NativeEngine::new(
-            //     config,
-            //     tunables,
-            //     Default::default(),
-            // ));
+        fn get_engine(config: impl CompilerConfig + Send + Sync) -> impl Engine + Send + Sync {
+            cfg_if::cfg_if! {
+                if #[cfg(all(
+                    feature = "default-jit", feature = "default-native"
+                ))] {
+                    compile_error!("Only one engine can be the default")
+                } else if #[cfg(feature = "default-jit")] {
+                    wasmer_engine_jit::JIT::new(&config)
+                        .tunables(Tunables::for_target)
+                        .engine()
+                } else if #[cfg(feature = "default-llvm")] {
+                    wasmer_engine_native::Native::new(&config)
+                        .tunables(Tunables::for_target)
+                        .engine()
+                } else {
+                    compile_error!("No default engine chosen")
+                }
+            }
         }
 
         let config = get_config();
         let engine = get_engine(config);
         Store {
-            engine: engine.into(),
+            engine: Arc::new(engine),
         }
     }
 }
