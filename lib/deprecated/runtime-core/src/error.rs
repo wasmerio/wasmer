e0ee4f8fec72953bd75e348cb23f0aafcc9309cb
Ivan Enderlin <ivan@mnt.io>
2020-06-25 13:27:06 +0200

    feat(deprecated) Port `ExportError`.

--- lib/deprecated/runtime-core/src/error.rs
+++ lib/deprecated/runtime-core/src/error.rs
@@ -1,3 +1,3 @@
-pub use crate::new::wasmer::MemoryError;
+pub use crate::new::wasmer::{ExportError, MemoryError};
 pub use crate::new::wasmer_compiler::CompileError;
 pub use crate::new::wasmer_engine::{InstantiationError, LinkError, RuntimeError};
