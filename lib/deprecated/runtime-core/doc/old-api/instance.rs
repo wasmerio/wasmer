921c48ea773a04cf91f12557d931daff45537c7d
Ivan Enderlin <ivan@mnt.io>
2020-06-30 16:42:12 +0200

    doc(deprecated) Start documenting the diff between the old and the new API.

---
+++ lib/deprecated/runtime-core/doc/old-api/instance.rs
@@ -0,0 +1,12 @@
+struct Instance {
+    exports: Exports,
+}
+
+impl Instance {
+    fn load<T>(&self, _loader: T) -> Result<Self, ()>;
+    fn resolve_func(&self, name: &str) -> Result<usize, ()>;
+    fn call(&self, name: &str, params: &[Value]) -> Result<Vec<Value>, Box<dyn Error>>;
+    fn context(&self) -> Ref<Ctx>;
+    fn context_mut(&mut self) -> RefMut<Ctx>;
+    fn module(&self) -> Module;
+}
