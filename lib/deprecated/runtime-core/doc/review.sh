9a7c8a7dd19d95cbe07877ff8361c4bcc9cd572a
Ivan Enderlin <ivan@mnt.io>
2020-07-03 15:54:10 +0200

    doc(deprecated) Show identical files in `review.sh`.

--- lib/deprecated/runtime-core/doc/review.sh
+++ lib/deprecated/runtime-core/doc/review.sh
@@ -3,7 +3,7 @@
 for i in `\ls new-api | grep "${1-.}"`; do
     clear
     echo -e "#######\n### ${i}\n#######\n\n\n\n"
-    diff -u old-api/${i} new-api/${i} | diffr
+    diff --report-identical-files --unified=10 old-api/${i} new-api/${i} | diffr
 
     echo -e "\n\n\n\nPress any key to diff the next file…"
     read
