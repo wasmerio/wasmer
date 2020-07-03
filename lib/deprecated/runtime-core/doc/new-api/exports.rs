c3ff296bcf3d2ec02cf97b4301c40a19f000be04
Ivan Enderlin <ivan@mnt.io>
2020-07-03 16:08:32 +0200

    doc(deprecated) Fix typo + simplify the review.

--- lib/deprecated/runtime-core/doc/new-api/exports.rs
+++ lib/deprecated/runtime-core/doc/new-api/exports.rs
@@ -2,5 +2,5 @@ struct Exports {}
 
 impl Exports {
     fn new() -> Self;
-    fn get<'a, T>(&'a self, name: &str) -> Result<T, ExportError>;
+    fn get<'a, T: Exportable<'a> + Clone + 'a>(&'a self, name: &str) -> Result<T, ExportError>;
 }
--- lib/deprecated/runtime-core/doc/old-api/pages.rs
+++ lib/deprecated/runtime-core/doc/old-api/pages.rs
@@ -1,6 +1,6 @@
 struct Pages(pub u32);
 
 impl Pages {
-    fn checked_add(self, rhs: Pages) -> Result<Pages, PageError>;
+    fn checked_add(self, rhs: Self) -> Result<Pages, PageError>;
     fn bytes(self) -> Bytes;
 }
