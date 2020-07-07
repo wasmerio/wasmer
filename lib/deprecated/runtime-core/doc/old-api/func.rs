5d56cf2d7f9af5e6dacaa2ba774331c85319e399
Ivan Enderlin <ivan@mnt.io>
2020-07-07 13:22:49 +0200

    doc(deprecated) Fix a typo.

--- lib/deprecated/runtime-core/doc/old-api/func.rs
+++ lib/deprecated/runtime-core/doc/old-api/func.rs
@@ -4,6 +4,6 @@ impl<Args, Rets> Func<Args, Rets> {
     fn new<F>(func: F) -> Self;
     fn params(&self) -> &'static [Type];
     fn returns(&self) -> &'static [Type];
-    fn call(...) -> Results<Rets, RuntimeError>;
+    fn call(...) -> Result<Rets, RuntimeError>;
     fn get_vm_func(&self) -> NonNull<Func>;
 }
