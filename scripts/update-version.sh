e06ca4ffd2c6774a20e9a161031ff48e61f947b4
Mark McCaskey <mark@wasmer.io>
2020-08-14 14:37:26 -0700

    Prepare for 1.0.0-alpha01.0 release
    This is the first alpha release of `1.0.0`: expect breaking changes.
    
    The version suffix `alpha01.0` was chosen so that we can ship updates
    that automatically update with the latter number and can prevent
    auto-updates by incrementing the former number (which is not actually
    a number as far as semver is concerned).
    
    Also because crates.io sorts versions lexicographically, it will
    display the wrong readme if we get to alpha10, so we pad with an extra
    0 just in case we need more than 9 releases of alpha.

--- scripts/update-version.sh
+++ scripts/update-version.sh
@@ -1,6 +1,6 @@
 # A script to update the version of all the crates at the same time
-PREVIOUS_VERSION='1.0.0-alpha.1'
-NEXT_VERSION='1.0.0-alpha.2'
+PREVIOUS_VERSION='1.0.0-alpha1.0'
+NEXT_VERSION='1.0.0-alpha01.0'
 
 # quick hack
 fd Cargo.toml --exec sed -i '' "s/version = \"$PREVIOUS_VERSION\"/version = \"$NEXT_VERSION\"/"
