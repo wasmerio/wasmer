3e70e9ccbbeaa1fc452070a4966bad222a6593e9
Ivan Enderlin <ivan@mnt.io>
2020-08-31 10:21:52 +0200

    test: Test the JIT engine being dropped twice through the Store.

---
+++ lib/api/tests/store.rs
@@ -0,0 +1,19 @@
+use anyhow::Result;
+use wasmer::*;
+
+#[test]
+fn store_dropped_twice() -> Result<()> {
+    let wat = r#"(module)"#;
+
+    {
+        let store = Store::default();
+        let _module = Module::new(&store, wat)?;
+    }
+
+    {
+        let store = Store::default();
+        let _module = Module::new(&store, wat)?;
+    }
+
+    Ok(())
+}
