8c03da87a987c1609bbb5de9c310b65043d8fe43
Ivan Enderlin <ivan@mnt.io>
2020-09-22 13:14:48 +0200

    feat(c-api) Reorganize the `build.rs` script.

--- lib/c-api/build.rs
+++ lib/c-api/build.rs
@@ -1,22 +1,7 @@
-extern crate cbindgen;
-
 use cbindgen::{Builder, Language};
 use std::{env, fs, path::PathBuf};
 
-fn main() {
-    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
-    let mut crate_wasmer_header_file = PathBuf::from(&crate_dir);
-    crate_wasmer_header_file.push("wasmer");
-
-    let out_dir = env::var("OUT_DIR").unwrap();
-    let mut out_wasmer_header_file = PathBuf::from(&out_dir);
-    out_wasmer_header_file.push("wasmer");
-
-    let mut pre_header = r#"
-#if !defined(WASMER_H_MACROS)
-
-#define WASMER_H_MACROS
-
+const PRE_HEADER: &'static str = r#"
 // Define the `ARCH_X86_X64` constant.
 #if defined(MSVC) && defined(_M_AMD64)
 #  define ARCH_X86_64
@@ -40,67 +25,119 @@ fn main() {
 #elif defined(MSVC) || __has_declspec_attribute(deprecated)
 #  define DEPRECATED(message) __declspec(deprecated(message))
 #endif
+"#;
 
-"#
-    .to_string();
+const WASI_FEATURE_AS_C_DEFINE: &'static str = "WASMER_WASI_ENABLED";
+const EMSCRIPTEN_FEATURE_AS_C_DEFINE: &'static str = "WASMER_EMSCRIPTEN_ENABLED";
+
+fn main() {
+    let crate_dir = env::var("CARGO_MANIFEST_DIR").unwrap();
+    let out_dir = env::var("OUT_DIR").unwrap();
+
+    build_wasmer_headers(&crate_dir, &out_dir);
+}
+
+fn build_wasmer_headers(crate_dir: &str, out_dir: &str) {
+    let mut crate_header_file = PathBuf::from(crate_dir);
+    crate_header_file.push("wasmer");
+
+    let mut out_header_file = PathBuf::from(out_dir);
+    out_header_file.push("wasmer");
+
+    let mut pre_header = format!(
+        r#"// The Wasmer C/C++ header file.
+
+#if !defined(WASMER_H_MACROS)
+
+#define WASMER_H_MACROS
+{pre_header}"#,
+        pre_header = PRE_HEADER
+    );
 
     #[cfg(feature = "wasi")]
     {
-        pre_header += "#define WASMER_WASI_ENABLED\n";
+        pre_header.push_str(&format!(
+            r#"
+// The `wasi` feature has been enabled for this build.
+#define {define}
+
+"#,
+            define = WASI_FEATURE_AS_C_DEFINE
+        ));
     }
 
     #[cfg(feature = "emscripten")]
     {
-        pre_header += "#define WASMER_EMSCRIPTEN_ENABLED\n";
+        pre_header.push_str(&format!(
+            r#"
+// The `emscripten` feature has been enabled for this build.
+#define {define}
+
+"#,
+            define = EMSCRIPTEN_FEATURE_AS_C_DEFINE
+        ));
     }
 
     // Close pre header.
-    pre_header += "#endif // WASMER_H_MACROS\n";
+    pre_header.push_str(
+        r#"#endif // WASMER_H_MACROS
 
-    // Generate the C bindings in the `OUT_DIR`.
-    out_wasmer_header_file.set_extension("h");
-    Builder::new()
-        .with_crate(crate_dir.clone())
-        .with_language(Language::C)
-        .with_include_guard("WASMER_H")
-        .with_header(&pre_header)
-        .with_define("target_family", "windows", "_WIN32")
-        .with_define("target_arch", "x86_64", "ARCH_X86_64")
-        .with_define("feature", "wasi", "WASMER_WASI_ENABLED")
-        .with_define("feature", "emscripten", "WASMER_EMSCRIPTEN_ENABLED")
-        .generate()
-        .expect("Unable to generate C bindings")
-        .write_to_file(out_wasmer_header_file.as_path());
 
-    // Generate the C++ bindings in the `OUT_DIR`.
-    out_wasmer_header_file.set_extension("hh");
-    Builder::new()
-        .with_crate(crate_dir)
-        .with_language(Language::Cxx)
-        .with_include_guard("WASMER_H")
-        .with_header(&pre_header)
-        .with_define("target_family", "windows", "_WIN32")
-        .with_define("target_arch", "x86_64", "ARCH_X86_64")
-        .with_define("feature", "wasi", "WASMER_WASI_ENABLED")
-        .with_define("feature", "emscripten", "WASMER_EMSCRIPTEN_ENABLED")
-        .generate()
-        .expect("Unable to generate C++ bindings")
-        .write_to_file(out_wasmer_header_file.as_path());
+//
+// OK, here we go. The code below is automatically generated.
+//
+"#,
+    );
 
-    // Copy the generated C bindings from `OUT_DIR` to
-    // `CARGO_MANIFEST_DIR`.
-    crate_wasmer_header_file.set_extension("h");
-    out_wasmer_header_file.set_extension("h");
-    fs::copy(
-        out_wasmer_header_file.as_path(),
-        crate_wasmer_header_file.as_path(),
-    )
-    .expect("Unable to copy the generated C bindings");
+    // C bindings.
+    {
+        // Generate the bindings in the `OUT_DIR`.
+        out_header_file.set_extension("h");
 
-    // Copy the generated C++ bindings from `OUT_DIR` to
-    // `CARGO_MANIFEST_DIR`.
-    crate_wasmer_header_file.set_extension("hh");
-    out_wasmer_header_file.set_extension("hh");
-    fs::copy(out_wasmer_header_file, crate_wasmer_header_file)
-        .expect("Unable to copy the generated C++ bindings");
+        Builder::new()
+            .with_language(Language::C)
+            .with_crate(crate_dir)
+            .with_include_guard("WASMER_H")
+            .with_header(&pre_header)
+            .with_define("target_family", "windows", "_WIN32")
+            .with_define("target_arch", "x86_64", "ARCH_X86_64")
+            .with_define("feature", "wasi", WASI_FEATURE_AS_C_DEFINE)
+            .with_define("feature", "emscripten", EMSCRIPTEN_FEATURE_AS_C_DEFINE)
+            .generate()
+            .expect("Unable to generate C bindings")
+            .write_to_file(out_header_file.as_path());
+
+        // Copy the generated bindings from `OUT_DIR` to
+        // `CARGO_MANIFEST_DIR`.
+        crate_header_file.set_extension("h");
+
+        fs::copy(out_header_file.as_path(), crate_header_file.as_path())
+            .expect("Unable to copy the generated C bindings");
+    }
+
+    // C++ bindings.
+    {
+        // Generate the bindings in the `OUT_DIR`.
+        out_header_file.set_extension("hh");
+
+        Builder::new()
+            .with_language(Language::Cxx)
+            .with_crate(crate_dir)
+            .with_include_guard("WASMER_H")
+            .with_header(&pre_header)
+            .with_define("target_family", "windows", "_WIN32")
+            .with_define("target_arch", "x86_64", "ARCH_X86_64")
+            .with_define("feature", "wasi", WASI_FEATURE_AS_C_DEFINE)
+            .with_define("feature", "emscripten", EMSCRIPTEN_FEATURE_AS_C_DEFINE)
+            .generate()
+            .expect("Unable to generate C++ bindings")
+            .write_to_file(out_header_file.as_path());
+
+        // Copy the generated bindings from `OUT_DIR` to
+        // `CARGO_MANIFEST_DIR`.
+        crate_header_file.set_extension("hh");
+
+        fs::copy(out_header_file, crate_header_file)
+            .expect("Unable to copy the generated C++ bindings");
+    }
 }
