92ca7899f532de348c99de7253d87e0cb8adb848
Ivan Enderlin <ivan@mnt.io>
2020-10-02 11:15:37 +0200

    chore(c-api) Use canonical path to `wasm*.h` headers.

--- lib/c-api/tests/wasm_c_api/test-early-exit.c
+++ lib/c-api/tests/wasm_c_api/test-early-exit.c
@@ -3,7 +3,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-#include "../../wasmer_wasm.h"
+#include "wasmer_wasm.h"
 
 #define own
 
--- lib/c-api/tests/wasm_c_api/test-wasi.c
+++ lib/c-api/tests/wasm_c_api/test-wasi.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <inttypes.h>
 
-#include "../../wasmer_wasm.h"
+#include "wasmer_wasm.h"
 
 #define BUF_SIZE 128
 #define own
