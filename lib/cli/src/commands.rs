65265dbd73c01c8660ed79b570ebef9de8e07a2c
Mark McCaskey <mark@wasmer.io>
2020-10-08 14:13:54 -0700

    Add first draft of `wasmer create-exe`

--- lib/cli/src/commands.rs
+++ lib/cli/src/commands.rs
@@ -2,6 +2,8 @@
 mod cache;
 mod compile;
 mod config;
+#[cfg(feature = "object-file")]
+mod create_exe;
 mod inspect;
 mod run;
 mod self_update;
@@ -9,6 +11,8 @@ mod validate;
 #[cfg(feature = "wast")]
 mod wast;
 
+#[cfg(feature = "object-file")]
+pub use create_exe::*;
 #[cfg(feature = "wast")]
 pub use wast::*;
 pub use {cache::*, compile::*, config::*, inspect::*, run::*, self_update::*, validate::*};
