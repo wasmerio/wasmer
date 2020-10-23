2452b9a604447d4fdf1b5c5ece67c823aeefc0a7
jubianchi <julien@wasmer.io>
2020-10-23 22:39:33 +0200

    chore(doc): Add the Wasmer logo to the generated API documentation

--- lib/emscripten/src/lib.rs
+++ lib/emscripten/src/lib.rs
@@ -8,7 +8,7 @@
     unreachable_patterns
 )]
 #![doc(html_favicon_url = "https://wasmer.io/static/icons/favicon.ico")]
-#![doc(html_logo_url = "https://avatars3.githubusercontent.com/u/44205449?s=200&v=4")]
+#![doc(html_logo_url = "https://github.com/wasmerio.png?size=200")]
 
 #[macro_use]
 extern crate log;
