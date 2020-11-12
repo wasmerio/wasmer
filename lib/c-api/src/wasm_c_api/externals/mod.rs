6f2957b4b364c0c664d66e40588282ad5e95a785
Ivan Enderlin <ivan@mnt.io>
2020-11-12 15:56:17 +0100

    feat(c-api) More functions handle null pointers.

--- lib/c-api/src/wasm_c_api/externals/mod.rs
+++ lib/c-api/src/wasm_c_api/externals/mod.rs
@@ -22,10 +22,10 @@ wasm_declare_boxed_vec!(extern);
 
 #[no_mangle]
 pub unsafe extern "C" fn wasm_func_as_extern(
-    func_ptr: Option<NonNull<wasm_func_t>>,
+    func: Option<NonNull<wasm_func_t>>,
 ) -> Option<Box<wasm_extern_t>> {
-    let func_ptr = func_ptr?;
-    let func = func_ptr.as_ref();
+    let func = func?;
+    let func = func.as_ref();
 
     Some(Box::new(wasm_extern_t {
         instance: func.instance.clone(),
@@ -35,13 +35,13 @@ pub unsafe extern "C" fn wasm_func_as_extern(
 
 #[no_mangle]
 pub unsafe extern "C" fn wasm_global_as_extern(
-    global_ptr: Option<NonNull<wasm_global_t>>,
+    global: Option<NonNull<wasm_global_t>>,
 ) -> Option<Box<wasm_extern_t>> {
-    let global_ptr = global_ptr?;
-    let global = global_ptr.as_ref();
+    let global = global?;
+    let global = global.as_ref();
 
     Some(Box::new(wasm_extern_t {
-        // update this if global does hold onto an `instance`
+        // TODO: update this if global does hold onto an `instance`
         instance: None,
         inner: Extern::Global(global.inner.clone()),
     }))
@@ -49,13 +49,13 @@ pub unsafe extern "C" fn wasm_global_as_extern(
 
 #[no_mangle]
 pub unsafe extern "C" fn wasm_memory_as_extern(
-    memory_ptr: Option<NonNull<wasm_memory_t>>,
+    memory: Option<NonNull<wasm_memory_t>>,
 ) -> Option<Box<wasm_extern_t>> {
-    let memory_ptr = memory_ptr?;
-    let memory = memory_ptr.as_ref();
+    let memory = memory?;
+    let memory = memory.as_ref();
 
     Some(Box::new(wasm_extern_t {
-        // update this if global does hold onto an `instance`
+        // TODO: update this if global does hold onto an `instance`
         instance: None,
         inner: Extern::Memory(memory.inner.clone()),
     }))
@@ -63,13 +63,13 @@ pub unsafe extern "C" fn wasm_memory_as_extern(
 
 #[no_mangle]
 pub unsafe extern "C" fn wasm_table_as_extern(
-    table_ptr: Option<NonNull<wasm_table_t>>,
+    table: Option<NonNull<wasm_table_t>>,
 ) -> Option<Box<wasm_extern_t>> {
-    let table_ptr = table_ptr?;
-    let table = table_ptr.as_ref();
+    let table = table?;
+    let table = table.as_ref();
 
     Some(Box::new(wasm_extern_t {
-        // update this if global does hold onto an `instance`
+        // TODO: update this if global does hold onto an `instance`
         instance: None,
         inner: Extern::Table(table.inner.clone()),
     }))
@@ -77,10 +77,11 @@ pub unsafe extern "C" fn wasm_table_as_extern(
 
 #[no_mangle]
 pub unsafe extern "C" fn wasm_extern_as_func(
-    extern_ptr: Option<NonNull<wasm_extern_t>>,
+    r#extern: Option<NonNull<wasm_extern_t>>,
 ) -> Option<Box<wasm_func_t>> {
-    let extern_ptr = extern_ptr?;
-    let r#extern = extern_ptr.as_ref();
+    let r#extern = r#extern?;
+    let r#extern = r#extern.as_ref();
+
     if let Extern::Function(f) = &r#extern.inner {
         Some(Box::new(wasm_func_t {
             inner: f.clone(),
@@ -93,10 +94,11 @@ pub unsafe extern "C" fn wasm_extern_as_func(
 
 #[no_mangle]
 pub unsafe extern "C" fn wasm_extern_as_global(
-    extern_ptr: Option<NonNull<wasm_extern_t>>,
+    r#extern: Option<NonNull<wasm_extern_t>>,
 ) -> Option<Box<wasm_global_t>> {
-    let extern_ptr = extern_ptr?;
-    let r#extern = extern_ptr.as_ref();
+    let r#extern = r#extern?;
+    let r#extern = r#extern.as_ref();
+
     if let Extern::Global(g) = &r#extern.inner {
         Some(Box::new(wasm_global_t { inner: g.clone() }))
     } else {
@@ -106,10 +108,11 @@ pub unsafe extern "C" fn wasm_extern_as_global(
 
 #[no_mangle]
 pub unsafe extern "C" fn wasm_extern_as_memory(
-    extern_ptr: Option<NonNull<wasm_extern_t>>,
+    r#extern: Option<NonNull<wasm_extern_t>>,
 ) -> Option<Box<wasm_memory_t>> {
-    let extern_ptr = extern_ptr?;
-    let r#extern = extern_ptr.as_ref();
+    let r#extern = r#extern?;
+    let r#extern = r#extern.as_ref();
+
     if let Extern::Memory(m) = &r#extern.inner {
         Some(Box::new(wasm_memory_t { inner: m.clone() }))
     } else {
@@ -119,10 +122,11 @@ pub unsafe extern "C" fn wasm_extern_as_memory(
 
 #[no_mangle]
 pub unsafe extern "C" fn wasm_extern_as_table(
-    extern_ptr: Option<NonNull<wasm_extern_t>>,
+    r#extern: Option<NonNull<wasm_extern_t>>,
 ) -> Option<Box<wasm_table_t>> {
-    let extern_ptr = extern_ptr?;
-    let r#extern = extern_ptr.as_ref();
+    let r#extern = r#extern?;
+    let r#extern = r#extern.as_ref();
+
     if let Extern::Table(t) = &r#extern.inner {
         Some(Box::new(wasm_table_t { inner: t.clone() }))
     } else {
