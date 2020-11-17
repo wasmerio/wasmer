508a47882286d4bb4dfb93a045431020a38d8828
Nick Lewycky <nick@wasmer.io>
2020-11-17 02:24:50 -0800

    Add specializations for vec types and implement conversions for them.
    ExternType now always has an const wasm_externtype_t* which it doesn't own.

--- lib/c-api/wasm.cc
+++ lib/c-api/wasm.cc
@@ -62,15 +62,40 @@ ownvec<typename remove_own<S2>::type> c_vec_to_cxx_ownvec(C c_vec,
   return v;
 }
 
+// Conversion from a C wasm_##name##_vec_t* to a C++ vec<T>.
+template <typename C, typename S1, typename S2>
+vec<S2> c_vec_to_cxx_vec(C c_vec, S2 (*convert)(S1)) {
+  auto v = vec<S2>::make_uninitialized(c_vec->size);
+  for (int i = 0; i < c_vec->size; ++i) {
+    v[i] = convert(c_vec->data[i]);
+  }
+  return v;
+}
+
 template <typename T> struct c_vec;
-template <> struct c_vec<wasm_valtype_t> {
-  using type = wasm_valtype_vec_t;
-  static constexpr auto new_empty = wasm_valtype_vec_new_empty;
-  static constexpr auto new_uninitialized = wasm_valtype_vec_new_uninitialized;
-  static constexpr auto new_ = wasm_valtype_vec_new;
-  static constexpr auto copy = wasm_valtype_vec_copy;
-  static constexpr auto delete_ = wasm_valtype_vec_delete;
-};
+#define DEFINE_C_VEC_SPECIALIZATION(name)                                      \
+  template <> struct c_vec<wasm_##name##_t> {                                  \
+    using type = wasm_##name##_vec_t;                                          \
+    static constexpr auto new_empty = wasm_##name##_vec_new_empty;             \
+    static constexpr auto new_uninitialized =                                  \
+        wasm_##name##_vec_new_uninitialized;                                   \
+    static constexpr auto new_ = wasm_##name##_vec_new;                        \
+    static constexpr auto copy = wasm_##name##_vec_copy;                       \
+    static constexpr auto delete_ = wasm_##name##_vec_delete;                  \
+  };
+// This list should match all WASM_DECLARE_VEC(type) in wasm.h.
+DEFINE_C_VEC_SPECIALIZATION(byte)
+DEFINE_C_VEC_SPECIALIZATION(val)
+DEFINE_C_VEC_SPECIALIZATION(frame)
+DEFINE_C_VEC_SPECIALIZATION(extern)
+DEFINE_C_VEC_SPECIALIZATION(valtype)
+DEFINE_C_VEC_SPECIALIZATION(functype)
+DEFINE_C_VEC_SPECIALIZATION(globaltype)
+DEFINE_C_VEC_SPECIALIZATION(tabletype)
+DEFINE_C_VEC_SPECIALIZATION(memorytype)
+DEFINE_C_VEC_SPECIALIZATION(externtype)
+DEFINE_C_VEC_SPECIALIZATION(importtype)
+DEFINE_C_VEC_SPECIALIZATION(exporttype)
 
 template <typename T, typename S1, typename S2>
 typename c_vec<S2>::type cxx_ownvec_to_c_vec(ownvec<T> &&cxx_vec,
@@ -79,6 +104,16 @@ typename c_vec<S2>::type cxx_ownvec_to_c_vec(ownvec<T> &&cxx_vec,
   c_vec<S2>::new_uninitialized(&v, cxx_vec.size());
   for (int i = 0, e = cxx_vec.size(); i != e; ++i) {
     v.data[i] = convert(cxx_vec[i].release());
+  }
+  return v;
+}
+
+template <typename T, typename S1, typename S2>
+typename c_vec<S2>::type cxx_vec_to_c_vec(vec<T> &&cxx_vec, S2 (*convert)(S1)) {
+  typename c_vec<S2>::type v;
+  c_vec<S2>::new_uninitialized(&v, cxx_vec.size());
+  for (int i = 0, e = cxx_vec.size(); i != e; ++i) {
+    v.data[i] = convert(cxx_vec[i]);
   }
   return v;
 }
@@ -224,15 +259,30 @@ ExternKind c_externkind_to_cxx_externkind(wasm_externkind_t kind) {
 class WasmerExternType;
 
 namespace {
-struct ExternKindWrapper {
-  ExternKind kind;
+struct ExternTypeWrapper {
+  wasm_externkind_t c_kind() const { return wasm_externtype_kind(externtype); }
+  const wasm_functype_t *func() const {
+    return wasm_externtype_as_functype_const(externtype);
+  }
+  const wasm_globaltype_t *global() const {
+    return wasm_externtype_as_globaltype_const(externtype);
+  }
+  const wasm_tabletype_t *table() const {
+    return wasm_externtype_as_tabletype_const(externtype);
+  }
+  const wasm_memorytype_t *memory() const {
+    return wasm_externtype_as_memorytype_const(externtype);
+  }
+
+  const wasm_externtype_t *externtype;
 };
 } // namespace
 
 template <typename T, typename Derived>
 struct WasmerExternTypeImpl : public From<T, Derived>,
-                              public ExternKindWrapper {
-  WasmerExternTypeImpl(ExternKind kind) : ExternKindWrapper{kind} {}
+                              public ExternTypeWrapper {
+  WasmerExternTypeImpl(const wasm_externtype_t *externtype)
+      : ExternTypeWrapper{externtype} {}
 };
 
 static own<ValType> c_valtype_to_cxx_ownvaltype(const wasm_valtype_t *c) {
@@ -250,7 +300,8 @@ static wasm_valtype_t *cxx_valtype_to_c_valtype(const ValType *val_type) {
 class WasmerFuncType : public WasmerExternTypeImpl<FuncType, WasmerFuncType> {
 public:
   explicit WasmerFuncType(c_own<const wasm_functype_t> &&functype)
-      : WasmerExternTypeImpl(ExternKind::FUNC), functype(std::move(functype)),
+      : WasmerExternTypeImpl(wasm_functype_as_externtype_const(functype.get())),
+        functype(std::move(functype)),
         params_(c_vec_to_cxx_ownvec(wasm_functype_params(functype.get()),
                                     c_valtype_to_cxx_ownvaltype)),
         results_(c_vec_to_cxx_ownvec(wasm_functype_results(functype.get()),
@@ -320,17 +371,11 @@ class WASM_API_EXTERN WasmerGlobalType
     : public WasmerExternTypeImpl<GlobalType, WasmerGlobalType> {
 public:
   explicit WasmerGlobalType(c_own<const wasm_globaltype_t> &&globaltype)
-      : WasmerExternTypeImpl(ExternKind::GLOBAL),
+      : WasmerExternTypeImpl(
+            wasm_globaltype_as_externtype_const(globaltype.get())),
         globaltype(std::move(globaltype)),
         valtype(c_valtype_to_cxx_ownvaltype(
             wasm_globaltype_content(globaltype.get()))) {}
-
-  explicit WasmerGlobalType(own<ValType> &&valtype, Mutability mutability)
-      : WasmerExternTypeImpl(ExternKind::GLOBAL),
-        globaltype(
-            wasm_globaltype_new(cxx_valtype_to_c_valtype(valtype.get()),
-                                cxx_mutability_to_c_mutability(mutability))),
-        valtype(std::move(valtype)) {}
 
   auto copy() const -> own<GlobalType> {
     return make_own(new WasmerGlobalType(
@@ -352,7 +397,10 @@ void GlobalType::destroy() { delete WasmerGlobalType::from(this); }
 
 auto GlobalType::make(own<ValType> &&valtype, Mutability mutability)
     -> own<GlobalType> {
-  return make_own(new WasmerGlobalType(std::move(valtype), mutability));
+  auto element = cxx_valtype_to_c_valtype(valtype.get());
+  auto cxx_mutability = cxx_mutability_to_c_mutability(mutability);
+  return make_own(new WasmerGlobalType(
+      make_c_own(wasm_globaltype_new(element, cxx_mutability))));
 }
 
 auto GlobalType::copy() const -> own<GlobalType> {
@@ -377,7 +425,8 @@ class WASM_API_EXTERN WasmerTableType
     : public WasmerExternTypeImpl<TableType, WasmerTableType> {
 public:
   explicit WasmerTableType(c_own<const wasm_tabletype_t> &&tabletype)
-      : WasmerExternTypeImpl(ExternKind::TABLE),
+      : WasmerExternTypeImpl(
+            wasm_tabletype_as_externtype_const(tabletype.get())),
         tabletype(std::move(tabletype)),
         element_(c_valtype_to_cxx_ownvaltype(
             wasm_tabletype_element(tabletype.get()))),
@@ -405,13 +454,61 @@ auto TableType::make(own<ValType> &&element, Limits limits) -> own<TableType> {
   return WasmerTableType::make(std::move(element), limits);
 }
 
-// TODO: memory
+class WASM_API_EXTERN WasmerMemoryType
+    : public From<MemoryType, WasmerMemoryType> {
+public:
+  explicit WasmerMemoryType(c_own<const wasm_memorytype_t> memorytype)
+      : memorytype(std::move(memorytype)) {}
+
+  static auto make(Limits limits) -> own<MemoryType> {
+    auto l = cxx_limits_to_c_limits(limits);
+    return make_own(new WasmerMemoryType(make_c_own(wasm_memorytype_new(&l))));
+  }
+  auto copy() const -> own<MemoryType> {
+    return make_own(new WasmerMemoryType(
+        make_c_own(wasm_memorytype_copy(memorytype.get()))));
+  }
+
+  auto limits() const -> const Limits &;
+
+  c_own<const wasm_memorytype_t> memorytype;
+};
+
+auto MemoryType::make(Limits limits) -> own<MemoryType> {
+  return WasmerMemoryType::make(limits);
+}
+auto MemoryType::copy() const -> own<MemoryType> {
+  return WasmerMemoryType::from(this)->copy();
+}
+
+auto MemoryType::limits() const -> const Limits & {
+  return WasmerMemoryType::from(this)->limits();
+}
 
 class WASM_API_EXTERN WasmerExternType
-    : public From<ExternType, WasmerExternType> {
+    : public WasmerExternTypeImpl<ExternType, WasmerExternType> {
+protected:
+  explicit WasmerExternType(const wasm_externtype_t *externtype)
+      : WasmerExternTypeImpl(externtype) {}
+
 public:
-  explicit WasmerExternType(wasm_externtype_t *externtype)
-      : externtype(externtype) {}
+  static auto make(const wasm_externtype_t *externtype) -> own<ExternType> {
+    auto owned_externtype = wasm_externtype_copy(externtype);
+    switch (wasm_externtype_kind(owned_externtype)) {
+    case WASM_EXTERN_FUNC:
+      return make_own(new WasmerFuncType(
+          make_c_own(wasm_externtype_as_functype_const(owned_externtype))));
+    case WASM_EXTERN_GLOBAL:
+      return make_own(new WasmerGlobalType(
+          make_c_own(wasm_externtype_as_globaltype_const(owned_externtype))));
+    case WASM_EXTERN_TABLE:
+      return make_own(new WasmerTableType(
+          make_c_own(wasm_externtype_as_tabletype_const(owned_externtype))));
+    case WASM_EXTERN_MEMORY:
+      return make_own(new WasmerMemoryType(
+          make_c_own(wasm_externtype_as_memorytype_const(owned_externtype))));
+    }
+  }
 
   auto copy() const -> own<ExternType> {
     return make_own(new WasmerExternType(wasm_externtype_copy(externtype)));
@@ -421,24 +518,51 @@ public:
     return c_externkind_to_cxx_externkind(wasm_externtype_kind(externtype));
   }
 
-  auto func() -> FuncType *;
-  auto global() -> GlobalType *;
-  auto table() -> TableType *;
-  auto memory() -> MemoryType *;
-
-  auto func() const -> const FuncType *;
-  auto global() const -> const GlobalType *;
-  auto table() const -> const TableType *;
-  auto memory() const -> const MemoryType *;
-
-  static const WasmerExternType *from(const ExternType *base) {
-    return static_cast<const WasmerExternType *>(base);
+  auto func() -> FuncType * {
+    return kind() == ExternKind::FUNC
+               ? static_cast<FuncType *>(static_cast<ExternType *>(this))
+               : nullptr;
   }
-  static WasmerExternType *from(ExternType *base) {
-    return static_cast<WasmerExternType *>(base);
+  auto global() -> GlobalType * {
+    return kind() == ExternKind::GLOBAL
+               ? static_cast<GlobalType *>(static_cast<ExternType *>(this))
+               : nullptr;
+  }
+  auto table() -> TableType * {
+    return kind() == ExternKind::TABLE
+               ? static_cast<TableType *>(static_cast<ExternType *>(this))
+               : nullptr;
+  }
+  auto memory() -> MemoryType * {
+    return kind() == ExternKind::FUNC
+               ? static_cast<MemoryType *>(static_cast<ExternType *>(this))
+               : nullptr;
   }
 
-  wasm_externtype_t *externtype;
+  auto func() const -> const FuncType * {
+    return kind() == ExternKind::FUNC
+               ? static_cast<const FuncType *>(
+                     static_cast<const ExternType *>(this))
+               : nullptr;
+  }
+  auto global() const -> const GlobalType * {
+    return kind() == ExternKind::GLOBAL
+               ? static_cast<const GlobalType *>(
+                     static_cast<const ExternType *>(this))
+               : nullptr;
+  }
+  auto table() const -> const TableType * {
+    return kind() == ExternKind::TABLE
+               ? static_cast<const TableType *>(
+                     static_cast<const ExternType *>(this))
+               : nullptr;
+  }
+  auto memory() const -> const MemoryType * {
+    return kind() == ExternKind::FUNC
+               ? static_cast<const MemoryType *>(
+                     static_cast<const ExternType *>(this))
+               : nullptr;
+  }
 };
 
 void ExternType::destroy() { delete WasmerExternType::from(this); }
@@ -476,3 +600,125 @@ auto ExternType::table() const -> const TableType * {
 auto ExternType::memory() const -> const MemoryType * {
   return WasmerExternType::from(this)->memory();
 }
+
+class WASM_API_EXTERN WasmerImportType
+    : public From<ImportType, WasmerImportType> {
+public:
+  explicit WasmerImportType(c_own<const wasm_importtype_t> &&importtype)
+      : importtype(std::move(importtype)),
+        externtype(
+            WasmerExternType::make(wasm_importtype_type(importtype.get()))),
+        module_(c_vec_to_cxx_vec(wasm_importtype_module(importtype.get()),
+                                 identity)),
+        name_(c_vec_to_cxx_vec(wasm_importtype_name(importtype.get()),
+                               identity)) {}
+
+  static byte_t identity(byte_t x) { return x; }
+
+  static auto make(Name &&module, Name &&name, own<ExternType> &&externtype)
+      -> own<ImportType> {
+    auto c_module = cxx_vec_to_c_vec(std::move(module), identity);
+    auto c_name = cxx_vec_to_c_vec(std::move(name), identity);
+    auto c_externtype = wasm_externtype_copy(
+        WasmerExternType::from(externtype.get())->externtype);
+    return make_own(new WasmerImportType(
+        make_c_own(wasm_importtype_new(&c_module, &c_name, c_externtype))));
+  }
+  auto copy() const -> own<ImportType> {
+    return make_own(new WasmerImportType(
+        make_c_own(wasm_importtype_copy(importtype.get()))));
+  }
+
+  auto module() const -> const Name & { return module_; }
+  auto name() const -> const Name & { return name_; }
+  auto type() const -> const ExternType * { return externtype.get(); }
+
+  c_own<const wasm_importtype_t> importtype;
+  own<ExternType> externtype;
+  Name module_;
+  Name name_;
+};
+
+void ImportType::destroy() { delete WasmerImportType::from(this); }
+
+auto ImportType::copy() const -> own<ImportType> {
+  return WasmerImportType::from(this)->copy();
+}
+
+auto ImportType::module() const -> const Name & {
+  return WasmerImportType::from(this)->module();
+}
+
+auto ImportType::name() const -> const Name & {
+  return WasmerImportType::from(this)->name();
+}
+
+auto ImportType::type() const -> const ExternType * {
+  return WasmerImportType::from(this)->type();
+}
+
+class WASM_API_EXTERN WasmerExportType
+    : public From<ExportType, WasmerExportType> {
+public:
+  explicit WasmerExportType(c_own<const wasm_exporttype_t> &&exporttype)
+      : exporttype(std::move(exporttype)),
+        name_(
+            c_vec_to_cxx_vec(wasm_exporttype_name(exporttype.get()), identity)),
+        externtype(
+            WasmerExternType::make(wasm_exporttype_type(exporttype.get()))) {}
+
+  static byte_t identity(byte_t x) { return x; }
+
+  static auto make(Name &&name, own<ExternType> &&externtype)
+      -> own<ExportType> {
+    auto c_name = cxx_vec_to_c_vec(std::move(name), identity);
+    auto c_externtype = wasm_externtype_copy(
+        WasmerExternType::from(externtype.get())->externtype);
+    return make_own(new WasmerExportType(
+        make_c_own(wasm_exporttype_new(&c_name, c_externtype))));
+  }
+
+  auto copy() const -> own<ExportType> {
+    return make_own(new WasmerExportType(
+        make_c_own(wasm_exporttype_copy(exporttype.get()))));
+  }
+
+  auto name() const -> const Name & { return name_; }
+  auto type() const -> const ExternType * { return externtype.get(); }
+
+  c_own<const wasm_exporttype_t> exporttype;
+  Name name_;
+  own<ExternType> externtype;
+};
+
+void ExportType::destroy() { delete WasmerExportType::from(this); }
+
+auto ExportType::make(Name &&name, own<ExternType> &&externtype)
+    -> own<ExportType> {
+  return WasmerExportType::make(std::move(name), std::move(externtype));
+}
+auto ExportType::copy() const -> own<ExportType> {
+  return WasmerExportType::from(this)->copy();
+}
+
+auto ExportType::name() const -> const Name & {
+  return WasmerExportType::from(this)->name();
+}
+auto ExportType::type() const -> const ExternType * {
+  return WasmerExportType::from(this)->type();
+}
+
+namespace {
+struct WasmerRefWrapper {};
+} // namespace
+
+class WASM_API_EXTERN WasmerRef : From<Ref, WasmerRef> {
+public:
+  auto copy() const -> own<Ref>;
+  auto same(const Ref *) const -> bool;
+
+  auto get_host_info() const -> void *;
+  void set_host_info(void *info, void (*finalizer)(void *) = nullptr);
+};
+
+void Ref::destroy() { delete WasmerRef::from(this); }
