5bc8c0a21ba4905a8794cb7786217a43d3b1b3f4
jubianchi <julien@wasmer.io>
2020-11-23 12:13:07 +0100

    chore: Add C examples

--- lib/c-api/src/wasm_c_api/externals/global.rs
+++ lib/c-api/src/wasm_c_api/externals/global.rs
@@ -78,3 +78,77 @@ pub unsafe extern "C" fn wasm_global_same(
 pub extern "C" fn wasm_global_type(global: &wasm_global_t) -> Box<wasm_globaltype_t> {
     Box::new(wasm_globaltype_t::new(global.inner.ty().clone()))
 }
+
+#[cfg(test)]
+mod tests {
+    use inline_c::assert_c;
+
+    #[test]
+    fn test_set_host_global_immutable() {
+        (assert_c! {
+            #include "tests/wasmer_wasm.h"
+
+            int main() {
+                wasm_engine_t* engine = wasm_engine_new();
+                wasm_store_t* store = wasm_store_new(engine);
+
+                wasm_val_t forty_two = WASM_F32_VAL(42);
+                wasm_val_t forty_three = WASM_F32_VAL(43);
+
+                wasm_valtype_t* type = wasm_valtype_new_i32();
+                wasm_globaltype_t* global_type = wasm_globaltype_new(type, WASM_CONST);
+                wasm_global_t* global = wasm_global_new(store, global_type, &forty_two);
+
+                wasm_global_set(global, &forty_three);
+
+                assert(wasmer_last_error_length() > 0);
+
+                wasm_globaltype_delete(global_type);
+                wasm_valtype_delete(type);
+                wasm_store_delete(store);
+                wasm_engine_delete(engine);
+
+                return 0;
+            }
+        })
+        .success();
+    }
+
+    #[test]
+    fn test_set_guest_global_immutable() {
+        (assert_c! {
+            #include "tests/wasmer_wasm.h"
+
+            int main() {
+                wasm_engine_t* engine = wasm_engine_new();
+                wasm_store_t* store = wasm_store_new(engine);
+
+                wasm_byte_vec_t wat;
+                wasmer_byte_vec_new_from_string(&wat, "(module (global $global (export \"global\") f32 (f32.const 1)))");
+                wasm_byte_vec_t* wasm_bytes = wat2wasm(&wat);
+                wasm_module_t* module = wasm_module_new(store, wasm_bytes);
+                wasm_extern_vec_t import_object = WASM_EMPTY_VEC;
+                wasm_instance_t* instance = wasm_instance_new(store, module, &import_object, NULL);
+
+                wasm_extern_vec_t exports;
+                wasm_instance_exports(instance, &exports);
+                wasm_global_t* global = wasm_extern_as_global(exports.data[0]);
+
+                wasm_val_t forty_two = WASM_F32_VAL(42);
+                wasm_global_set(global, &forty_two);
+
+                printf("%d", wasmer_last_error_length());
+                assert(wasmer_last_error_length() > 0);
+
+                wasm_instance_delete(instance);
+                wasm_byte_vec_delete(wasm_bytes);
+                wasm_byte_vec_delete(&wat);
+                wasm_store_delete(store);
+                wasm_engine_delete(engine);
+
+                return 0;
+            }
+        })
+        .success();
+    }
+}
