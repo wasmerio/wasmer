6bd09d47e24a543f5065b091171180b863c4c731
Brooks J Rady <b.j.rady@gmail.com>
2020-11-25 22:59:14 +0000

    Also export the HostFile!

--- lib/wasi/src/lib.rs
+++ lib/wasi/src/lib.rs
@@ -22,8 +22,8 @@ mod utils;
 use crate::syscalls::*;
 
 pub use crate::state::{
-    Fd, Pipe, Stderr, Stdin, Stdout, WasiFile, WasiFs, WasiFsError, WasiState, WasiStateBuilder,
-    WasiStateCreationError, ALL_RIGHTS, VIRTUAL_ROOT_FD,
+    Fd, HostFile, Pipe, Stderr, Stdin, Stdout, WasiFile, WasiFs, WasiFsError, WasiState,
+    WasiStateBuilder, WasiStateCreationError, ALL_RIGHTS, VIRTUAL_ROOT_FD,
 };
 pub use crate::syscalls::types;
 pub use crate::utils::{get_wasi_version, is_wasi_module, WasiVersion};
