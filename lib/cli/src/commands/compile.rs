22fea8efd7be7ff6004a6ccdb4d595c4bb671db2
Nick Lewycky <nick@wasmer.io>
2020-12-01 23:22:55 -0800

    Disable native and object-file engines for now.
    Fix build break exposed by trying to build wasmer with one engine.

--- lib/cli/src/commands/compile.rs
+++ lib/cli/src/commands/compile.rs
@@ -41,20 +41,25 @@ impl Compile {
     pub(crate) fn get_recommend_extension(
         engine_type: &EngineType,
         target_triple: &Triple,
-    ) -> &'static str {
+    ) -> Result<&'static str> {
         match engine_type {
             #[cfg(feature = "native")]
-            EngineType::Native => {
-                wasmer_engine_native::NativeArtifact::get_default_extension(target_triple)
-            }
+            EngineType::Native => Ok(wasmer_engine_native::NativeArtifact::get_default_extension(
+                target_triple,
+            )),
             #[cfg(feature = "jit")]
-            EngineType::JIT => wasmer_engine_jit::JITArtifact::get_default_extension(target_triple),
+            EngineType::JIT => Ok(wasmer_engine_jit::JITArtifact::get_default_extension(
+                target_triple,
+            )),
             #[cfg(feature = "object-file")]
-            EngineType::ObjectFile => {
-                wasmer_engine_object_file::ObjectFileArtifact::get_default_extension(target_triple)
-            }
+            EngineType::ObjectFile => Ok(
+                wasmer_engine_object_file::ObjectFileArtifact::get_default_extension(target_triple),
+            ),
             #[cfg(not(all(feature = "native", feature = "jit", feature = "object-file")))]
-            _ => bail!("selected engine type is not compiled in"),
+            engine => bail!(
+                "The `{}` engine is not included in this binary.",
+                engine.to_string()
+            ),
         }
     }
 
@@ -81,7 +86,7 @@ impl Compile {
             .file_stem()
             .map(|osstr| osstr.to_string_lossy().to_string())
             .unwrap_or_default();
-        let recommended_extension = Self::get_recommend_extension(&engine_type, target.triple());
+        let recommended_extension = Self::get_recommend_extension(&engine_type, target.triple())?;
         match self.output.extension() {
             Some(ext) => {
                 if ext != recommended_extension {
