2e91cce61398a5052116a0e34be349491a9226c7
Brooks J Rady <b.j.rady@gmail.com>
2020-12-03 13:01:34 +0000

    Start what will eventually be an example

---
+++ tests/wasi-wast/wasi/tests/pipe_reverse.rs
@@ -0,0 +1,10 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+
+    let output: String = input.chars().rev().collect();
+
+    println!("{}", output);
+}
