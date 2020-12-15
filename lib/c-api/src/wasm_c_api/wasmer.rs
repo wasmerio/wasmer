778681e8a0fad45a06e70620e7d2a531316d24c7
Ivan Enderlin <ivan@mnt.io>
2020-12-15 16:26:29 +0100

    test+doc(c-api) Test and document the `wasmer_module_*_name` functions.

--- lib/c-api/src/wasm_c_api/wasmer.rs
+++ lib/c-api/src/wasm_c_api/wasmer.rs
@@ -1,10 +1,57 @@
-//! Wasmer-specific extensions to the Wasm C API.
+//! Non-standard Wasmer-specific extensions to the Wasm C API.
 
 use super::module::wasm_module_t;
 use super::types::wasm_name_t;
 use std::str;
 use std::sync::Arc;
 
+/// Non-standard Wasmer-specific API to get the module's name,
+/// otherwise `out` is untouched.
+///
+/// # Example
+///
+/// ```rust
+/// # use inline_c::assert_c;
+/// # fn main() {
+/// #    (assert_c! {
+/// # #include "tests/wasmer_wasm.h"
+/// #
+/// int main() {
+///     // Create the engine and the store.
+///     wasm_engine_t* engine = wasm_engine_new();
+///     wasm_store_t* store = wasm_store_new(engine);
+///
+///     // Create a WebAssembly module from a WAT definition.
+///     wasm_byte_vec_t wat;
+///     wasmer_byte_vec_new_from_string(&wat, "(module $moduleName)");
+///     //                                             ^~~~~~~~~~~ that's the name!
+///     wasm_byte_vec_t wasm;
+///     wat2wasm(&wat, &wasm);
+///
+///     // Create the module.
+///     wasm_module_t* module = wasm_module_new(store, &wasm);
+///
+///     // Read the module's name.
+///     wasm_name_t name;
+///     wasm_module_name(module, &name);
+///
+///     // It works!
+///     wasmer_assert_name(&name, "moduleName");
+///
+///     // Free everything.
+///     wasm_byte_vec_delete(&name);
+///     wasm_module_delete(module);
+///     wasm_byte_vec_delete(&wasm);
+///     wasm_byte_vec_delete(&wat);
+///     wasm_store_delete(store);
+///     wasm_engine_delete(engine);
+///
+///     return 0;
+/// }
+/// #    })
+/// #    .success();
+/// # }
+/// ```
 #[no_mangle]
 pub unsafe extern "C" fn wasm_module_name(
     module: &wasm_module_t,
@@ -19,11 +66,77 @@ pub unsafe extern "C" fn wasm_module_name(
     *out = name.as_bytes().to_vec().into();
 }
 
+/// Non-standard Wasmer-specific API to set the module's name. The
+/// function returns `true` if the name has been updated, `false`
+/// otherwise.
+///
+/// # Example
+///
+/// ```rust
+/// # use inline_c::assert_c;
+/// # fn main() {
+/// #    (assert_c! {
+/// # #include "tests/wasmer_wasm.h"
+/// #
+/// int main() {
+///     // Create the engine and the store.
+///     wasm_engine_t* engine = wasm_engine_new();
+///     wasm_store_t* store = wasm_store_new(engine);
+///
+///     // Create a WebAssembly module from a WAT definition.
+///     wasm_byte_vec_t wat;
+///     wasmer_byte_vec_new_from_string(&wat, "(module)");
+///     wasm_byte_vec_t wasm;
+///     wat2wasm(&wat, &wasm);
+///
+///     // Create the module.
+///     wasm_module_t* module = wasm_module_new(store, &wasm);
+///
+///     // Read the module's name. There is none for the moment.
+///     {
+///         wasm_name_t name;
+///         name.size = 0;
+///         wasm_module_name(module, &name);
+///
+///         assert(name.size == 0);
+///     }
+///
+///     // So, let's set a new name.
+///     {
+///         wasm_name_t name;
+///         wasmer_byte_vec_new_from_string(&name, "hello");
+///         wasm_module_set_name(module, &name);
+///     }
+///
+///     // And now, let's see the new name.
+///     {
+///         wasm_name_t name;
+///         wasm_module_name(module, &name);
+///
+///         // It works!
+///         wasmer_assert_name(&name, "hello");
+///
+///         wasm_byte_vec_delete(&name);
+///     }
+///
+///     // Free everything.
+///     wasm_module_delete(module);
+///     wasm_byte_vec_delete(&wasm);
+///     wasm_byte_vec_delete(&wat);
+///     wasm_store_delete(store);
+///     wasm_engine_delete(engine);
+///
+///     return 0;
+/// }
+/// #    })
+/// #    .success();
+/// # }
+/// ```
 #[no_mangle]
 pub unsafe extern "C" fn wasm_module_set_name(
     module: &mut wasm_module_t,
     // own
-    name: Box<wasm_name_t>,
+    name: &wasm_name_t,
 ) -> bool {
     let name = match name.into_slice() {
         Some(name) => match str::from_utf8(name) {
