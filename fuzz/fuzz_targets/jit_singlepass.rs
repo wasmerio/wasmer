fe8a94d5312e87887bf4d81aae161b1ada0ab9d5
Nick Lewycky <nick@wasmer.io>
2021-03-15 12:46:56 -0700

    Singlepass doesn't support multi-value returns, ignore those errors.

--- fuzz/fuzz_targets/jit_singlepass.rs
+++ fuzz/fuzz_targets/jit_singlepass.rs
@@ -1,15 +1,25 @@
 #![no_main]
 
 use libfuzzer_sys::fuzz_target;
+use wasm_smith::Module as FuzzModule;
 use wasmer::{imports, Instance, Module, Store};
 use wasmer_compiler_singlepass::Singlepass;
 use wasmer_engine_jit::JIT;
-use wasm_smith::Module as FuzzModule;
 
 fuzz_target!(|module: FuzzModule| {
     let wasm_bytes = module.to_bytes();
     let compiler = Singlepass::default();
     let store = Store::new(&JIT::new(compiler).engine());
-    let module = Module::new(&store, &wasm_bytes).unwrap();
-    Instance::new(&module, &imports! {}).unwrap();
+    let module = Module::new(&store, &wasm_bytes);
+    let module = match module {
+        Ok(m) => m,
+        Err(e) => {
+            let error_message = format!("{}", e);
+            if error_message.contains("Validation error: invalid result arity: func type returns multiple values") || error_message.contains("Validation error: blocks, loops, and ifs accept no parameters when multi-value is not enabled") {
+                return;
+            }
+            panic!("{}", e);
+        }
+    };
+    let _ignored = Instance::new(&module, &imports! {});
 });
