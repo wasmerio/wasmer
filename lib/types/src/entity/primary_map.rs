0435c3e211cd609785f20f53f85b26c685f570b7
Syrus Akbary <me@syrusakbary.com>
2021-04-29 15:20:52 -0700

    Make primarymap rkyv encoding the derived one

--- lib/types/src/entity/primary_map.rs
+++ lib/types/src/entity/primary_map.rs
@@ -13,6 +13,8 @@ use crate::lib::std::ops::{Index, IndexMut};
 use crate::lib::std::slice;
 use crate::lib::std::vec::Vec;
 use loupe::{MemoryUsage, MemoryUsageTracker};
+#[cfg(feature = "enable-rkyv")]
+use rkyv::{Archive, Deserialize as RkyvDeserialize, Serialize as RkyvSerialize};
 #[cfg(feature = "enable-serde")]
 use serde::{Deserialize, Serialize};
 use std::mem;
@@ -34,6 +36,10 @@ use std::mem;
 /// `into_boxed_slice`.
 #[derive(Debug, Clone, Hash, PartialEq, Eq)]
 #[cfg_attr(feature = "enable-serde", derive(Serialize, Deserialize))]
+#[cfg_attr(
+    feature = "enable-rkyv",
+    derive(RkyvSerialize, RkyvDeserialize, Archive)
+)]
 pub struct PrimaryMap<K, V>
 where
     K: EntityRef,
