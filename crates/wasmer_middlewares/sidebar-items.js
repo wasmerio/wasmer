d8539d24d085e0002d772c6b6e64a1073a69ce09
syrusakbary <syrusakbary@users.noreply.github.com>
2021-06-07 16:24:06 +0000

    deploy: 0ca884d62dda82e18fc3744dc8cbe3bbf626abfd

--- crates/wasmer_middlewares/sidebar-items.js
+++ crates/wasmer_middlewares/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"mod":[["metering","`metering` is a middleware for tracking how many operators are executed in total and putting a limit on the total number of operators executed."]]});
+initSidebarItems({"mod":[["metering","`metering` is a middleware for tracking how many operators are executed in total and putting a limit on the total number of operators executed. The WebAssemblt instance execution is stopped when the limit is reached."]]});
