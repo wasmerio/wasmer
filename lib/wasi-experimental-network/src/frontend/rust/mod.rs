0e0c3ed1939a3de653b787bf171a2dc7b513b8d1
Ivan Enderlin <ivan@mnt.io>
2021-07-01 11:53:59 +0200

    !draft The Rust frontend fallbacks to `std::net` if WASI is not targeted.

--- lib/wasi-experimental-network/src/frontend/rust/mod.rs
+++ lib/wasi-experimental-network/src/frontend/rust/mod.rs
@@ -1,5 +1,11 @@
-mod c;
-mod socketaddr;
-mod tcp;
+cfg_if::cfg_if! {
+    if #[cfg(target_os = "wasi")] {
+        mod c;
+        mod socketaddr;
+        mod tcp;
 
-pub use tcp::TcpListener;
+        pub use tcp::{TcpListener, TcpStream, Incoming};
+    } else {
+        pub use std::net::{TcpListener, TcpStream, Incoming};
+    }
+}
