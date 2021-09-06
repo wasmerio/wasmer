8596a9fa1141debc8f3dec92256b9d4552183bab
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2021-09-06 18:10:07 +0200

    wip, remove_dir bug
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- tests/wasi-wast/wasi/tests/fd_rename_path.rs
+++ tests/wasi-wast/wasi/tests/fd_rename_path.rs
@@ -2,11 +2,13 @@
 // dir: test_fs
 
 use std::fs;
+use std::path::PathBuf;
 
 fn main() {
-    let old_path = "test_fs/oldname";
-    let new_path = "test_fs/newname";
+    let old_path = PathBuf::from("test_fs/wasitests/dirtorename");
+    let new_path = PathBuf::from("test_fs/wasitests/dirrenamed");
+    fs::create_dir_all(&old_path).expect("cannot create the directory");
 
-    assert!(fs::rename(old_path, new_path).is_ok());
-    assert!(fs::rename(new_path, old_path).is_ok());
+    fs::rename(old_path, &new_path).expect("cannot rename the directory");
+    fs::remove_dir(&new_path).expect("cannot remove the directory");
 }
