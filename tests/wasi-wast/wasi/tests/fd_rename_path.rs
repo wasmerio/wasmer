f449ae56738ab3bfea70204e634b265ffe0855a1
Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>
2021-09-09 10:19:25 +0200

    clean the environment in tests
    Signed-off-by: Benjamin Coenen <5719034+bnjjj@users.noreply.github.com>

--- tests/wasi-wast/wasi/tests/fd_rename_path.rs
+++ tests/wasi-wast/wasi/tests/fd_rename_path.rs
@@ -7,6 +7,10 @@ use std::path::PathBuf;
 fn main() {
     let old_path = PathBuf::from("test_fs/wasitests/dirtorename");
     let new_path = PathBuf::from("test_fs/wasitests/dirrenamed");
+    // Clean the test environment
+    let _ = fs::remove_dir(&old_path);
+    let _ = fs::remove_dir(&new_path);
+
     fs::create_dir_all(&old_path).expect("cannot create the directory");
 
     fs::rename(old_path, &new_path).expect("cannot rename the directory");
