4f3b0a95a3800d5396cf3d31268898631924a9bd
ptitSeb <sebastien.chev@gmail.com>
2021-09-28 11:25:21 +0200

    feat(compiler) - Fixed Arm64Call relocation

--- lib/compiler/src/relocation.rs
+++ lib/compiler/src/relocation.rs
@@ -158,10 +158,10 @@ impl Relocation {
             RelocationKind::Arm64Call => {
                 let reloc_address = start + self.offset as usize;
                 let reloc_addend = self.addend as isize;
-                let reloc_delta_u32 = (target_func_address as u32)
-                    .wrapping_sub(reloc_address as u32)
-                    .wrapping_add(reloc_addend as u32);
-                (reloc_address, reloc_delta_u32 as u64)
+                let reloc_delta_u32 = target_func_address
+                    .wrapping_sub(reloc_address as u64)
+                    .wrapping_add(reloc_addend as u64);
+                (reloc_address, reloc_delta_u32)
             }
             // RelocationKind::X86PCRelRodata4 => {
             //     (start, target_func_address)
--- lib/engine-universal/src/link.rs
+++ lib/engine-universal/src/link.rs
@@ -1,6 +1,6 @@
 //! Linking for Universal-compiled code.
 
-use std::ptr::{write_unaligned, read_unaligned};
+use std::ptr::{read_unaligned, write_unaligned};
 use wasmer_compiler::{
     JumpTable, JumpTableOffsets, Relocation, RelocationKind, RelocationTarget, Relocations,
     SectionIndex,
@@ -55,7 +55,7 @@ fn apply_relocation(
         RelocationKind::X86PCRelRodata4 => {}
         RelocationKind::Arm64Call => unsafe {
             let (reloc_address, reloc_delta) = r.for_address(body, target_func_address as u64);
-            let reloc_delta = reloc_delta as u32 | read_unaligned(reloc_address as *mut u32);
+            let reloc_delta = (reloc_delta / 4) as u32 | read_unaligned(reloc_address as *mut u32);
             write_unaligned(reloc_address as *mut u32, reloc_delta);
         },
         kind => panic!(
