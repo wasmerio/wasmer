5e2714fc0e2121bd05c761f1dd4460429d904f00
ptitSeb <sebastien.chev@gmail.com>
2021-09-30 14:59:57 +0200

    fix(test) new change on the logic of fd_rename_path test

--- tests/wasi-wast/wasi/tests/fd_rename_path.rs
+++ tests/wasi-wast/wasi/tests/fd_rename_path.rs
@@ -6,22 +6,23 @@ use std::path::PathBuf;
 
 fn main() {
     let mut idx = 0;
+    fs::create_dir_all(PathBuf::from("test_fs/wasitests")).expect("cannot create the parent directory");
+
     let old_path = loop {
         let old_path = PathBuf::from(format!("test_fs/wasitests/dirtorename-{}", idx));
-        if ! fs::read_dir(old_path.clone()).ok().is_some() {
+        let created = fs::create_dir(old_path.clone());
+        if created.ok().is_some() {
             break old_path;
         }
         idx+=1;
+        if idx > 10 {
+            panic!("too many try ay creating the folder");
+        }
     };
 
     let new_path = PathBuf::from(format!("test_fs/wasitests/dirrenamed-{}", idx));
 
-    // Clean the test environment
-    let _ = fs::remove_dir(&old_path);
-    let _ = fs::remove_dir(&new_path);
-
-    fs::create_dir_all(&old_path).expect("cannot create the directory");
-
     fs::rename(old_path, &new_path).expect("cannot rename the directory");
+
     fs::remove_dir(&new_path).expect("cannot remove the directory");
 }
