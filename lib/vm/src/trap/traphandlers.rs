dbf7ce0fc70815a9997d28c3046265725c0c6905
Ast-x64 <Ast-x64@protonmail.com>
2022-02-19 22:23:28 +0800

    Add riscv64 support
    This adds riscv64 support for `wasmer-vm`.
    This requires `libc >= 0.2.118` so `Cargo.lock` is modified.

--- lib/vm/src/trap/traphandlers.rs
+++ lib/vm/src/trap/traphandlers.rs
@@ -219,6 +219,9 @@ cfg_if::cfg_if! {
                 } else if #[cfg(all(target_os = "linux", target_arch = "aarch64"))] {
                     let cx = &*(cx as *const libc::ucontext_t);
                     cx.uc_mcontext.pc as *const u8
+                } else if #[cfg(all(target_os = "linux", target_arch = "riscv64"))] {
+                    let cx = &*(cx as *const libc::ucontext_t);
+                    cx.uc_mcontext.__gregs[libc::REG_PC] as *const u8
                 } else if #[cfg(all(target_os = "android", target_arch = "aarch64"))] {
                     let cx = &*(cx as *const libc::ucontext_t);
                     cx.uc_mcontext.pc as *const u8
