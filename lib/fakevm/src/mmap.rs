a355a3cebe0565fe223d9df923d8152d1990b7d5
ptitSeb <sebastien.chev@gmail.com>
2022-04-04 10:46:45 +0200

    Added some FakeVM Mmap emulation so universal engine works

--- lib/fakevm/src/mmap.rs
+++ lib/fakevm/src/mmap.rs
@@ -16,11 +16,7 @@ fn round_up_to_page_size(size: usize, page_size: usize) -> usize {
 /// and initially-zeroed memory and a length.
 #[derive(Debug)]
 pub struct Mmap {
-    // Note that this is stored as a `usize` instead of a `*const` or `*mut`
-    // pointer to allow this structure to be natively `Send` and `Sync` without
-    // `unsafe impl`. This type is sendable across threads and shareable since
-    // the coordination all happens at the OS layer.
-    ptr: usize,
+    mem: Vec<u8>,
     len: usize,
 }
 
@@ -31,50 +27,52 @@ impl Mmap {
         // contains code to create a non-null dangling pointer value when
         // constructed empty, so we reuse that here.
         let empty = Vec::<u8>::new();
-        Self {
-            ptr: empty.as_ptr() as usize,
-            len: 0,
-        }
+        Self { mem: empty, len: 0 }
     }
 
     /// Create a new `Mmap` pointing to at least `size` bytes of page-aligned accessible memory.
     pub fn with_at_least(size: usize) -> Result<Self, String> {
-        let page_size = 65536; //arbitrary 64K pages
+        let page_size = 4096; //arbitrary 4K pages
         let rounded_size = round_up_to_page_size(size, page_size);
         Self::accessible_reserved(rounded_size, rounded_size)
     }
 
     /// Not implemented on FakeVM
     pub fn accessible_reserved(
-        _accessible_size: usize,
+        accessible_size: usize,
         _mapping_size: usize,
     ) -> Result<Self, String> {
-        panic!("Not implemented!");
+        let mut mem = Vec::<u8>::new();
+        mem.resize(accessible_size, 0);
+        Ok(Self {
+            mem: mem,
+            len: accessible_size,
+        })
     }
 
     /// Not implemented on FakeVM
     pub fn make_accessible(&mut self, _start: usize, _len: usize) -> Result<(), String> {
-        panic!("Not implemented!");
+        Ok(())
     }
 
     /// Return the allocated memory as a slice of u8.
     pub fn as_slice(&self) -> &[u8] {
-        unsafe { slice::from_raw_parts(self.ptr as *const u8, self.len) }
+        self.mem.as_slice()
     }
 
     /// Return the allocated memory as a mutable slice of u8.
     pub fn as_mut_slice(&mut self) -> &mut [u8] {
-        unsafe { slice::from_raw_parts_mut(self.ptr as *mut u8, self.len) }
+        self.mem.as_mut_slice()
     }
 
     /// Return the allocated memory as a pointer to u8.
     pub fn as_ptr(&self) -> *const u8 {
-        self.ptr as *const u8
+        self.mem.as_ptr() as *const u8
     }
 
     /// Return the allocated memory as a mutable pointer to u8.
     pub fn as_mut_ptr(&mut self) -> *mut u8 {
-        self.ptr as *mut u8
+        self.mem.as_mut_ptr() as *mut u8
     }
 
     /// Return the length of the allocated memory.
