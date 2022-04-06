477bec75e1ef60225973a7d0c1400fb3ec72e456
Amanieu d'Antras <amanieu@gmail.com>
2022-04-06 19:37:19 +0100

    Fix exports_memory example

--- examples/exports_memory.rs
+++ examples/exports_memory.rs
@@ -29,7 +29,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     global.get $offset
     global.get $length)
 
-  (data (global.get $offset) "Hello, World!"))
+  (data (i32.const 42) "Hello, World!"))
 "#,
     )?;
 
