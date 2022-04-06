683c3688d441a7ed4319bd9f6f67b0f70883eaf5
Amanieu d'Antras <amanieu@gmail.com>
2022-04-06 17:27:10 +0100

    Update spec test workaround for singlepass
    This is necessary for singlepass which doesn't support multi-value yet.

--- fuzz/fuzz_targets/equivalence_universal.rs
+++ fuzz/fuzz_targets/equivalence_universal.rs
@@ -54,7 +54,7 @@ fn maybe_instantiate_singlepass(wasm_bytes: &[u8]) -> Result<Option<Instance>> {
         Ok(m) => m,
         Err(e) => {
             let error_message = format!("{}", e);
-            if error_message.contains("Validation error: invalid result arity: func type returns multiple values") || error_message.contains("Validation error: blocks, loops, and ifs accept no parameters when multi-value is not enabled") || error_message.contains("multi-value returns not yet implemented") {
+            if error_message.contains("Validation error: invalid result arity: func type returns multiple values") || error_message.contains("Validation error: blocks, loops, and ifs may only produce a resulttype when multi-value is not enabled") || error_message.contains("multi-value returns not yet implemented") {
                 return Ok(None);
             }
             return Err(e.into());
