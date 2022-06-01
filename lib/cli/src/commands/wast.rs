f65d42f3242888b21583e46a7d594876d69fd22d
ptitSeb <sebastien.chev@gmail.com>
2022-06-01 15:43:07 +0200

    Migrate wasmer-cli to new Context API

--- lib/cli/src/commands/wast.rs
+++ lib/cli/src/commands/wast.rs
@@ -3,6 +3,7 @@ use crate::store::StoreOptions;
 use anyhow::{Context, Result};
 use std::path::PathBuf;
 use structopt::StructOpt;
+use wasmer::Context as WasmerContext;
 use wasmer_wast::Wast as WastSpectest;
 
 #[derive(Debug, StructOpt)]
@@ -28,7 +29,8 @@ impl Wast {
     }
     fn inner_execute(&self) -> Result<()> {
         let (store, _compiler_name) = self.store.get_store()?;
-        let mut wast = WastSpectest::new_with_spectest(store);
+        let ctx = WasmerContext::new(&store, ());
+        let mut wast = WastSpectest::new_with_spectest(ctx);
         wast.fail_fast = self.fail_fast;
         wast.run_file(&self.path).with_context(|| "tests failed")?;
         eprintln!("Wast tests succeeded for `{}`.", self.path.display());
