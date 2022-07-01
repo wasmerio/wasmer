a1809c72e1edf1c43f6b4b236d7629894a52d3a0
Manos Pitsidianakis <manos@wasmer.io>
2022-07-01 13:01:47 +0300

    Leave rkyv dependency only in wasmer-types
    rkyv is only needed in wasmer-types crate after moving all serializable
    types there.

--- lib/compiler/src/universal_artifact/mod.rs
+++ lib/compiler/src/universal_artifact/mod.rs
@@ -2,10 +2,8 @@
 
 mod artifact;
 mod engine;
-mod serialize;
 mod trampoline;
 
 pub use self::artifact::UniversalArtifactBuild;
 pub use self::engine::UniversalEngineBuilder;
-pub use self::serialize::SerializableModule;
 pub use self::trampoline::*;
