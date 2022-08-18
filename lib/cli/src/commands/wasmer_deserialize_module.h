6d0d40e34685dc146a12570516a453a77475ff33
Manos Pitsidianakis <manos@wasmer.io>
2022-08-18 13:50:40 +0300

    create-exe: fix serialized create-exe not working
    Unified generated objects interfaces: both static and serialized objects
    expose the same function signature to create the module:
    
      wasm_module_t* wasmer_object_module_new(wasm_store_t* store, const char* module_name);

--- lib/cli/src/commands/wasmer_deserialize_module.h
+++ lib/cli/src/commands/wasmer_deserialize_module.h
@@ -10,7 +10,7 @@ extern "C" {
 extern size_t WASMER_MODULE_LENGTH asm("WASMER_MODULE_LENGTH");
 extern char WASMER_MODULE_DATA asm("WASMER_MODULE_DATA");
 
-wasm_module_t* wasmer_module_new(wasm_store_t* store, const char* wasm_name) {
+wasm_module_t* wasmer_object_module_new(wasm_store_t* store, const char* module_name) {
   wasm_byte_vec_t module_byte_vec = {
     .size = WASMER_MODULE_LENGTH,
     .data = (const char*)&WASMER_MODULE_DATA,
