b122feeb837427dbfa47db69dd702b58f4422c7e
Syrus Akbary <me@syrusakbary.com>
2022-08-25 17:35:29 +0200

    Removed deprecated JsImportObject

--- lib/api/src/js/mod.rs
+++ lib/api/src/js/mod.rs
@@ -30,7 +30,6 @@ mod externals;
 mod function_env;
 mod imports;
 mod instance;
-mod js_import_object;
 mod mem_access;
 mod module;
 #[cfg(feature = "wasm-types-polyfill")]
@@ -54,7 +53,6 @@ pub use crate::js::externals::{
 pub use crate::js::function_env::{FunctionEnv, FunctionEnvMut};
 pub use crate::js::imports::Imports;
 pub use crate::js::instance::Instance;
-pub use crate::js::js_import_object::JsImportObject;
 pub use crate::js::mem_access::{MemoryAccessError, WasmRef, WasmSlice, WasmSliceIter};
 pub use crate::js::module::{Module, ModuleTypeHints};
 pub use crate::js::native::TypedFunction;
