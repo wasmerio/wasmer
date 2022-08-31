9849e123393a6ac2c523d1fa55cf433eb91568b1
Felix Schütt <felix@wasmer.io>
2022-08-31 11:38:25 +0200

    Add wasi_io_typenames and wasi_snapshot0

--- lib/wasi-types-generated/regenerate.sh
+++ lib/wasi-types-generated/regenerate.sh
@@ -16,4 +16,14 @@ wit-bindgen wasmer \
     "$BASEDIR"/wit/wasi-filesystem.wit \
     --out-dir "$BASEDIR"/src/wasi_filesystem
 
+wit-bindgen wasmer \
+    --import \
+    "$BASEDIR"/wit/wasi-io-typenames.wit \
+    --out-dir "$BASEDIR"/src/wasi_io_typenames
+
+wit-bindgen wasmer \
+    --import \
+    "$BASEDIR"/wit/wasi-snapshot0.wit \
+    --out-dir "$BASEDIR"/src/wasi_snapshot0
+
 #wit-bindgen rust-wasm --import wit/wasi-snapshot0.wit wit/wasi-filesystem.wit --out-dir .
--- lib/wasi-types-generated/src/lib.rs
+++ lib/wasi-types-generated/src/lib.rs
@@ -1,2 +1,4 @@
 pub mod wasi;
 pub mod wasi_filesystem;
+pub mod wasi_io_typenames;
+pub mod wasi_snapshot0;
