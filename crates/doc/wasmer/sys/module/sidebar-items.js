f7a158e4e9d02e974027c096d3bf3be77082c108
syrusakbary <syrusakbary@users.noreply.github.com>
2022-09-02 05:15:52 +0000

    deploy: e259738ff8fea0a0db2886d0215c7cc07242b3de

--- crates/doc/wasmer/sys/module/sidebar-items.js
+++ crates/doc/wasmer/sys/module/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["IoCompileError",""]],"struct":[["Module","A WebAssembly Module contains stateless WebAssembly code that has already been compiled and can be instantiated multiple times."]]});
+initSidebarItems({"enum":[["IoCompileError",""]],"struct":[["Module","A WebAssembly Module contains stateless WebAssembly code that has already been compiled and can be instantiated multiple times."]],"trait":[["IntoBytes",""]]});
