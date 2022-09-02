670df982120959ae79a06ac7be3174033e297d6a
Felix Schütt <felix@wasmer.io>
2022-09-02 15:56:30 +0200

    Remove macro-test crate, verify that the proc-macro works

--- lib/api/macro-wasmer-test/src/main.rs
+++
@@ -1,9 +0,0 @@
-#[macro_wasmer_universal_test::universal_test]
-fn extern_ref_passed_and_returned() -> Result<(), ()> {
-    Ok(())
-}
-
-fn main() {
-    extern_ref_passed_and_returned_js();
-    extern_ref_passed_and_returned().unwrap();
-}
