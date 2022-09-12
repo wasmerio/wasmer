b98b691a44176265f86550f15a9fae7075182fa3
Felix Schütt <felix@wasmer.io>
2022-09-12 18:24:42 +0200

    Fix compilation issues on Windows

--- lib/wasi/src/syscalls/windows.rs
+++ lib/wasi/src/syscalls/windows.rs
@@ -3,19 +3,19 @@ use tracing::debug;
 use wasmer::WasmRef;
 
 pub fn platform_clock_res_get(
-    clock_id: wasi::Clockid,
+    clock_id: wasi::Snapshot0Clockid,
     resolution: WasmRef<Timestamp>,
 ) -> Result<i64, wasi::Errno> {
     let resolution_val = match clock_id {
         // resolution of monotonic clock at 10ms, from:
         // https://docs.microsoft.com/en-us/windows/desktop/api/sysinfoapi/nf-sysinfoapi-gettickcount64
-        wasi::Clockid::Monotonic => 10_000_000,
+        wasi::Snapshot0Clockid::Monotonic => 10_000_000,
         // TODO: verify or compute this
-        wasi::Clockid::Realtime => 1,
-        wasi::Clockid::ProcessCputimeId => {
+        wasi::Snapshot0Clockid::Realtime => 1,
+        wasi::Snapshot0Clockid::ProcessCputimeId => {
             return Err(wasi::Errno::Inval);
         }
-        wasi::Clockid::ThreadCputimeId => {
+        wasi::Snapshot0Clockid::ThreadCputimeId => {
             return Err(wasi::Errno::Inval);
         }
         _ => return Err(wasi::Errno::Inval),
@@ -24,15 +24,15 @@ pub fn platform_clock_res_get(
 }
 
 pub fn platform_clock_time_get(
-    clock_id: wasi::Clockid,
+    clock_id: wasi::Snapshot0Clockid,
     precision: Timestamp,
 ) -> Result<i64, wasi::Errno> {
     let nanos = match clock_id {
-        wasi::Clockid::MONOTONIC => {
+        wasi::Snapshot0Clockid::Monotonic => {
             let tick_ms = unsafe { winapi::um::sysinfoapi::GetTickCount64() };
             tick_ms * 1_000_000
         }
-        wasi::Clockid::REALTIME => {
+        wasi::Snapshot0Clockid::Realtime => {
             let duration = std::time::SystemTime::now()
                 .duration_since(std::time::UNIX_EPOCH)
                 .map_err(|e| {
@@ -41,10 +41,10 @@ pub fn platform_clock_time_get(
                 })?;
             duration.as_nanos() as u64
         }
-        wasi::Clockid::ProcessCputimeId => {
+        wasi::Snapshot0Clockid::ProcessCputimeId => {
             unimplemented!("wasi::platform_clock_time_get(wasi::Clockid::ProcessCputimeId, ..)")
         }
-        wasi::Clockid::ThreadCputimeId => {
+        wasi::Snapshot0Clockid::ThreadCputimeId => {
             unimplemented!("wasi::platform_clock_time_get(wasi::Clockid::ThreadCputimeId, ..)")
         }
         _ => return Err(wasi::Errno::Inval),
