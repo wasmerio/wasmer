94baa50bbfb4a5ccbc088e28f5c3dc55646179c6
Felix Schütt <felix@wasmer.io>
2022-09-15 11:38:13 +0200

    cargo fmt

--- lib/c-api/tests/wasmer-c-api-test-runner/src/lib.rs
+++ lib/c-api/tests/wasmer-c-api-test-runner/src/lib.rs
@@ -10,7 +10,6 @@ pub struct Config {
     pub msvc_cflags: String,
     pub msvc_ldflags: String,
     pub msvc_ldlibs: String,
-
 }
 
 impl Config {
@@ -30,13 +29,12 @@ impl Config {
     }
 }
 
-
 #[derive(Default)]
-pub struct RemoveTestsOnDrop { }
+pub struct RemoveTestsOnDrop {}
 
-impl Drop for RemoveTestsOnDrop { 
+impl Drop for RemoveTestsOnDrop {
     fn drop(&mut self) {
-        let manifest_dir = env!("CARGO_MANIFEST_DIR");    
+        let manifest_dir = env!("CARGO_MANIFEST_DIR");
         for entry in std::fs::read_dir(&manifest_dir).unwrap() {
             let entry = entry.unwrap();
             let path = entry.path();
@@ -64,16 +62,13 @@ const CAPI_BASE_TESTS: &[&str] = &[
     "wasm-c-api/example/callback",
     "wasm-c-api/example/memory",
     "wasm-c-api/example/start",
-
     "wasm-c-api/example/global",
     "wasm-c-api/example/reflect",
     "wasm-c-api/example/trap",
-
     "wasm-c-api/example/hello",
     "wasm-c-api/example/serialize",
     "wasm-c-api/example/multi",
 ];
-
 
 const CAPI_BASE_TESTS_NOT_WORKING: &[&str] = &[
     "wasm-c-api/example/finalize",
@@ -85,7 +80,6 @@ const CAPI_BASE_TESTS_NOT_WORKING: &[&str] = &[
 // Runs all the tests that are working in the /c directory
 #[test]
 fn test_ok() {
-
     let _drop = RemoveTestsOnDrop::default();
     let config = Config::get();
     println!("config: {:#?}", config);
@@ -96,7 +90,6 @@ fn test_ok() {
 
     if target.contains("msvc") {
         for test in CAPI_BASE_TESTS.iter() {
-
             let mut build = cc::Build::new();
             let mut build = build
                 .cargo_metadata(false)
@@ -108,10 +101,10 @@ fn test_ok() {
                 .host(&host)
                 .target(target)
                 .opt_level(1);
-    
+
             let compiler = build.try_get_compiler().unwrap();
             let mut command = compiler.to_command();
-    
+
             command.arg(&format!("{manifest_dir}/../{test}.c"));
             if !config.msvc_cflags.is_empty() {
                 command.arg(config.msvc_cflags.clone());
@@ -128,88 +121,95 @@ fn test_ok() {
             }
             let wasmer_dll_dir = format!("{}/lib", config.wasmer_dir);
             command.arg(&format!("/OUT:\"{manifest_dir}/../{test}.exe\""));
-    
+
             let exe_dir = format!("{manifest_dir}/../wasm-c-api/example");
-    
+
             // run vcvars
             let vcvars_bat_path = find_vcvars64(&compiler).expect("no vcvars64.bat");
             let mut vcvars = std::process::Command::new("cmd");
             vcvars.arg("/C");
             vcvars.arg(vcvars_bat_path);
             println!("running {vcvars:?}");
-    
+
             // cmd /C vcvars64.bat
-            let output = vcvars.output()
-            .expect("could not invoke vcvars64.bat at {vcvars_bat_path}");
-            
+            let output = vcvars
+                .output()
+                .expect("could not invoke vcvars64.bat at {vcvars_bat_path}");
+
             if !output.status.success() {
                 println!("");
                 println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
                 println!("stderr: {}", String::from_utf8_lossy(&output.stderr));
                 panic!("failed to invoke vcvars64.bat {test}");
             }
-    
+
             println!("compiling {test}: {command:?}");
-    
+
             // compile
-            let output = command.output().expect(&format!("failed to compile {command:#?}"));
+            let output = command
+                .output()
+                .expect(&format!("failed to compile {command:#?}"));
             if !output.status.success() {
                 println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
                 println!("stdout: {}", String::from_utf8_lossy(&output.stderr));
                 panic!("failed to compile {test}");
             }
-    
+
             let path = std::env::var("PATH").unwrap_or_default();
             let newpath = format!("{wasmer_dll_dir};{path}");
-    
+
             // execute
             let mut command = std::process::Command::new(&format!("{manifest_dir}/../{test}.exe"));
             command.env("PATH", newpath.clone());
             command.current_dir(exe_dir.clone());
             println!("executing {test}: {command:?}");
             println!("setting current dir = {exe_dir}");
-            let output = command.output().expect(&format!("failed to run {command:#?}"));
+            let output = command
+                .output()
+                .expect(&format!("failed to run {command:#?}"));
             if !output.status.success() {
                 println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
                 println!("stdout: {}", String::from_utf8_lossy(&output.stderr));
                 panic!("failed to execute {test}");
             }
-    
-            // cc -g -IC:/Users/felix/Development/wasmer/lib/c-api/tests/ 
-            //          -IC:/Users/felix/Development/wasmer/package/include  
+
+            // cc -g -IC:/Users/felix/Development/wasmer/lib/c-api/tests/
+            //          -IC:/Users/felix/Development/wasmer/package/include
             //
-            //          -Wl,-rpath,C:/Users/felix/Development/wasmer/package/lib  
+            //          -Wl,-rpath,C:/Users/felix/Development/wasmer/package/lib
             //
-            //          wasm-c-api/example/callback.c  
+            //          wasm-c-api/example/callback.c
             //
-            //          -LC:/Users/felix/Development/wasmer/package/lib -lwasmer 
-            // 
+            //          -LC:/Users/felix/Development/wasmer/package/lib -lwasmer
+            //
             // -o wasm-c-api/example/callback
-    
         }
     } else {
         for test in CAPI_BASE_TESTS.iter() {
-
             let mut command = std::process::Command::new("cc");
-    
+
             command.arg(config.cflags.clone());
             command.arg(config.ldflags.clone());
             command.arg(&format!("{manifest_dir}/../{test}.c"));
             command.arg(config.ldlibs.clone());
             command.arg("-o");
             command.arg(&format!("{manifest_dir}/../{test}"));
-    
+
             // compile
-            let output = command.output().expect(&format!("failed to compile {command:#?}"));
+            let output = command
+                .output()
+                .expect(&format!("failed to compile {command:#?}"));
             if !output.status.success() {
                 println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
                 println!("stdout: {}", String::from_utf8_lossy(&output.stderr));
                 panic!("failed to compile {test}");
             }
-    
+
             // execute
             let mut command = std::process::Command::new(&format!("{manifest_dir}/../{test}"));
-            let output = command.output().expect(&format!("failed to run {command:#?}"));
+            let output = command
+                .output()
+                .expect(&format!("failed to run {command:#?}"));
             if !output.status.success() {
                 println!("stdout: {}", String::from_utf8_lossy(&output.stdout));
                 println!("stdout: {}", String::from_utf8_lossy(&output.stderr));
@@ -223,18 +223,17 @@ fn test_ok() {
         let _ = std::fs::remove_file(&format!("{manifest_dir}/../{test}.exe"));
         let _ = std::fs::remove_file(&format!("{manifest_dir}/../{test}"));
     }
- }
+}
 
- #[cfg(test)]
- fn find_vcvars64(compiler: &cc::Tool) -> Option<String> {
-    
+#[cfg(test)]
+fn find_vcvars64(compiler: &cc::Tool) -> Option<String> {
     if !compiler.is_like_msvc() {
         return None;
     }
-    
+
     let path = compiler.path();
     let path = format!("{}", path.display());
     let split = path.split("VC").nth(0)?;
 
     Some(format!("{split}VC\\Auxiliary\\Build\\vcvars64.bat"))
- }
+}
