29b8ed93eab1c7194914828b2a8c358591c28763
Felix Schütt <felix@wasmer.io>
2022-09-15 13:40:18 +0200

    cargo fmt

--- tests/integration/cli/src/assets.rs
+++ tests/integration/cli/src/assets.rs
@@ -64,11 +64,15 @@ pub fn get_wasmer_path() -> PathBuf {
     }
     if !ret.exists() {
         if let Some(s) = env!("CARGO_MANIFEST_DIR").split("wasmer").next() {
-            #[cfg(target_os = "windows")] {
-                return std::path::Path::new(&format!("{s}wasmer/target/release/wasmer.exe")).to_path_buf();
+            #[cfg(target_os = "windows")]
+            {
+                return std::path::Path::new(&format!("{s}wasmer/target/release/wasmer.exe"))
+                    .to_path_buf();
             }
-            #[cfg(not(target_os = "windows"))] {
-                return std::path::Path::new(&format!("{s}wasmer/target/release/wasmer")).to_path_buf();
+            #[cfg(not(target_os = "windows"))]
+            {
+                return std::path::Path::new(&format!("{s}wasmer/target/release/wasmer"))
+                    .to_path_buf();
             }
         }
         panic!("Could not find wasmer executable path! {:?}", ret);
