a7c849ff7f676befdbf86c52a1b047d7888ccc24
Felix Schütt <felix@wasmer.io>
2022-10-24 19:20:34 +0200

    Fix mistake in fd_rights integration test

--- tests/wasi-wast/wasi/tests/fd_rights.rs
+++ tests/wasi-wast/wasi/tests/fd_rights.rs
@@ -1,11 +1,9 @@
 use std::io::{Read, Write};
 
 fn main() {
-    println!("0");
     let readdir = std::fs::read_dir("/").unwrap();
     let files = readdir.collect::<Vec<_>>();
     assert!(files.is_empty());
-    println!("1");
 
     let mut filehandle = std::fs::OpenOptions::new()
         .read(false) // <- should only be writeable, not readable
@@ -13,11 +11,8 @@ fn main() {
         .create(true)
         .open("foo.txt")
         .unwrap();
-    println!("1.5");
     filehandle.write_all(b"test");
-    println!("2");
 
     let mut contents = String::new();
     assert!(filehandle.read_to_string(&mut contents).is_err());
-    println!("3");
 }
