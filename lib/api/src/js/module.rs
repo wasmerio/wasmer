4ef92afbaa31726c69da1d98b2a4cb12dbef901b
Christoph Herzog <chris@theduke.at>
2022-11-08 09:41:06 +0100

    api: Don't duplicate IntoBytes trait for js and sys

--- lib/api/src/js/module.rs
+++ lib/api/src/js/module.rs
@@ -53,46 +53,6 @@ pub struct ModuleTypeHints {
     pub exports: Vec<ExternType>,
 }
 
-pub trait IntoBytes {
-    fn into_bytes(self) -> Bytes;
-}
-
-impl IntoBytes for Bytes {
-    fn into_bytes(self) -> Bytes {
-        self
-    }
-}
-
-impl IntoBytes for Vec<u8> {
-    fn into_bytes(self) -> Bytes {
-        Bytes::from(self)
-    }
-}
-
-impl IntoBytes for &[u8] {
-    fn into_bytes(self) -> Bytes {
-        Bytes::from(self.to_vec())
-    }
-}
-
-impl<const N: usize> IntoBytes for &[u8; N] {
-    fn into_bytes(self) -> Bytes {
-        Bytes::from(self.to_vec())
-    }
-}
-
-impl IntoBytes for &str {
-    fn into_bytes(self) -> Bytes {
-        Bytes::from(self.as_bytes().to_vec())
-    }
-}
-
-impl IntoBytes for Cow<'_, [u8]> {
-    fn into_bytes(self) -> Bytes {
-        Bytes::from(self.to_vec())
-    }
-}
-
 /// A WebAssembly Module contains stateless WebAssembly
 /// code that has already been compiled and can be instantiated
 /// multiple times.
