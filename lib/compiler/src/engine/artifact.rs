e97118a71cb9b87dd8e0e491e2a0f2a4bb73065a
Felix Schütt <felix@wasmer.io>
2022-11-16 13:51:14 +0100

    Remove windows_underscore specialcase
    Sebastien suggested this to resolve issues on windows 32 bit,
    but it didn't help anything

--- lib/compiler/src/engine/artifact.rs
+++ lib/compiler/src/engine/artifact.rs
@@ -541,8 +541,6 @@ impl Artifact {
 
         let (_compile_info, mut symbol_registry) = metadata.split();
 
-        symbol_registry.set_windows_underscore(true);
-
         let compilation: wasmer_types::compilation::function::Compilation = compiler
             .compile_module(
                 target,
--- lib/types/src/compilation/symbols.rs
+++ lib/types/src/compilation/symbols.rs
@@ -71,17 +71,8 @@ pub struct ModuleMetadata {
 
 /// A simple metadata registry
 pub struct ModuleMetadataSymbolRegistry {
-    /// Whether to prefix function symbols with a "_" when compiling the object
-    pub windows_underscore: bool,
     /// Symbol prefix stirng
     pub prefix: String,
-}
-
-impl ModuleMetadataSymbolRegistry {
-    /// Sets the self.windows_underscore field
-    pub fn set_windows_underscore(&mut self, b: bool) {
-        self.windows_underscore = b;
-    }
 }
 
 impl ModuleMetadata {
@@ -89,7 +80,6 @@ impl ModuleMetadata {
     pub fn split(&mut self) -> (&mut CompileModuleInfo, ModuleMetadataSymbolRegistry) {
         let compile_info = &mut self.compile_info;
         let symbol_registry = ModuleMetadataSymbolRegistry {
-            windows_underscore: false,
             prefix: self.prefix.clone(),
         };
         (compile_info, symbol_registry)
@@ -98,7 +88,6 @@ impl ModuleMetadata {
     /// Returns symbol registry.
     pub fn get_symbol_registry(&self) -> ModuleMetadataSymbolRegistry {
         ModuleMetadataSymbolRegistry {
-            windows_underscore: false,
             prefix: self.prefix.clone(),
         }
     }
