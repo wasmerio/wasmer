f6323436666679a60bf7a28a22f1177fb9c6790f
John Sharratt's Shared Account <johnathan.sharratt@gmail.com>
2022-11-22 17:02:26 +1100

    Modified the TTY object so it can be send/sync

--- lib/wasi/src/os/tty.rs
+++ lib/wasi/src/os/tty.rs
@@ -1,6 +1,10 @@
-use std::sync::{Arc, Mutex};
+use std::{
+    borrow::Cow,
+    sync::{Arc, Mutex},
+};
 
 use derivative::*;
+use futures::future::BoxFuture;
 use wasmer_vbus::SignalHandlerAbi;
 use wasmer_vfs::{AsyncWriteExt, VirtualFile};
 use wasmer_wasi_types::wasi::{Signal, Snapshot0Clockid};
@@ -144,187 +148,253 @@ impl Tty {
         self.signaler.replace(signaler);
     }
 
-    pub async fn on_event(&mut self, event: InputEvent) {
-        match event {
-            InputEvent::Key => {
-                // do nothing
-            }
-            InputEvent::Data(data) => {
-                // Due to a nasty bug in xterm.js on Android mobile it sends the keys you press
-                // twice in a row with a short interval between - this hack will avoid that bug
-                if self.is_mobile {
-                    let now = platform_clock_time_get(Snapshot0Clockid::Monotonic, 1_000_000)
-                        .unwrap() as u128;
-                    if let Some((what, when)) = self.last.as_ref() {
-                        if what.as_str() == data && now - *when < TTY_MOBILE_PAUSE {
-                            self.last = None;
-                            return;
-                        }
-                    }
-                    self.last = Some((data.clone(), now))
+    pub fn on_event(mut self, event: InputEvent) -> BoxFuture<'static, Self> {
+        Box::pin(async move {
+            match event {
+                InputEvent::Key => {
+                    // do nothing
+                    self
                 }
+                InputEvent::Data(data) => {
+                    // Due to a nasty bug in xterm.js on Android mobile it sends the keys you press
+                    // twice in a row with a short interval between - this hack will avoid that bug
+                    if self.is_mobile {
+                        let now = platform_clock_time_get(Snapshot0Clockid::Monotonic, 1_000_000)
+                            .unwrap() as u128;
+                        if let Some((what, when)) = self.last.as_ref() {
+                            if what.as_str() == data && now - *when < TTY_MOBILE_PAUSE {
+                                self.last = None;
+                                return self;
+                            }
+                        }
+                        self.last = Some((data.clone(), now))
+                    }
 
-                self.on_data(data.as_bytes()).await
+                    self.on_data(data.as_bytes().to_vec().into()).await
+                }
+                InputEvent::Raw(data) => self.on_data(data.into()).await,
             }
-            InputEvent::Raw(data) => self.on_data(&data[..]).await,
-        }
+        })
     }
 
-    async fn on_enter(&mut self, _data: &str) {
-        // Add a line feed on the end and take the line
-        let mut data = self.line.clone();
-        self.line.clear();
-        data.push_str("\n");
-
-        // If echo is on then write a new line
-        {
-            let options = self.options.inner.lock().unwrap();
-            if options.echo {
-                drop(options);
-                self.stdout("\n".as_bytes()).await;
-            }
-        }
-
-        // Send the data to the process
-        let _ = self.stdin.write(data.as_bytes()).await;
-    }
-
-    async fn on_ctrl_c(&mut self, _data: &str) {
-        if let Some(signaler) = self.signaler.as_ref() {
-            signaler.signal(Signal::Sigint as u8);
-
-            let (echo, _line_buffering) = {
-                let options = self.options.inner.lock().unwrap();
-                (options.echo, options.line_buffering)
-            };
-
+    fn on_enter(mut self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move {
+            // Add a line feed on the end and take the line
+            let mut data = self.line.clone();
             self.line.clear();
-            if echo {
-                self.stdout("\n".as_bytes()).await;
+            data.push_str("\n");
+
+            // If echo is on then write a new line
+            {
+                let echo = {
+                    let options = self.options.inner.lock().unwrap();
+                    options.echo
+                };
+                if echo {
+                    let _ = self.stdout.write("\n".as_bytes()).await;
+                }
             }
-            let _ = self.stdin.write("\n".as_bytes()).await;
-        }
+
+            // Send the data to the process
+            let _ = self.stdin.write(data.as_bytes()).await;
+            self
+        })
     }
 
-    async fn on_backspace(&mut self, _data: &str) {
+    fn on_ctrl_c(mut self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move {
+            if let Some(signaler) = self.signaler.as_ref() {
+                signaler.signal(Signal::Sigint as u8);
+
+                let (echo, _line_buffering) = {
+                    let options = self.options.inner.lock().unwrap();
+                    (options.echo, options.line_buffering)
+                };
+
+                self.line.clear();
+                if echo {
+                    self.stdout.write("\n".as_bytes()).await;
+                }
+                let _ = self.stdin.write("\n".as_bytes()).await;
+            }
+            self
+        })
+    }
+
+    fn on_backspace(mut self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
         // Remove a character (if there are none left we are done)
         if self.line.is_empty() {
-            return;
+            return Box::pin(async move { self });
         }
         let len = self.line.len();
         self.line = (&self.line[..len - 1]).to_string();
 
-        // If echo is on then write the backspace
-        {
-            let options = self.options.inner.lock().unwrap();
-            if options.echo {
-                drop(options);
-                self.stdout("\u{0008} \u{0008}".as_bytes()).await;
+        Box::pin(async move {
+            // If echo is on then write the backspace
+            {
+                let echo = {
+                    let options = self.options.inner.lock().unwrap();
+                    options.echo
+                };
+                if echo {
+                    let _ = self.stdout.write("\u{0008} \u{0008}".as_bytes()).await;
+                }
             }
-        }
+            self
+        })
     }
 
-    async fn on_tab(&mut self, _data: &str) {}
+    fn on_tab(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_cursor_left(&mut self, _data: &str) {}
+    fn on_cursor_left(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_cursor_right(&mut self, _data: &str) {}
+    fn on_cursor_right(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_cursor_up(&mut self, _data: &str) {}
+    fn on_cursor_up(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_cursor_down(&mut self, _data: &str) {}
+    fn on_cursor_down(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_home(&mut self, _data: &str) {}
+    fn on_home(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_end(&mut self, _data: &str) {}
+    fn on_end(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_ctrl_l(&mut self, _data: &str) {}
+    fn on_ctrl_l(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_page_up(&mut self, _data: &str) {}
+    fn on_page_up(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_page_down(&mut self, _data: &str) {}
+    fn on_page_down(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f1(&mut self, _data: &str) {}
+    fn on_f1(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f2(&mut self, _data: &str) {}
+    fn on_f2(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f3(&mut self, _data: &str) {}
+    fn on_f3(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f4(&mut self, _data: &str) {}
+    fn on_f4(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f5(&mut self, _data: &str) {}
+    fn on_f5(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f6(&mut self, _data: &str) {}
+    fn on_f6(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f7(&mut self, _data: &str) {}
+    fn on_f7(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f8(&mut self, _data: &str) {}
+    fn on_f8(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f9(&mut self, _data: &str) {}
+    fn on_f9(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f10(&mut self, _data: &str) {}
+    fn on_f10(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f11(&mut self, _data: &str) {}
+    fn on_f11(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_f12(&mut self, _data: &str) {}
+    fn on_f12(self, _data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move { self })
+    }
 
-    async fn on_data(&mut self, data: &[u8]) {
+    fn on_data(mut self, data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
         // If we are line buffering then we need to check for some special cases
         let options = { self.options.inner.lock().unwrap().clone() };
         if options.line_buffering {
             let echo = options.echo;
             drop(options);
-            let data = String::from_utf8_lossy(data);
-            let data = data.as_ref();
-            return match data {
-                "\r" | "\u{000A}" => self.on_enter(data).await,
-                "\u{0003}" => self.on_ctrl_c(data).await,
-                "\u{007F}" => self.on_backspace(data).await,
-                "\u{0009}" => self.on_tab(data).await,
-                "\u{001B}\u{005B}\u{0044}" => self.on_cursor_left(data).await,
-                "\u{001B}\u{005B}\u{0043}" => self.on_cursor_right(data).await,
-                "\u{0001}" | "\u{001B}\u{005B}\u{0048}" => self.on_home(data).await,
-                "\u{001B}\u{005B}\u{0046}" => self.on_end(data).await,
-                "\u{001B}\u{005B}\u{0041}" => self.on_cursor_up(data).await,
-                "\u{001B}\u{005B}\u{0042}" => self.on_cursor_down(data).await,
-                "\u{000C}" => self.on_ctrl_l(data).await,
-                "\u{001B}\u{005B}\u{0035}\u{007E}" => self.on_page_up(data).await,
-                "\u{001B}\u{005B}\u{0036}\u{007E}" => self.on_page_down(data).await,
-                "\u{001B}\u{004F}\u{0050}" => self.on_f1(data).await,
-                "\u{001B}\u{004F}\u{0051}" => self.on_f2(data).await,
-                "\u{001B}\u{004F}\u{0052}" => self.on_f3(data).await,
-                "\u{001B}\u{004F}\u{0053}" => self.on_f4(data).await,
-                "\u{001B}\u{005B}\u{0031}\u{0035}\u{007E}" => self.on_f5(data).await,
-                "\u{001B}\u{005B}\u{0031}\u{0037}\u{007E}" => self.on_f6(data).await,
-                "\u{001B}\u{005B}\u{0031}\u{0038}\u{007E}" => self.on_f7(data).await,
-                "\u{001B}\u{005B}\u{0031}\u{0039}\u{007E}" => self.on_f8(data).await,
-                "\u{001B}\u{005B}\u{0032}\u{0030}\u{007E}" => self.on_f9(data).await,
-                "\u{001B}\u{005B}\u{0032}\u{0031}\u{007E}" => self.on_f10(data).await,
-                "\u{001B}\u{005B}\u{0032}\u{0033}\u{007E}" => self.on_f11(data).await,
-                "\u{001B}\u{005B}\u{0032}\u{0034}\u{007E}" => self.on_f12(data).await,
-                data => {
+            return match String::from_utf8_lossy(data.as_ref()).as_ref() {
+                "\r" | "\u{000A}" => self.on_enter(data),
+                "\u{0003}" => self.on_ctrl_c(data),
+                "\u{007F}" => self.on_backspace(data),
+                "\u{0009}" => self.on_tab(data),
+                "\u{001B}\u{005B}\u{0044}" => self.on_cursor_left(data),
+                "\u{001B}\u{005B}\u{0043}" => self.on_cursor_right(data),
+                "\u{0001}" | "\u{001B}\u{005B}\u{0048}" => self.on_home(data),
+                "\u{001B}\u{005B}\u{0046}" => self.on_end(data),
+                "\u{001B}\u{005B}\u{0041}" => self.on_cursor_up(data),
+                "\u{001B}\u{005B}\u{0042}" => self.on_cursor_down(data),
+                "\u{000C}" => self.on_ctrl_l(data),
+                "\u{001B}\u{005B}\u{0035}\u{007E}" => self.on_page_up(data),
+                "\u{001B}\u{005B}\u{0036}\u{007E}" => self.on_page_down(data),
+                "\u{001B}\u{004F}\u{0050}" => self.on_f1(data),
+                "\u{001B}\u{004F}\u{0051}" => self.on_f2(data),
+                "\u{001B}\u{004F}\u{0052}" => self.on_f3(data),
+                "\u{001B}\u{004F}\u{0053}" => self.on_f4(data),
+                "\u{001B}\u{005B}\u{0031}\u{0035}\u{007E}" => self.on_f5(data),
+                "\u{001B}\u{005B}\u{0031}\u{0037}\u{007E}" => self.on_f6(data),
+                "\u{001B}\u{005B}\u{0031}\u{0038}\u{007E}" => self.on_f7(data),
+                "\u{001B}\u{005B}\u{0031}\u{0039}\u{007E}" => self.on_f8(data),
+                "\u{001B}\u{005B}\u{0032}\u{0030}\u{007E}" => self.on_f9(data),
+                "\u{001B}\u{005B}\u{0032}\u{0031}\u{007E}" => self.on_f10(data),
+                "\u{001B}\u{005B}\u{0032}\u{0033}\u{007E}" => self.on_f11(data),
+                "\u{001B}\u{005B}\u{0032}\u{0034}\u{007E}" => self.on_f12(data),
+                _ => Box::pin(async move {
                     if echo == true {
-                        self.stdout(data.as_bytes()).await;
+                        let _ = self.stdout.write(data.as_ref()).await;
                     }
-                    self.line.push_str(data);
-                }
+                    self.line
+                        .push_str(String::from_utf8_lossy(data.as_ref()).as_ref());
+                    self
+                }),
             };
         };
 
-        // If the echo is enabled then write it to the terminal
-        if options.echo == true {
-            drop(options);
-            self.stdout(data).await;
-        } else {
-            drop(options);
-        }
+        Box::pin(async move {
+            // If the echo is enabled then write it to the terminal
+            if options.echo == true {
+                drop(options);
+                let _ = self.stdout.write(data.as_ref()).await;
+            } else {
+                drop(options);
+            }
 
-        // Now send it to the process
-        let _ = self.stdin.write(data).await;
+            // Now send it to the process
+            let _ = self.stdin.write(data.as_ref()).await;
+            self
+        })
     }
 
-    async fn stdout(&mut self, data: &[u8]) {
-        let _ = self.stdout.write(&data[..]).await;
+    fn stdout(mut self, data: Cow<'static, [u8]>) -> BoxFuture<'static, Self> {
+        Box::pin(async move {
+            let _ = self.stdout.write(data.as_ref()).await;
+            self
+        })
     }
 }
 
