61819d6e39f59afa068d3491e8f9034e0066972a
Felix Schütt <felix@wasmer.io>
2022-11-23 17:29:39 +0100

    Fix cargo build-wasmer

--- xtask/xtask-build-capi/src/main.rs
+++ xtask/xtask-build-capi/src/main.rs
@@ -10,6 +10,37 @@ fn project_root() -> PathBuf {
         .to_path_buf()
 }
 
+fn package_capi(out: &PathBuf) {
+    use flate2::write::GzEncoder;
+    use flate2::Compression;
+    use std::fs::File;
+    let tar = File::create(out).unwrap();
+    let enc = GzEncoder::new(tar, Compression::none());
+    let mut a = tar::Builder::new(enc);
+
+    let in_dir = project_root();
+    let lib_dir = project_root().join("package").join("lib");
+    let include_dir = project_root().join("package").join("include");
+    let release_dir = in_dir.join("target").join("release");
+
+    std::fs::create_dir_all(&lib_dir).unwrap();
+    std::fs::create_dir_all(&include_dir).unwrap();
+
+    let libwasmer_path = release_dir.join("libwasmer.a");
+    if libwasmer_path.exists() {
+        std::fs::copy(&libwasmer_path, lib_dir.join("libwasmer.a")).unwrap();
+    }
+
+    let dll_path = release_dir.join("wasmer.dll");
+    if dll_path.exists() {
+        std::fs::copy(&dll_path, lib_dir.join("wasmer.dll")).unwrap();
+    }
+
+    a.append_dir_all("lib", &lib_dir).unwrap();
+    a.append_dir_all("include", &include_dir).unwrap();
+    a.finish().unwrap();
+}
+
 fn main() {
     let compilers = env::var("COMPILERS").unwrap_or("cranelift".to_string());
     let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
@@ -51,4 +82,8 @@ fn main() {
     }
 
     println!("build capi succeeded, compilers = {compilers}");
+
+    let out_path = project_root().join("build-capi.tar.gz");
+    package_capi(&out_path);
+    println!("packaged to {}", out_path.display());
 }
--- xtask/xtask-test-capi/src/main.rs
+++ xtask/xtask-test-capi/src/main.rs
@@ -10,6 +10,29 @@ fn project_root() -> PathBuf {
 }
 
 fn main() {
+    /*
+        exclude_tests := --exclude wasmer-c-api --exclude wasmer-cli --exclude wasmer-compiler-cli
+        # Is failing to compile in Linux for some reason
+        exclude_tests += --exclude wasmer-wasi-experimental-io-devices
+        # We run integration tests separately (it requires building the c-api)
+        exclude_tests += --exclude wasmer-integration-tests-cli
+        exclude_tests += --exclude wasmer-integration-tests-ios
+
+        ifneq (, $(findstring llvm,$(compilers)))
+            ENABLE_LLVM := 1
+        else
+            # We exclude LLVM from our package testing
+            exclude_tests += --exclude wasmer-compiler-llvm
+        endif
+    */
+
+    // build-capi package-capi
+    // $(foreach compiler_engine,$(capi_compilers_engines),test-capi-crate-$(compiler_engine) test-capi-integration-$(compiler_engine))
+    /*
+        WASMER_CAPI_CONFIG=$(shell echo $@ | sed -e s/test-capi-crate-//) $(CARGO_BINARY) test $(CARGO_TARGET) --manifest-path lib/c-api/Cargo.toml --release \
+        --no-default-features --features wat,compiler,wasi,middlewares,webc_runner $(capi_compiler_features) -- --nocapture
+    */
+    // std::fs::read("./build-capi.tar.gz");
     let compilers = std::env::var("COMPILERS").unwrap();
     println!("test capi, compilers = {compilers}");
 }
