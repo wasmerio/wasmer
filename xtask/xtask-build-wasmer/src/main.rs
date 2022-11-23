77cf2dc11302810987a768f62e7d836032eb2493
Felix Schütt <felix@wasmer.io>
2022-11-23 17:29:39 +0100

    Fix cargo build-wasmer

--- xtask/xtask-build-wasmer/src/main.rs
+++ xtask/xtask-build-wasmer/src/main.rs
@@ -10,6 +10,34 @@ fn project_root() -> PathBuf {
         .to_path_buf()
 }
 
+fn package_wasmer(out: &PathBuf) {
+    use flate2::write::GzEncoder;
+    use flate2::Compression;
+    use std::fs::File;
+    let tar = File::create(out).unwrap();
+    let enc = GzEncoder::new(tar, Compression::none());
+    let mut a = tar::Builder::new(enc);
+
+    let in_dir = project_root();
+    let bin_dir = project_root().join("package").join("bin");
+    let release_dir = in_dir.join("target").join("release");
+
+    std::fs::create_dir_all(&bin_dir).unwrap();
+
+    let libwasmer_path = release_dir.join("wasmer");
+    if libwasmer_path.exists() {
+        std::fs::copy(&libwasmer_path, bin_dir.join("wasmer")).unwrap();
+    }
+
+    let libwasmer_path = release_dir.join("wasmer.exe");
+    if libwasmer_path.exists() {
+        std::fs::copy(&libwasmer_path, bin_dir.join("wasmer.exe")).unwrap();
+    }
+
+    a.append_dir_all("bin", &bin_dir).unwrap();
+    a.finish().unwrap();
+}
+
 fn main() {
     let compilers = env::var("COMPILERS").unwrap_or("cranelift".to_string());
     let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
@@ -35,6 +63,8 @@ fn main() {
         "webc_runner",
         "--features",
         &format!("{compiler_features}wasmer-artifact-create,static-artifact-create,wasmer-artifact-load,static-artifact-load"),
+        "--bin",
+        "wasmer",
     ]);
 
     println!("running {cmd:?}");
@@ -48,5 +78,10 @@ fn main() {
         panic!("build capi failed");
     }
 
-    println!("build capi succeeded, compilers = {compilers}");
+    println!("build wasmer succeeded, compilers = {compilers}");
+
+    let out_path = project_root().join("build-wasmer.tar.gz");
+    package_wasmer(&out_path);
+
+    println!("packaged to = {}", out_path.display());
 }
--- xtask/xtask-test-wasmer/src/main.rs
+++ xtask/xtask-test-wasmer/src/main.rs
@@ -1,3 +1,4 @@
+use std::env;
 use std::path::{Path, PathBuf};
 use std::process::{Command, Stdio};
 
@@ -9,8 +10,93 @@ fn project_root() -> PathBuf {
         .to_path_buf()
 }
 
+fn start_test(args: &[&str]) {
+    let args = args.iter().map(|s| s.to_string()).collect::<Vec<_>>();
+    let cargo = env::var("CARGO").unwrap_or_else(|_| "cargo".to_string());
+    let compilers = env::var("COMPILERS").unwrap_or("cranelift".to_string());
+
+    let mut cmd = Command::new(&cargo);
+    cmd.current_dir(project_root());
+    cmd.stdin(Stdio::null());
+    cmd.stdout(Stdio::inherit());
+    cmd.stderr(Stdio::inherit());
+    cmd.args(args);
+
+    println!("running {cmd:?}");
+
+    let cmd = cmd.output().unwrap();
+
+    if !cmd.status.success() {
+        println!("test wasmer paniced with status {}", cmd.status);
+        println!("stdout: {}", String::from_utf8_lossy(&cmd.stdout));
+        println!("stderr: {}", String::from_utf8_lossy(&cmd.stdout));
+        panic!("test wasmer failed");
+    }
+
+    println!("{cmd:?} succeeded, compilers = {compilers}");
+}
+
 fn main() {
-    // $(CARGO_BINARY) test $(CARGO_TARGET) --release --tests $(compiler_features)
-    let compilers = std::env::var("COMPILERS").unwrap();
-    println!("test wasmer, compilers = {compilers}");
+    let compilers = env::var("COMPILERS").unwrap_or("cranelift".to_string());
+    let mut compiler_features = compilers
+        .replace(" ", ",")
+        .split(",")
+        .collect::<Vec<_>>()
+        .join(",");
+
+    if !compiler_features.is_empty() {
+        compiler_features.push(',');
+    };
+
+    let mut exclude_tests = vec![
+        "wasmer-c-api",
+        "wasmer-cli",
+        "wasmer-compiler-cli",
+        "wasmer-wasi-experimental-io-devices",
+        "wasmer-integration-tests-ios",
+    ];
+
+    if !compiler_features.contains("llvm") {
+        exclude_tests.push("wasmer-compiler-llvm");
+    }
+
+    let exclude_tests = exclude_tests.join("--exclude ");
+
+    start_test(&[
+        "test",
+        "--release",
+        "--tests",
+        "--features",
+        "cranelift",
+        "--features",
+        &format!("{compiler_features}"),
+    ]);
+
+    start_test(&[
+        "test",
+        "--all",
+        "--release",
+        &exclude_tests,
+        "--features",
+        "cranelift",
+    ]);
+
+    start_test(&[
+        "test",
+        &compiler_features,
+        "--features",
+        "wasi,cranelift",
+        "--examples",
+    ]);
+
+    start_test(&[
+        "test",
+        &compiler_features,
+        "--features",
+        "wasi,cranelift",
+        "--examples",
+        "--release", // <-
+    ]);
+
+    start_test(&["test", "--doc", "--all", "--features", "cranelift,std"]);
 }
