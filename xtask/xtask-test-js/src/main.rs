ef4e8e7237fab9cbf4037fc8f180f46a3eb4ca1e
Felix Schütt <felix@wasmer.io>
2022-11-23 13:46:58 +0100

    Add tasks for cargo build-wasmer and cargo build-capi

--- xtask/xtask-test-js/src/main.rs
+++ xtask/xtask-test-js/src/main.rs
@@ -1,3 +1,14 @@
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+
+fn project_root() -> PathBuf {
+    Path::new(&env!("CARGO_MANIFEST_DIR"))
+        .ancestors()
+        .nth(2)
+        .unwrap()
+        .to_path_buf()
+}
+
 fn main() {
     println!("Hello, world!");
 }
--- xtask/xtask-test-wasmer/src/main.rs
+++ xtask/xtask-test-wasmer/src/main.rs
@@ -1,4 +1,16 @@
+use std::path::{Path, PathBuf};
+use std::process::{Command, Stdio};
+
+fn project_root() -> PathBuf {
+    Path::new(&env!("CARGO_MANIFEST_DIR"))
+        .ancestors()
+        .nth(2)
+        .unwrap()
+        .to_path_buf()
+}
+
 fn main() {
+    // $(CARGO_BINARY) test $(CARGO_TARGET) --release --tests $(compiler_features)
     let compilers = std::env::var("COMPILERS").unwrap();
     println!("test wasmer, compilers = {compilers}");
 }
