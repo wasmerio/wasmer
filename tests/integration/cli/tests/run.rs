80b7cb99f628ed30d8692e9240b2593beefa0e05
Felix Schütt <felix@wasmer.io>
2022-11-30 18:24:23 +0100

    Adjust unit test to test for all configurations for create-exe

--- tests/integration/cli/tests/run.rs
+++ tests/integration/cli/tests/run.rs
@@ -33,41 +33,51 @@ fn test_cross_compile_python_windows() -> anyhow::Result<()> {
         "x86_64-windows-gnu",
     ];
 
+    let compilers = &[
+        "cranelift",
+        "singlepass",
+        "llvm",
+    ];
+
     for t in targets {
-        let python_wasmer_path = temp_dir.path().join(format!("{t}-python"));
+        for c in compilers {
+            println!("{t} target {c}");
+            let python_wasmer_path = temp_dir.path().join(format!("{t}-python"));
 
-        let mut output = Command::new(get_wasmer_path());
-
-        output.arg("create-exe");
-        output.arg(wasi_test_python_path());
-        output.arg("--target");
-        output.arg(t);
-        output.arg("-o");
-        output.arg(python_wasmer_path.clone());
-        let output = output.output()?;
-
-        let stdout = std::str::from_utf8(&output.stdout)
-            .expect("stdout is not utf8! need to handle arbitrary bytes");
-
-        let stderr = std::str::from_utf8(&output.stderr)
-            .expect("stderr is not utf8! need to handle arbitrary bytes");
-
-        if !output.status.success() {
-            bail!("linking failed with: stdout: {stdout}\n\nstderr: {stderr}");
-        }
-
-        println!("stdout: {stdout}");
-        println!("stderr: {stderr}");
-
-        if !python_wasmer_path.exists() {
-            let p = std::fs::read_dir(temp_dir.path())
-                .unwrap()
-                .filter_map(|e| Some(e.ok()?.path()))
-                .collect::<Vec<_>>();
-            panic!(
-                "target {t} was not compiled correctly {stdout} {stderr}, tempdir: {:#?}",
-                p
-            );
+            let mut output = Command::new(get_wasmer_path());
+    
+            output.arg("create-exe");
+            output.arg(wasi_test_python_path());
+            output.arg("--target");
+            output.arg(t);
+            output.arg("-o");
+            output.arg(python_wasmer_path.clone());
+            output.arg(format!("--{c}"));
+            let output = output.output()?;
+    
+            let stdout = std::str::from_utf8(&output.stdout)
+                .expect("stdout is not utf8! need to handle arbitrary bytes");
+    
+            let stderr = std::str::from_utf8(&output.stderr)
+                .expect("stderr is not utf8! need to handle arbitrary bytes");
+    
+            if !output.status.success() {
+                bail!("linking failed with: stdout: {stdout}\n\nstderr: {stderr}");
+            }
+    
+            println!("stdout: {stdout}");
+            println!("stderr: {stderr}");
+    
+            if !python_wasmer_path.exists() {
+                let p = std::fs::read_dir(temp_dir.path())
+                    .unwrap()
+                    .filter_map(|e| Some(e.ok()?.path()))
+                    .collect::<Vec<_>>();
+                panic!(
+                    "target {t} was not compiled correctly {stdout} {stderr}, tempdir: {:#?}",
+                    p
+                );
+            }
         }
     }
 
