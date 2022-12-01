73ad8684377c12cc8baf40585e18b728a6eb2015
Christoph Herzog <chris@theduke.at>
2022-12-01 21:33:19 +0100

    Add Debug impls for HttpRequest/Response

--- lib/wasi/src/http/mod.rs
+++ lib/wasi/src/http/mod.rs
@@ -12,13 +12,27 @@ pub struct HttpRequestOptions {
 }
 
 // TODO: use types from http crate?
-#[derive(Debug)]
 pub struct HttpRequest {
     pub url: String,
     pub method: String,
     pub headers: Vec<(String, String)>,
     pub body: Option<Vec<u8>>,
     pub options: HttpRequestOptions,
+}
+
+impl std::fmt::Debug for HttpRequest {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("HttpRequest")
+            .field("url", &self.url)
+            .field("method", &self.method)
+            .field("headers", &self.headers)
+            .field(
+                "body",
+                &self.body.as_deref().map(|b| String::from_utf8_lossy(b)),
+            )
+            .field("options", &self.options)
+            .finish()
+    }
 }
 
 // TODO: use types from http crate?
@@ -30,6 +44,23 @@ pub struct HttpResponse {
     pub status: u16,
     pub status_text: String,
     pub headers: Vec<(String, String)>,
+}
+
+impl std::fmt::Debug for HttpResponse {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.debug_struct("HttpResponse")
+            .field("pos", &self.pos)
+            .field(
+                "body",
+                &self.body.as_deref().map(|b| String::from_utf8_lossy(b)),
+            )
+            .field("ok", &self.ok)
+            .field("redirected", &self.redirected)
+            .field("status", &self.status)
+            .field("status_text", &self.status_text)
+            .field("headers", &self.headers)
+            .finish()
+    }
 }
 
 pub trait HttpClient: std::fmt::Debug {
