d55b1f0bb2a4afd8c7771bd2a2825cfcd9e0368e
Christoph Herzog <chris@theduke.at>
2022-12-08 15:03:27 +0100

    wasi: Re-generate binding types

--- lib/wasi-types/regenerate.sh
+++ lib/wasi-types/regenerate.sh
@@ -1,4 +1,6 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+set -Eeuxo pipefail
 
 BASEDIR=$(dirname "$0")
 
@@ -8,23 +10,25 @@ rm -f \
 
 cat "$BASEDIR"/wit-clean/typenames.wit "$BASEDIR"/wit-clean/wasi_unstable.wit > "$BASEDIR"/wit-clean/output.wit
 
-cargo install --force wai-bindgen
-git pull origin force-generate-structs
+if ! command -v wai-bindgen &>/dev/null; then
+    echo "Error: wai-bindgen isn't installed."
+    echo 'Please install it with "cargo install wai-bindgen-cli --version 0.2.2" and try again.'
+    exit 1
+fi
 
 wai-bindgen rust-wasm \
-    --import "$BASEDIR"/wit-clean/output.wit \
     --force-generate-structs \
+    --import "$BASEDIR"/wit-clean/output.wit \
     --out-dir "$BASEDIR"/src/wasi \
 
 awk '{sub(/mod output/,"pub mod output")}1' src/wasi/bindings.rs > src/wasi/bindings2.rs
-cargo fmt --all
 cp src/wasi/bindings2.rs src/wasi/bindings.rs
 rm src/wasi/bindings2.rs
 
 cd ./wasi-types-generator-extra
 cargo build
-pwd
 `pwd`/target/debug/wasi-types-generator-extra
 cd ..
+# rm src/wasi/bindings.rs
 
 cargo fmt --all
