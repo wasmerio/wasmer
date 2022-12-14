5e612f950f9320d0792628146f08e7e48be7d6eb
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2022-12-14 18:04:04 +1100

    Added TTY methods used to remove the STDIN and replace it will a null file which will kill the pipe

--- lib/wasi/src/os/tty.rs
+++ lib/wasi/src/os/tty.rs
@@ -6,7 +6,7 @@ use std::{
 use derivative::*;
 use futures::future::BoxFuture;
 use wasmer_vbus::SignalHandlerAbi;
-use wasmer_vfs::{AsyncWriteExt, VirtualFile};
+use wasmer_vfs::{AsyncWriteExt, VirtualFile, NullFile};
 use wasmer_wasi_types::wasi::{Signal, Snapshot0Clockid};
 
 use crate::syscalls::platform_clock_time_get;
@@ -146,6 +146,17 @@ impl Tty {
 
     pub fn stdin_mut(&mut self) -> &mut (dyn VirtualFile + Send + Sync + 'static) {
         self.stdin.as_mut()
+    }
+
+    pub fn stdin_replace(&mut self, mut stdin: Box<dyn VirtualFile + Send + Sync + 'static>) -> Box<dyn VirtualFile + Send + Sync + 'static> {
+        std::mem::swap(&mut self.stdin, &mut stdin);
+        stdin
+    }
+
+    pub fn stdin_take(&mut self) -> Box<dyn VirtualFile + Send + Sync + 'static> {
+        let mut stdin: Box<dyn VirtualFile + Send + Sync + 'static> = Box::new(NullFile::default());
+        std::mem::swap(&mut self.stdin, &mut stdin);
+        stdin
     }
 
     pub fn options(&self) -> TtyOptions {
