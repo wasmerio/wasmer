423335b8920ba2a0cc74ebed7320493775117096
Christoph Herzog <chris@theduke.at>
2022-12-15 00:16:39 +0100

    fix(wasi): Update fd stats after sync
    Previously the fd_sync() WASI syscall implementation would not update
    the FileStat statistics for a file descriptor upon flushing.
    
    This leads to incorrect metadata on fd_fdstat_get syscalls, which breaks
    some tests.
    
    This commit changes the syscall implementation to update the stats.
    
    NOTE: this is still a relatively suboptimal implementation, we should
    probably always update the stats.
    
    This will have to be done in the context of a general Inode system
    rewrite.

--- lib/wasi/src/syscalls/wasi/fd_sync.rs
+++ lib/wasi/src/syscalls/wasi/fd_sync.rs
@@ -15,6 +15,7 @@ pub fn fd_sync(mut ctx: FunctionEnvMut<'_, WasiEnv>, fd: WasiFd) -> Result<Errno
     debug!("=> fd={}", fd);
     let env = ctx.data();
     let (_, mut state, inodes) = env.get_memory_and_wasi_state_and_inodes(&ctx, 0);
+    drop(env);
     let fd_entry = wasi_try_ok!(state.fs.get_fd(fd));
     if !fd_entry.rights.contains(Rights::FD_SYNC) {
         return Ok(Errno::Access);
@@ -28,14 +29,31 @@ pub fn fd_sync(mut ctx: FunctionEnvMut<'_, WasiEnv>, fd: WasiFd) -> Result<Errno
             Kind::File { handle, .. } => {
                 if let Some(handle) = handle {
                     let handle = handle.clone();
+                    drop(fd_entry);
                     drop(inode);
                     drop(guard);
                     drop(inodes);
 
-                    wasi_try_ok!(__asyncify(&mut ctx, None, async move {
-                        let mut handle = handle.write().unwrap();
-                        handle.flush().await.map_err(map_io_err)
-                    })?)
+                    let size = {
+                        wasi_try_ok!(__asyncify(&mut ctx, None, async move {
+                            let mut handle = handle.write().unwrap();
+                            handle.flush().await.map_err(map_io_err)?;
+                            Ok(handle.size())
+                        })?)
+                    };
+
+                    // Update FileStat to reflect the correct current size.
+                    // TODO: don't lock twice - currently needed to not keep a lock on all inodes
+                    {
+                        let env = ctx.data();
+                        let (_, mut state, inodes) =
+                            env.get_memory_and_wasi_state_and_inodes_mut(&ctx, 0);
+
+                        let fd_entry = wasi_try_ok!(state.fs.get_fd(fd));
+                        let inode = fd_entry.inode;
+                        let mut guard = inodes.arena[inode].stat.write().unwrap();
+                        guard.st_size = size;
+                    }
                 } else {
                     return Ok(Errno::Inval);
                 }
