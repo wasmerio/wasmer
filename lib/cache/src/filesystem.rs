66f840db48688b703886db7a038949534cf62b4b
David Sherret <dsherret@gmail.com>
2022-12-21 20:15:32 -0500

    Update Cache trait.

--- lib/cache/src/filesystem.rs
+++ lib/cache/src/filesystem.rs
@@ -4,7 +4,7 @@ use crate::hash::Hash;
 use std::fs::{create_dir_all, File};
 use std::io::{self, Write};
 use std::path::PathBuf;
-use wasmer::{DeserializeError, Module, SerializeError, Store};
+use wasmer::{DeserializeError, Module, SerializeError, AsEngineRef};
 
 /// Representation of a directory that contains compiled wasm artifacts.
 ///
@@ -91,14 +91,14 @@ impl Cache for FileSystemCache {
     type DeserializeError = DeserializeError;
     type SerializeError = SerializeError;
 
-    unsafe fn load(&self, store: &Store, key: Hash) -> Result<Module, Self::DeserializeError> {
+    unsafe fn load(&self, engine: &impl AsEngineRef, key: Hash) -> Result<Module, Self::DeserializeError> {
         let filename = if let Some(ref ext) = self.ext {
             format!("{}.{}", key.to_string(), ext)
         } else {
             key.to_string()
         };
         let path = self.path.join(filename);
-        Module::deserialize_from_file(store, path)
+        Module::deserialize_from_file(engine, path)
     }
 
     fn store(&mut self, key: Hash, module: &Module) -> Result<(), Self::SerializeError> {
