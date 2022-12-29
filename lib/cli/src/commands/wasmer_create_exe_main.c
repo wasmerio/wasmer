ed251a46f5e06fbd793aad864eea6f596c74ac2a
Felix Schütt <felix@wasmer.io>
2022-12-29 18:59:51 +0100

    Use strstr to detect WASI failure

--- lib/cli/src/commands/wasmer_create_exe_main.c
+++ lib/cli/src/commands/wasmer_create_exe_main.c
@@ -237,7 +237,7 @@ int main(int argc, char *argv[]) {
     wasm_message_t retrieved_message;
     // TODO: this is a shitty solution, but it's good enough for now
     wasm_trap_message(trap, &retrieved_message);
-    if (strcmp(retrieved_message.data, "WASI exited with code: 0") == 0) {
+    if (strstr(retrieved_message.data, "WASI exited with code: 0")) {
       wasm_trap_delete(trap);
     } else {
       fprintf(stderr, "%s", retrieved_message.data);
