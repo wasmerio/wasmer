a859d689419028b441ab82505f299ce2da6ec8d2
Felix Schütt <felix@wasmer.io>
2023-01-02 18:24:30 +0100

    create-exe: use -lc on all platforms

--- lib/cli/src/commands/create_exe.rs
+++ lib/cli/src/commands/create_exe.rs
@@ -1139,11 +1139,7 @@ fn link_exe_from_dir(
     cmd.arg("-target");
     cmd.arg(&zig_triple);
 
-    if zig_triple.contains("windows") {
-        cmd.arg("-lc++");
-    } else {
-        cmd.arg("-lc");
-    }
+    cmd.arg("-lc");
 
     let mut include_dirs = include_dirs;
     include_dirs.sort();
