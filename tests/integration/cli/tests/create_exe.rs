233b0fed7b4ad2441eec76f9e17bfc17167ab042
Felix Schütt <felix@wasmer.io>
2023-01-17 09:34:14 +0100

    Revert "Added reference to ticket #3481 on the disabled tests"
    This reverts commit 6c8b1fdf9b123d2b863c117aa4b412bfc3b01c71.

--- tests/integration/cli/tests/create_exe.rs
+++ tests/integration/cli/tests/create_exe.rs
@@ -481,11 +481,6 @@ fn create_exe_works_with_file() -> anyhow::Result<()> {
     Ok(())
 }
 
-// Ignored because of -lunwind linker issue on Windows
-// see https://github.com/wasmerio/wasmer/issues/3459
-//#[cfg_attr(target_os = "windows", ignore)]
-// TODO: fix the cretae_exe with serialized object issue
-#[ignore]
 #[test]
 fn create_exe_serialized_works() -> anyhow::Result<()> {
     let temp_dir = tempfile::tempdir()?;
