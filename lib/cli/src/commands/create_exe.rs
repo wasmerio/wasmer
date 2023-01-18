2ce32dba799a76d088ad89d74657353266877e40
Felix Schütt <felix@wasmer.io>
2023-01-18 19:00:47 +0100

    Debug why get_store_for_target returns wrong UnwindInfo

--- lib/cli/src/commands/create_exe.rs
+++ lib/cli/src/commands/create_exe.rs
@@ -224,9 +224,7 @@ impl CreateExe {
             return Err(anyhow::anyhow!("input path cannot be a directory"));
         }
 
-        let (store, compiler_type) = self
-            .compiler
-            .get_store_for_target(Target::new(target_triple, Default::default()))?;
+        let (store, compiler_type) = self.compiler.get_store_for_target(target.clone())?;
         println!("Compiler: {}", compiler_type.to_string());
         println!("Target: {:?}", target);
         println!("Format: {:?}", object_format);
