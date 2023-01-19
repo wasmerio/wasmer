902670a721b43c34c63019b0dc4e67f481d832ff
Christoph Herzog <chris@theduke.at>
2023-01-19 18:19:57 +0100

    Restore changes from master
    Align with master on two changes which were probably accidentally
    screwed up during a merge or rebase.

--- lib/api/src/sys/module.rs
+++ lib/api/src/sys/module.rs
@@ -267,11 +267,11 @@ impl Module {
     /// # }
     /// ```
     pub unsafe fn deserialize(
-        store: &impl AsStoreRef,
+        engine: &impl AsEngineRef,
         bytes: impl IntoBytes,
     ) -> Result<Self, DeserializeError> {
         let bytes = bytes.into_bytes();
-        let artifact = store.as_store_ref().engine().deserialize(&bytes)?;
+        let artifact = engine.as_engine_ref().engine().deserialize(&bytes)?;
         Ok(Self::from_artifact(artifact))
     }
 
