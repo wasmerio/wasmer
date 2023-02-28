fc64202104a4f3471327d2d13bbccd7a6bb4eeee
Syrus Akbary <me@syrusakbary.com>
2023-02-28 09:24:21 -0800

    Use AtomicUsize instead of AtomicU64

--- lib/types/src/store_id.rs
+++ lib/types/src/store_id.rs
@@ -1,6 +1,6 @@
 use std::{
-    num::NonZeroU64,
-    sync::atomic::{AtomicU64, Ordering},
+    num::NonZeroUsize,
+    sync::atomic::{AtomicUsize, Ordering},
 };
 
 /// Unique ID to identify a context.
@@ -9,14 +9,14 @@ use std::{
 /// context. This is used to check that a handle is always used with the
 /// correct context.
 #[derive(Debug, Copy, Clone, Eq, PartialEq, Hash)]
-pub struct StoreId(NonZeroU64);
+pub struct StoreId(NonZeroUsize);
 
 impl Default for StoreId {
     // Allocates a unique ID for a new context.
     fn default() -> Self {
         // No overflow checking is needed here: overflowing this would take
         // thousands of years.
-        static NEXT_ID: AtomicU64 = AtomicU64::new(1);
-        Self(NonZeroU64::new(NEXT_ID.fetch_add(1, Ordering::Relaxed)).unwrap())
+        static NEXT_ID: AtomicUsize = AtomicUsize::new(1);
+        Self(NonZeroUsize::new(NEXT_ID.fetch_add(1, Ordering::Relaxed)).unwrap())
     }
 }
