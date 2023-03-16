a4b8eab5fc9978d3b921bd0432749c383bf18f63
Syrus Akbary <me@syrusakbary.com>
2023-03-16 15:31:12 -0700

    Improved linting a bit

--- lib/wasi/src/runtime/task_manager/tokio.rs
+++ lib/wasi/src/runtime/task_manager/tokio.rs
@@ -78,11 +78,10 @@ impl VirtualTaskManager for TokioTaskManager {
         match spawn_type {
             SpawnType::CreateWithType(mut mem) => {
                 mem.ty.shared = true;
-                Memory::new(store, mem.ty)
-                    .map_err(|err| {
-                        tracing::error!("could not create memory: {err}");
-                        WasiThreadError::MemoryCreateFailed
-                    })
+                Memory::new(store, mem.ty).map_err(|err| {
+                    tracing::error!("could not create memory: {err}");
+                    WasiThreadError::MemoryCreateFailed
+                })
             }
             SpawnType::NewThread(mem) => Ok(Some(mem)),
             SpawnType::Create => Ok(None),
