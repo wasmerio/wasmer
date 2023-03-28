b5d7a71f81b5eb200c6d1885425c7863e52e19cb
syrusakbary <syrusakbary@users.noreply.github.com>
2023-03-28 22:58:43 +0000

    deploy: bb9e8b063a994ab132deac9d732de030dd9ba750

--- crates/doc/wasmer_registry/package/sidebar-items.js
+++ crates/doc/wasmer_registry/package/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"constant":[["REGEX_PACKAGE_WITH_VERSION",""]],"struct":[["PACKAGE_WITH_VERSION",""],["Package",""]]};
+window.SIDEBAR_ITEMS = {"constant":[["REGEX_PACKAGE_WITH_VERSION",""]],"mod":[["builder",""]],"struct":[["PACKAGE_WITH_VERSION",""],["Package",""]]};
