557b66acd1a1736d8e8f8d3120c44518a9f06f0b
syrusakbary <syrusakbary@users.noreply.github.com>
2023-03-29 22:48:48 +0000

    deploy: 466d63ed466f16ebc335f50039e5c0aa5ad0d810

--- crates/doc/wasmer_wasix/runtime/sidebar-items.js
+++ crates/doc/wasmer_wasix/runtime/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"mod":[["task_manager",""]],"struct":[["DefaultTty",""],["PluggableRuntime",""]],"trait":[["WasiRuntime","Represents an implementation of the WASI runtime - by default everything is unimplemented."]],"type":[["ArcTunables",""]]};
+window.SIDEBAR_ITEMS = {"mod":[["task_manager",""]],"struct":[["DefaultTty",""],["PluggableRuntime",""]],"trait":[["WasiRuntime","Represents an implementation of the WASI runtime - by default everything is unimplemented."]]};
