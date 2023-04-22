f8848422255cb8523c5708238a9b0dc47440b008
Syrus Akbary <me@syrusakbary.com>
2023-04-21 23:42:47 -0700

    Improved error

--- lib/compiler/src/engine/trap/error.rs
+++ lib/compiler/src/engine/trap/error.rs
@@ -1,10 +1,11 @@
 use super::frame_info::{GlobalFrameInfo, FRAME_INFO};
 use backtrace::Backtrace;
+use std::convert::TryInto;
 use std::error::Error;
 use std::fmt;
 use std::sync::Arc;
-use wasmer_types::FrameInfo;
-use wasmer_vm::{Trap, TrapCode};
+use wasmer_types::{FrameInfo, TrapCode};
+use wasmer_vm::Trap;
 
 /// A struct representing an aborted instruction execution, with a message
 /// indicating the cause.
@@ -76,6 +77,7 @@ impl RuntimeError {
     /// assert_eq!("unexpected error", trap.message());
     /// ```
     pub fn new_from_source(source: Trap, wasm_trace: Vec<FrameInfo>) -> Self {
+        println!("CREATING ERROR FROM TRAP {}", source);
         Self {
             inner: Arc::new(RuntimeErrorInner { source, wasm_trace }),
         }
@@ -85,101 +87,15 @@ impl RuntimeError {
     ///
     /// This error object can be passed through Wasm frames and later retrieved
     /// using the `downcast` method.
-    #[cfg(feature = "std")]
     pub fn user(error: Box<dyn Error + Send + Sync>) -> Self {
-        match error.downcast::<Self>() {
-            // The error is already a RuntimeError, we return it directly
-            Ok(runtime_error) => *runtime_error,
-            Err(error) => {
-                let info = FRAME_INFO.read().unwrap();
-                Self::new_with_trace(&info, Trap::user(error))
-            }
-        }
-    }
-
-    /// Creates a custom user Error.
-    ///
-    /// This error object can be passed through Wasm frames and later retrieved
-    /// using the `downcast` method.
-    #[cfg(feature = "core")]
-    pub fn user(error: Box<dyn CoreError + Send + Sync>) -> Self {
-        match error.downcast::<Self>() {
-            // The error is already a RuntimeError, we return it directly
-            Ok(runtime_error) => *runtime_error,
-            Err(error) => {
-                let info = FRAME_INFO.read().unwrap();
-                Self::new_with_trace(&info, Trap::user(error))
-            }
-        }
-    }
-
-    fn wasm_trace(
-        info: &GlobalFrameInfo,
-        trap_pc: Option<usize>,
-        backtrace: &Backtrace,
-    ) -> Vec<FrameInfo> {
-        // Let's construct the trace
-        backtrace
-            .frames()
-            .iter()
-            .filter_map(|frame| {
-                let pc = frame.ip() as usize;
-                if pc == 0 {
-                    None
-                } else {
-                    // Note that we need to be careful about the pc we pass in here to
-                    // lookup frame information. This program counter is used to
-                    // translate back to an original source location in the origin wasm
-                    // module. If this pc is the exact pc that the trap happened at,
-                    // then we look up that pc precisely. Otherwise backtrace
-                    // information typically points at the pc *after* the call
-                    // instruction (because otherwise it's likely a call instruction on
-                    // the stack). In that case we want to lookup information for the
-                    // previous instruction (the call instruction) so we subtract one as
-                    // the lookup.
-                    let pc_to_lookup = if Some(pc) == trap_pc { pc } else { pc - 1 };
-                    Some(pc_to_lookup)
-                }
-            })
-            .filter_map(|pc| info.lookup_frame_info(pc))
-            .collect::<Vec<_>>()
-    }
-
-    fn new_with_trace(info: &GlobalFrameInfo, source: Trap) -> Self {
-        let wasm_trace: Vec<FrameInfo> = match &source {
-            // A user error
-            Trap::User(_) => Self::wasm_trace(info, None, &Backtrace::new_unresolved()),
-            // A trap caused by the VM being Out of Memory
-            Trap::OOM { backtrace } => Self::wasm_trace(info, None, backtrace),
-            // A trap caused by an error on the generated machine code for a Wasm function
-            Trap::Wasm {
-                pc,
-                signal_trap: _,
-                backtrace,
-            } => Self::wasm_trace(info, Some(*pc), backtrace),
-            // A trap triggered manually from the Wasmer runtime
-            Trap::Lib {
-                trap_code: _,
-                backtrace,
-            } => Self::wasm_trace(info, None, backtrace),
-        };
-
-        Self::new_from_source(source, wasm_trace)
+        // if error.is::<RuntimeError>() {
+        //     return *error.downcast::<RuntimeError>().unwrap();
+        // }
+        error.into()
     }
 
     /// Returns a reference the `message` stored in `Trap`.
     pub fn message(&self) -> String {
-        self.inner.source.to_string()
-    }
-
-    /// Returns a list of function frames in WebAssembly code that led to this
-    /// trap happening.
-    pub fn trace(&self) -> &[FrameInfo] {
-        &self.inner.wasm_trace
-    }
-
-    /// Returns trap code, if it's a Trap
-    pub fn to_trap(self) -> Option<TrapCode> {
         match self.inner.source {
             Trap::Wasm {
                 pc, signal_trap, ..
@@ -190,10 +106,23 @@ impl RuntimeError {
                     .map_or(signal_trap.unwrap_or(TrapCode::StackOverflow), |info| {
                         info.trap_code
                     });
-                Some(code)
+                code.message().to_string()
             }
-            _ => None,
+            Trap::Lib { trap_code, .. } => trap_code.message().to_string(),
+            _ => self.inner.source.to_string(),
         }
+        // self.inner.source.to_string()
+    }
+
+    /// Returns a list of function frames in WebAssembly code that led to this
+    /// trap happening.
+    pub fn trace(&self) -> &[FrameInfo] {
+        &self.inner.wasm_trace
+    }
+
+    /// Returns trap code, if it's a Trap
+    pub fn to_trap(self) -> Option<TrapCode> {
+        self.try_into().ok()
     }
 
     /// Attempts to downcast the `RuntimeError` to a concrete type.
@@ -262,15 +191,111 @@ impl std::error::Error for RuntimeError {
     fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
         match &self.inner.source {
             Trap::User(err) => Some(&**err),
-            // RuntimeErrorSource::Trap(err) => Some(err),
             _ => None,
+        }
+    }
+}
+
+#[cfg(feature = "std")]
+impl From<Box<dyn Error + Send + Sync>> for RuntimeError {
+    fn from(error: Box<dyn Error + Send + Sync>) -> Self {
+        match error.downcast::<Self>() {
+            // The error is already a RuntimeError, we return it directly
+            Ok(runtime_error) => *runtime_error,
+            Err(error) => Trap::user(error).into(),
+        }
+    }
+}
+
+#[cfg(feature = "core")]
+impl From<Box<dyn CoreError + Send + Sync>> for RuntimeError {
+    fn from(error: Box<dyn Error + Send + Sync>) -> Self {
+        match error.downcast::<Self>() {
+            // The error is already a RuntimeError, we return it directly
+            Ok(runtime_error) => *runtime_error,
+            Err(error) => Trap::user(error).into(),
+        }
+    }
+}
+
+// ------
+
+impl TryInto<TrapCode> for RuntimeError {
+    type Error = Self;
+    fn try_into(self) -> Result<TrapCode, Self> {
+        match self.inner.source {
+            Trap::Wasm {
+                pc, signal_trap, ..
+            } => {
+                let info = FRAME_INFO.read().unwrap();
+                let code: TrapCode = info
+                    .lookup_trap_info(pc)
+                    .map_or(signal_trap.unwrap_or(TrapCode::StackOverflow), |info| {
+                        info.trap_code
+                    });
+                Ok(code)
+            }
+            _ => Err(self),
         }
     }
 }
 
 impl From<Trap> for RuntimeError {
     fn from(trap: Trap) -> Self {
+        if trap.is::<RuntimeError>() {
+            return trap.downcast::<RuntimeError>().unwrap();
+        }
         let info = FRAME_INFO.read().unwrap();
-        Self::new_with_trace(&info, trap)
+        let wasm_trace: Vec<FrameInfo> = match &trap {
+            // A user error
+            Trap::User(_err) => wasm_trace(&info, None, &Backtrace::new_unresolved()),
+            // A trap caused by the VM being Out of Memory
+            Trap::OOM { backtrace } => wasm_trace(&info, None, backtrace),
+            // A trap caused by an error on the generated machine code for a Wasm function
+            Trap::Wasm {
+                pc,
+                signal_trap: _,
+                backtrace,
+            } => wasm_trace(&info, Some(*pc), backtrace),
+            // A trap triggered manually from the Wasmer runtime
+            Trap::Lib {
+                trap_code: _,
+                backtrace,
+            } => wasm_trace(&info, None, backtrace),
+        };
+
+        RuntimeError::new_from_source(trap, wasm_trace)
     }
+}
+
+fn wasm_trace(
+    info: &GlobalFrameInfo,
+    trap_pc: Option<usize>,
+    backtrace: &Backtrace,
+) -> Vec<FrameInfo> {
+    // Let's construct the trace
+    backtrace
+        .frames()
+        .iter()
+        .filter_map(|frame| {
+            let pc = frame.ip() as usize;
+            if pc == 0 {
+                None
+            } else {
+                // Note that we need to be careful about the pc we pass in here to
+                // lookup frame information. This program counter is used to
+                // translate back to an original source location in the origin wasm
+                // module. If this pc is the exact pc that the trap happened at,
+                // then we look up that pc precisely. Otherwise backtrace
+                // information typically points at the pc *after* the call
+                // instruction (because otherwise it's likely a call instruction on
+                // the stack). In that case we want to lookup information for the
+                // previous instruction (the call instruction) so we subtract one as
+                // the lookup.
+                let pc_to_lookup = if Some(pc) == trap_pc { pc } else { pc - 1 };
+                Some(pc_to_lookup)
+            }
+        })
+        .filter_map(|pc| info.lookup_frame_info(pc))
+        .collect::<Vec<_>>()
 }
