82bf00e3bfddbf7c3fcff2512efa28423947b86d
Michael-F-Bryan <michael@wasmer.io>
2023-05-16 04:26:36 +0800

    Added a basic implementation for load_package_tree()

--- lib/wasi/src/runtime/package_loader/mod.rs
+++ lib/wasi/src/runtime/package_loader/mod.rs
@@ -1,37 +1,7 @@
 mod builtin_loader;
+mod load_package_tree;
+mod types;
 
-pub use self::builtin_loader::BuiltinLoader;
-
-use std::{fmt::Debug, ops::Deref};
-
-use anyhow::Error;
-use webc::compat::Container;
-
-use crate::{
-    bin_factory::BinaryPackage,
-    runtime::resolver::{Resolution, Summary},
+pub use self::{
+    builtin_loader::BuiltinLoader, load_package_tree::load_package_tree, types::PackageLoader,
 };
-
-#[async_trait::async_trait]
-pub trait PackageLoader: Debug {
-    async fn load(&self, summary: &Summary) -> Result<Container, Error>;
-}
-
-#[async_trait::async_trait]
-impl<D, P> PackageLoader for D
-where
-    D: Deref<Target = P> + Debug + Send + Sync,
-    P: PackageLoader + Send + Sync + ?Sized + 'static,
-{
-    async fn load(&self, summary: &Summary) -> Result<Container, Error> {
-        (**self).load(summary).await
-    }
-}
-
-/// Given a fully resolved package, load it into memory for execution.
-pub async fn load_package_tree(
-    _loader: &impl PackageLoader,
-    _resolution: &Resolution,
-) -> Result<BinaryPackage, Error> {
-    todo!();
-}
