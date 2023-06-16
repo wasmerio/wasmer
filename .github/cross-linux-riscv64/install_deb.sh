96c81f2ceca60b586a4b792da56ab15cfb18aeb8
ptitSeb <sebastien.chev@gmail.com>
2023-06-16 10:53:37 +0200

    [RISCV64][CI] Use simpler way to install riscv64 packages

--- .github/cross-linux-riscv64/install_deb.sh
+++ .github/cross-linux-riscv64/install_deb.sh
@@ -15,30 +15,30 @@ apt-get install --assume-yes --no-install-recommends \
   sharutils \
   gnupg
 
-# Add port from sid to get some riscv packages
-debsource="deb http://deb.debian.org/debian-ports sid main"
-
-# temporarily use debian sources rather than ubuntu.
-touch /etc/apt/sources.list
-mv /etc/apt/sources.list /etc/apt/sources.list.bak
-echo -e "${debsource}" > /etc/apt/sources.list
+## Add port from bookworm to get some riscv packages
+#debsource="deb http://deb.debian.org/debian-ports bookworm main"
+#
+## temporarily use debian sources rather than ubuntu.
+#touch /etc/apt/sources.list
+#mv /etc/apt/sources.list /etc/apt/sources.list.bak
+#echo -e "${debsource}" > /etc/apt/sources.list
 
 dpkg --add-architecture "${arch}" || echo "foreign-architecture ${arch}" \
   > /etc/dpkg/dpkg.cfg.d/multiarch
 
 # Add Debian keys.
-curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{7.0,8,9,10}.asc' -O
-curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{8,9,10}-security.asc' -O
-curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/release-{7,8,9,10}.asc' -O
-curl --retry 3 -sSfL 'https://www.ports.debian.org/archive_{2020,2021,2022,2023}.key' -O
-
-for key in *.asc *.key; do
-  apt-key add "${key}"
-  rm "${key}"
-done
-
-# allow apt-get to retry downloads
-echo 'APT::Acquire::Retries "3";' > /etc/apt/apt.conf.d/80-retries
+#curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{7.0,8,9,10,11}.asc' -O
+#curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/archive-key-{8,9,10,11}-security.asc' -O
+#curl --retry 3 -sSfL 'https://ftp-master.debian.org/keys/release-{7,8,9,10,11}.asc' -O
+#curl --retry 3 -sSfL 'https://www.ports.debian.org/archive_{2020,2021,2022,2023}.key' -O
+#
+#for key in *.asc *.key; do
+#  apt-key add "${key}"
+#  rm "${key}"
+#done
+#
+## allow apt-get to retry downloads
+#echo 'APT::Acquire::Retries "3";' > /etc/apt/apt.conf.d/80-retries
 
 apt-get update
 for dep in $@; do
