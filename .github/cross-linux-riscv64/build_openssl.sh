bd031799024e196fc7a7f76b3afefa41c0e23681
ptitSeb <sebastien.chev@gmail.com>
2023-06-16 14:38:42 +0200

    [RISCV64][CI] Fixed typos

--- .github/cross-linux-riscv64/build_openssl.sh
+++ .github/cross-linux-riscv64/build_openssl.sh
@@ -11,6 +11,7 @@ rm openssl-3.1.1.tar.gz
 cd openssl-3.1.1
 
 AR=riscv64-linux-gnu-ar NM=riscv64-linux-gnu-nm CC=riscv64-linux-gnu-gcc \
-   ./Configure -static no-asm no-tests --prefix=/opensll_riscv64 linux64-riscv64
+   ./Configure -static no-asm no-tests --prefix=/openssl_riscv64 linux64-riscv64
+
 make -j
 make install
