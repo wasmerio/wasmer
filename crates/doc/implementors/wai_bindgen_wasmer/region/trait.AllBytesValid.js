8205a7a984945c0901b5ea545bf35d3083180a56
syrusakbary <syrusakbary@users.noreply.github.com>
2023-06-19 12:22:55 +0000

    deploy: 592e34b23f4d5369e51338a3b64d0eebd4e49631

--- crates/doc/implementors/wai_bindgen_wasmer/region/trait.AllBytesValid.js
+++ crates/doc/implementors/wai_bindgen_wasmer/region/trait.AllBytesValid.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
 "wai_bindgen_wasmer":[],
-"wasmer_wasix":[["impl <a class=\"trait\" href=\"wai_bindgen_wasmer/region/trait.AllBytesValid.html\" title=\"trait wai_bindgen_wasmer::region::AllBytesValid\">AllBytesValid</a> for <a class=\"struct\" href=\"wasmer_wasix/bindings/wasix_http_client_v1/struct.RedirectFollow.html\" title=\"struct wasmer_wasix::bindings::wasix_http_client_v1::RedirectFollow\">RedirectFollow</a>"]]
+"wasmer_wasix":[["impl AllBytesValid for <a class=\"struct\" href=\"wasmer_wasix/bindings/wasix_http_client_v1/struct.RedirectFollow.html\" title=\"struct wasmer_wasix::bindings::wasix_http_client_v1::RedirectFollow\">RedirectFollow</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
