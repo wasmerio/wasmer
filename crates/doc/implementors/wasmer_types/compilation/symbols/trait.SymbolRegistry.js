a2cec22923b773bc72e80bac59f9bdb611568fd4
ptitSeb <ptitSeb@users.noreply.github.com>
2023-06-29 16:41:01 +0000

    deploy: c7b0e7f7457a6d7370650e9e6d44f7191d3220be

--- crates/doc/implementors/wasmer_types/compilation/symbols/trait.SymbolRegistry.js
+++ crates/doc/implementors/wasmer_types/compilation/symbols/trait.SymbolRegistry.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"wasmer_compiler_llvm":[["impl SymbolRegistry for <a class=\"struct\" href=\"wasmer_compiler_llvm/compiler/struct.ShortNames.html\" title=\"struct wasmer_compiler_llvm::compiler::ShortNames\">ShortNames</a>"]],
+"wasmer_compiler_llvm":[["impl <a class=\"trait\" href=\"wasmer_types/compilation/symbols/trait.SymbolRegistry.html\" title=\"trait wasmer_types::compilation::symbols::SymbolRegistry\">SymbolRegistry</a> for <a class=\"struct\" href=\"wasmer_compiler_llvm/compiler/struct.ShortNames.html\" title=\"struct wasmer_compiler_llvm::compiler::ShortNames\">ShortNames</a>"]],
 "wasmer_types":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
