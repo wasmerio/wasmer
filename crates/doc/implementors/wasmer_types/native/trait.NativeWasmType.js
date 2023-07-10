61d48632a628ce0d2bf7d83b3fd959a214e31907
Michael-F-Bryan <Michael-F-Bryan@users.noreply.github.com>
2023-07-10 04:30:14 +0000

    deploy: 8f02600fd6d54ee78099d80ca2ff6bfd2e9e4209

--- crates/doc/implementors/wasmer_types/native/trait.NativeWasmType.js
+++ crates/doc/implementors/wasmer_types/native/trait.NativeWasmType.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"wasmer":[["impl <a class=\"trait\" href=\"wasmer_types/native/trait.NativeWasmType.html\" title=\"trait wasmer_types::native::NativeWasmType\">NativeWasmType</a> for <a class=\"struct\" href=\"wasmer/extern_ref/struct.ExternRef.html\" title=\"struct wasmer::extern_ref::ExternRef\">ExternRef</a>"],["impl <a class=\"trait\" href=\"wasmer_types/native/trait.NativeWasmType.html\" title=\"trait wasmer_types::native::NativeWasmType\">NativeWasmType</a> for <a class=\"struct\" href=\"wasmer/externals/function/struct.Function.html\" title=\"struct wasmer::externals::function::Function\">Function</a>"]],
+"wasmer":[["impl NativeWasmType for <a class=\"struct\" href=\"wasmer/extern_ref/struct.ExternRef.html\" title=\"struct wasmer::extern_ref::ExternRef\">ExternRef</a>"],["impl NativeWasmType for <a class=\"struct\" href=\"wasmer/externals/function/struct.Function.html\" title=\"struct wasmer::externals::function::Function\">Function</a>"]],
 "wasmer_types":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
