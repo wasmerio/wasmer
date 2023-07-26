e331c314d2eae35ff36eed230d7e5787a975bbaf
ptitSeb <ptitSeb@users.noreply.github.com>
2023-07-26 18:54:40 +0000

    deploy: edc4a52565b92ef4f1073d05aea070291b97663a

--- crates/doc/implementors/virtual_mio/interest/trait.InterestHandler.js
+++ crates/doc/implementors/virtual_mio/interest/trait.InterestHandler.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {
 "virtual_mio":[],
 "virtual_net":[],
-"wasmer_wasix":[["impl InterestHandler for <a class=\"struct\" href=\"wasmer_wasix/syscalls/wasix/epoll_ctl/struct.EpollHandler.html\" title=\"struct wasmer_wasix::syscalls::wasix::epoll_ctl::EpollHandler\">EpollHandler</a>"]]
+"wasmer_wasix":[["impl <a class=\"trait\" href=\"virtual_mio/interest/trait.InterestHandler.html\" title=\"trait virtual_mio::interest::InterestHandler\">InterestHandler</a> for <a class=\"struct\" href=\"wasmer_wasix/syscalls/wasix/epoll_ctl/struct.EpollHandler.html\" title=\"struct wasmer_wasix::syscalls::wasix::epoll_ctl::EpollHandler\">EpollHandler</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- crates/doc/wasmer_wasix/runtime/task_manager/tokio/sidebar-items.js
+++ crates/doc/wasmer_wasix/runtime/task_manager/tokio/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":[["TokioRuntimeGuard",""],["TokioTaskManager","A task manager that uses tokio to spawn tasks."]]};
+window.SIDEBAR_ITEMS = {"enum":[["RuntimeOrHandle",""]],"struct":[["TokioRuntimeGuard",""],["TokioTaskManager","A task manager that uses tokio to spawn tasks."]]};
