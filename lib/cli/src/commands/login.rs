94829e859d7311191dd26e7f839829e52e66bacb
Syrus Akbary <me@syrusakbary.com>
2023-07-27 15:54:46 +0200

    Update login.rs

--- lib/cli/src/commands/login.rs
+++ lib/cli/src/commands/login.rs
@@ -251,9 +251,7 @@ impl Login {
                                     .with_prompt(format!(
                                         "{} {} - [y/{}]",
                                         style("?").yellow().bold(),
-                                        style("Do you want to login again?")
-                                            .bright()
-                                            .bold(),
+                                        style("Do you want to login again?").bright().bold(),
                                         style("N").green().bold()
                                     ))
                                     .show_default(false)
