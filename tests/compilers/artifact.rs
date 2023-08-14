f3ff26f52d1dc5ec1025384addb32f37415e87f0
Arshia Ghafoori <arshia001@live.com>
2023-08-14 10:25:37 +0400

    Fix deserialization roundtrip test on windows

--- tests/compilers/artifact.rs
+++ tests/compilers/artifact.rs
@@ -1,5 +1,6 @@
 use std::{fs, path::PathBuf};
 
+use cfg_if::cfg_if;
 use wasmer::{Engine, Module};
 
 #[test]
@@ -21,10 +22,21 @@ fn artifact_serialization_roundtrip() {
 
 #[test]
 fn artifact_deserialization_roundtrip() {
+    // This test is included to make sure we don't break the serialized format
+    // by mistake. Otherwise, everything in this test is already tested in
+    // `artifact_serialization_roundtrip`.
     let file_names = ["bash.wasmu", "cowsay.wasmu", "python-3.11.3.wasmu"];
 
+    cfg_if!(
+        if #[cfg(target_os = "windows")] {
+            let base_path = "tests/compilers/wasmu/windows";
+        } else {
+            let base_path = "tests/compilers/wasmu/linux";
+        }
+    );
+
     for file_name in file_names {
-        let path = PathBuf::from("tests/compilers/wasmu").join(file_name);
+        let path = PathBuf::from(base_path).join(file_name);
         let wasm_module_bytes = fs::read(path).unwrap();
         let engine = Engine::default();
         let module = unsafe { Module::deserialize(&engine, wasm_module_bytes.clone()) }.unwrap();
