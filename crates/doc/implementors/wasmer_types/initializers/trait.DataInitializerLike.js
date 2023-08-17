b929cd2719969453851632c88af0df754fb84770
syrusakbary <syrusakbary@users.noreply.github.com>
2023-08-17 19:14:17 +0000

    deploy: cb39704e5f6da6c19378b54538372a933aa3ea31

--- crates/doc/implementors/wasmer_types/initializers/trait.DataInitializerLike.js
+++
@@ -1,4 +0,0 @@
-(function() {var implementors = {
-"wasmer_compiler":[["impl&lt;'a&gt; DataInitializerLike&lt;'a&gt; for <a class=\"enum\" href=\"wasmer_compiler/engine/artifact/enum.OwnedDataInitializerVariant.html\" title=\"enum wasmer_compiler::engine::artifact::OwnedDataInitializerVariant\">OwnedDataInitializerVariant</a>&lt;'a&gt;"]],
-"wasmer_types":[]
-};if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
