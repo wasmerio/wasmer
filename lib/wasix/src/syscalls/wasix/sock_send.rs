85a3551ae29f5deeef84533ce2da589d78cfc274
ptitSeb <sebastien.chev@gmail.com>
2023-09-22 19:17:04 +0100

    Use write instead of send for pipe, to accomodate socketpair (#4224)
    * Use write instead of send for pipe, to accomodate socketpair
    
    * Also handling sock_recv for pipe
    
    * happy clippy

--- lib/wasix/src/syscalls/wasix/sock_send.rs
+++ lib/wasix/src/syscalls/wasix/sock_send.rs
@@ -25,7 +25,16 @@ pub fn sock_send<M: MemorySize>(
     si_flags: SiFlags,
     ret_data_len: WasmPtr<M::Offset, M>,
 ) -> Result<Errno, WasiError> {
-    sock_send_internal(ctx, sock, si_data, si_data_len, si_flags, ret_data_len)
+    let env = ctx.data();
+    let fd_entry = env.state.fs.get_fd(sock).unwrap();
+    let guard = fd_entry.inode.read();
+    let use_write = matches!(guard.deref(), Kind::Pipe { .. });
+    drop(guard);
+    if use_write {
+        fd_write(ctx, sock, si_data, si_data_len, ret_data_len)
+    } else {
+        sock_send_internal(ctx, sock, si_data, si_data_len, si_flags, ret_data_len)
+    }
 }
 
 pub(super) fn sock_send_internal<M: MemorySize>(
