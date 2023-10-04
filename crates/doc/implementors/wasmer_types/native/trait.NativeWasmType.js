1f5435c5ee2b5c984a98a92cfaeed60573077c9d
ptitSeb <ptitSeb@users.noreply.github.com>
2023-10-04 13:22:20 +0000

    deploy: 1b10c61a7a5a9db7c074ca5e8715257a097f0ad4

--- crates/doc/implementors/wasmer_types/native/trait.NativeWasmType.js
+++ crates/doc/implementors/wasmer_types/native/trait.NativeWasmType.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"wasmer":[["impl NativeWasmType for <a class=\"struct\" href=\"wasmer/extern_ref/struct.ExternRef.html\" title=\"struct wasmer::extern_ref::ExternRef\">ExternRef</a>"],["impl NativeWasmType for <a class=\"struct\" href=\"wasmer/externals/function/struct.Function.html\" title=\"struct wasmer::externals::function::Function\">Function</a>"]],
+"wasmer":[["impl NativeWasmType for <a class=\"struct\" href=\"wasmer/externals/function/struct.Function.html\" title=\"struct wasmer::externals::function::Function\">Function</a>"],["impl NativeWasmType for <a class=\"struct\" href=\"wasmer/extern_ref/struct.ExternRef.html\" title=\"struct wasmer::extern_ref::ExternRef\">ExternRef</a>"]],
 "wasmer_types":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
