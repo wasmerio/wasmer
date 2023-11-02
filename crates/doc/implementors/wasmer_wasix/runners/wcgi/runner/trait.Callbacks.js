49efe79c55a563c5e634657cb65e6703797e9216
theduke <theduke@users.noreply.github.com>
2023-11-02 13:44:13 +0000

    deploy: dcc9cc1f1b27126e212a910d181f8a84cfac6e61

--- crates/doc/implementors/wasmer_wasix/runners/wcgi/runner/trait.Callbacks.js
+++ crates/doc/implementors/wasmer_wasix/runners/wcgi/runner/trait.Callbacks.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"wasmer_cli":[["impl Callbacks for <a class=\"struct\" href=\"wasmer_cli/commands/run/struct.Callbacks.html\" title=\"struct wasmer_cli::commands::run::Callbacks\">Callbacks</a>"]],
+"wasmer_cli":[["impl <a class=\"trait\" href=\"wasmer_wasix/runners/wcgi/runner/trait.Callbacks.html\" title=\"trait wasmer_wasix::runners::wcgi::runner::Callbacks\">Callbacks</a> for <a class=\"struct\" href=\"wasmer_cli/commands/run/struct.Callbacks.html\" title=\"struct wasmer_cli::commands::run::Callbacks\">Callbacks</a>"]],
 "wasmer_wasix":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
