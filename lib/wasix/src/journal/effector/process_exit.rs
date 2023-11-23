cc95b8787eb1d86f168eef06299b53a1d1b1ea72
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2023-11-24 01:06:05 +1100

    Fixed the process exit record replay

--- lib/wasix/src/journal/effector/process_exit.rs
+++ lib/wasix/src/journal/effector/process_exit.rs
@@ -1,3 +1,5 @@
+use virtual_mio::InlineWaker;
+
 use super::*;
 
 impl JournalEffector {
@@ -9,7 +11,15 @@ impl JournalEffector {
     }
 
     pub fn apply_process_exit(env: &WasiEnv, exit_code: Option<ExitCode>) -> anyhow::Result<()> {
-        env.blocking_on_exit(exit_code);
+        // If we are in the phase of replaying journals then we
+        // close all the file descriptors but we don't actually send
+        // any signals
+        if env.replaying_journal {
+            let state = env.state.clone();
+            InlineWaker::block_on(state.fs.close_all());
+        } else {
+            env.blocking_on_exit(exit_code);
+        }
         Ok(())
     }
 }
