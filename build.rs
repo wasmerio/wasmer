8143848b3f0092f09864361c70b22dd6055de54a
Syrus Akbary <me@syrusakbary.com>
2023-11-24 18:00:26 -0800

    Remove fully wasi-wast

--- build.rs
+++ build.rs
@@ -14,15 +14,6 @@ use test_generator::{
 };
 
 fn main() -> anyhow::Result<()> {
-    // As rerun-if-changed doesn't support globs, we use another crate
-    // to check changes in directories.
-    build_deps::rerun_if_changed_paths("tests/wasi-wast/wasi/unstable/*")
-        .expect("Can't get directory");
-    build_deps::rerun_if_changed_paths("tests/wasi-wast/wasi/snapshot1/*")
-        .expect("Can't get directory");
-    build_deps::rerun_if_changed_paths("tests/wasi-wast/wasi/nightly-2022-10-18/*")
-        .expect("Can't get directory");
-
     let out_dir = PathBuf::from(
         env::var_os("OUT_DIR").expect("The OUT_DIR environment variable must be set"),
     );
@@ -71,33 +62,6 @@ fn main() -> anyhow::Result<()> {
             buffer: String::new(),
             path: vec![],
         };
-
-        with_test_module(&mut wasitests, "wasitests", |wasitests| {
-            for wasi_version in &["unstable", "snapshot1", "nightly_2022_10_18"] {
-                with_test_module(wasitests, wasi_version, |wasitests| {
-                    for (wasi_filesystem_test_name, wasi_filesystem_kind) in &[
-                        ("host_fs", "WasiFileSystemKind::Host"),
-                        ("mem_fs", "WasiFileSystemKind::InMemory"),
-                        ("tmp_fs", "WasiFileSystemKind::Tmp"),
-                        ("passthru_fs", "WasiFileSystemKind::PassthruMemory"),
-                        ("union_fs", "WasiFileSystemKind::UnionHostMemory"),
-                        ("root_fs", "WasiFileSystemKind::RootFileSystemBuilder"),
-                    ] {
-                        with_test_module(wasitests, wasi_filesystem_test_name, |wasitests| {
-                            test_directory(
-                                wasitests,
-                                format!("tests/wasi-wast/wasi/{}", wasi_version),
-                                |out, path| wasi_processor(out, path, wasi_filesystem_kind),
-                            )
-                        })?;
-                    }
-
-                    Ok(())
-                })?;
-            }
-
-            Ok(())
-        })?;
 
         let wasitests_output = out_dir.join("generated_wasitests.rs");
         fs::write(&wasitests_output, wasitests.buffer)?;
