8f29361bd6179811406bfc1aac411454d0a90576
Syrus Akbary <me@syrusakbary.com>
2023-11-24 17:07:37 -0800

    Improved file_create

--- lib/wasix/src/runners/wasi_common.rs
+++ lib/wasix/src/runners/wasi_common.rs
@@ -1,5 +1,4 @@
 use std::{
-    collections::HashMap,
     path::{Path, PathBuf},
     sync::Arc,
 };
--- tests/wasi.fyi/wasi-libstd-test/fs_file_create.rs
+++ tests/wasi.fyi/wasi-libstd-test/fs_file_create.rs
@@ -1,6 +1,9 @@
 use std::fs;
 
 fn main() {
-  assert!(fs::File::create("fs_file_create.dir/new_file").is_ok());
-  assert!(fs::metadata("fs_file_create.dir/new_file").unwrap().is_file());
+    assert!(fs::File::create("fs_file_create.dir/new_file").is_ok());
+    assert!(fs::metadata("fs_file_create.dir/new_file")
+        .unwrap()
+        .is_file());
+    assert!(fs::remove_file("fs_file_create.dir/new_file").is_ok());
 }
