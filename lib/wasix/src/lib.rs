wasmerio/wasmer

commit 94f27f81f601773071bd64dcd91c4c6b0d7d4196

Stability improvements (#4204)
* Fixed an issue where the socket address family was being corrupted due
to padding fields being incorrect used.

* Added a comment

* Faster compiles for debug by using release version of cranelift

* Added additional fixes for some missing parts of sockets

* Fixed a blocking issue inside an async and added sha256 caching of webc files

* Faster checksums for debug

* Removed error messages when a WASI instance shuts down and doing cleanups in parallel

* Reduced the logging level on WasiEnv cleanup code

* Fixed linter, wasi-web build, Windows build, the tests failling because of the lack of tockio multi threaded runtime and test-js

---------

Co-authored-by: ptitSeb <sebastien.chev@gmail.com>

lib/wasix/src/runtime/package_loader/builtin_loader.rs
@@ -239,7 +239,11 @@ impl FileSystemCache {
     async fn lookup(&self, hash: &WebcHash) -> Result<Option<Container>, Error> {
         let path = self.path(hash);
 
-        match Container::from_disk(&path) {
+        #[cfg(target_arch = "wasm32")]
+        let container = Container::from_disk(&path);
+        #[cfg(not(target_arch = "wasm32"))]
+        let container = tokio::task::block_in_place(|| Container::from_disk(&path));
+        match container {
             Ok(c) => Ok(Some(c)),
             Err(ContainerError::Open { error, .. })
             | Err(ContainerError::Read { error, .. })
@@ -371,8 +375,7 @@ mod tests {
         }
     }
 
-    #[tokio::test]
-    async fn cache_misses_will_trigger_a_download() {
+    async fn cache_misses_will_trigger_a_download_internal() {
         let temp = TempDir::new().unwrap();
         let client = Arc::new(DummyClient::with_responses([HttpResponse {
             body: Some(PYTHON.to_vec()),
@@ -420,5 +423,17 @@ mod tests {
         // and cached in memory for next time
         let in_memory = loader.in_memory.0.read().unwrap();
         assert!(in_memory.contains_key(&summary.dist.webc_sha256));
+    }
+
+    #[cfg(not(target_arch = "wasm32"))]
+    #[tokio::test(flavor = "multi_thread")]
+    async fn cache_misses_will_trigger_a_download() {
+        cache_misses_will_trigger_a_download_internal().await
+    }
+
+    #[cfg(target_arch = "wasm32")]
+    #[tokio::test()]
+    async fn cache_misses_will_trigger_a_download() {
+        cache_misses_will_trigger_a_download_internal().await
     }
 }

lib/wasix/src/runtime/resolver/filesystem_source.rs
@@ -23,10 +23,17 @@ impl Source for FileSystemSource {
             _ => return Err(QueryError::Unsupported),
         };
 
-        // FIXME: These two operations will block
+        #[cfg(target_arch = "wasm32")]
         let webc_sha256 = WebcHash::for_file(&path)
             .with_context(|| format!("Unable to hash \"{}\"", path.display()))?;
+        #[cfg(not(target_arch = "wasm32"))]
+        let webc_sha256 = tokio::task::block_in_place(|| WebcHash::for_file(&path))
+            .with_context(|| format!("Unable to hash \"{}\"", path.display()))?;
+        #[cfg(target_arch = "wasm32")]
         let container = Container::from_disk(&path)
+            .with_context(|| format!("Unable to parse \"{}\"", path.display()))?;
+        #[cfg(not(target_arch = "wasm32"))]
+        let container = tokio::task::block_in_place(|| Container::from_disk(&path))
             .with_context(|| format!("Unable to parse \"{}\"", path.display()))?;
 
         let url = crate::runtime::resolver::utils::url_from_file_path(&path)

lib/wasix/src/runtime/resolver/web_source.rs
@@ -240,13 +240,20 @@ impl Source for WebSource {
             .await
             .context("Unable to get the locally cached file")?;
 
-        // FIXME: this will block
+        #[cfg(target_arch = "wasm32")]
         let webc_sha256 = WebcHash::for_file(&local_path)
+            .with_context(|| format!("Unable to hash \"{}\"", local_path.display()))?;
+        #[cfg(not(target_arch = "wasm32"))]
+        let webc_sha256 = tokio::task::block_in_place(|| WebcHash::for_file(&local_path))
             .with_context(|| format!("Unable to hash \"{}\"", local_path.display()))?;
 
         // Note: We want to use Container::from_disk() rather than the bytes
         // our HTTP client gave us because then we can use memory-mapped files
+        #[cfg(target_arch = "wasm32")]
         let container = Container::from_disk(&local_path)
+            .with_context(|| format!("Unable to load \"{}\"", local_path.display()))?;
+        #[cfg(not(target_arch = "wasm32"))]
+        let container = tokio::task::block_in_place(|| Container::from_disk(&local_path))
             .with_context(|| format!("Unable to load \"{}\"", local_path.display()))?;
         let pkg = PackageInfo::from_manifest(container.manifest())
             .context("Unable to determine the package's metadata")?;
@@ -447,8 +454,7 @@ mod tests {
         }
     }
 
-    #[tokio::test]
-    async fn empty_cache_does_a_full_download() {
+    async fn empty_cache_does_a_full_download_internal() {
         let dummy_etag = "This is an etag";
         let temp = TempDir::new().unwrap();
         let client = DummyClient::with_responses([ResponseBuilder::new()
@@ -472,9 +478,18 @@ mod tests {
         assert_eq!(std::fs::read_to_string(etag_path).unwrap(), dummy_etag);
         assert_eq!(std::fs::read(path).unwrap(), PYTHON);
     }
+    #[cfg(not(target_arch = "wasm32"))]
+    #[tokio::test(flavor = "multi_thread")]
+    async fn empty_cache_does_a_full_download() {
+        empty_cache_does_a_full_download_internal().await
+    }
+    #[cfg(target_arch = "wasm32")]
+    #[tokio::test()]
+    async fn empty_cache_does_a_full_download() {
+        empty_cache_does_a_full_download_internal().await
+    }
 
-    #[tokio::test]
-    async fn cache_hit() {
+    async fn cache_hit_internal() {
         let temp = TempDir::new().unwrap();
         let client = Arc::new(DummyClient::with_responses([]));
         let source = WebSource::new(temp.path(), client.clone());
@@ -490,9 +505,18 @@ mod tests {
         // And no requests were sent
         assert_eq!(client.requests.lock().unwrap().len(), 0);
     }
+    #[cfg(not(target_arch = "wasm32"))]
+    #[tokio::test(flavor = "multi_thread")]
+    async fn cache_hit() {
+        cache_hit_internal().await
+    }
+    #[cfg(target_arch = "wasm32")]
+    #[tokio::test()]
+    async fn cache_hit() {
+        cache_hit_internal().await
+    }
 
-    #[tokio::test]
-    async fn fall_back_to_stale_cache_if_request_fails() {
+    async fn fall_back_to_stale_cache_if_request_fails_internal() {
         let temp = TempDir::new().unwrap();
         let client = Arc::new(DummyClient::with_responses([ResponseBuilder::new()
             .with_status(StatusCode::INTERNAL_SERVER_ERROR)
@@ -513,9 +537,18 @@ mod tests {
         // The etag file wasn't written
         assert!(!python_path.with_extension("etag").exists());
     }
+    #[cfg(not(target_arch = "wasm32"))]
+    #[tokio::test(flavor = "multi_thread")]
+    async fn fall_back_to_stale_cache_if_request_fails() {
+        fall_back_to_stale_cache_if_request_fails_internal().await
+    }
+    #[cfg(target_arch = "wasm32")]
+    #[tokio::test()]
+    async fn fall_back_to_stale_cache_if_request_fails() {
+        fall_back_to_stale_cache_if_request_fails_internal().await
+    }
 
-    #[tokio::test]
-    async fn download_again_if_etag_is_different() {
+    async fn download_again_if_etag_is_different_internal() {
         let temp = TempDir::new().unwrap();
         let client = Arc::new(DummyClient::with_responses([
             ResponseBuilder::new().with_etag("coreutils").build(),
@@ -548,5 +581,15 @@ mod tests {
             std::fs::read_to_string(path.with_extension("etag")).unwrap(),
             "coreutils"
         );
+    }
+    #[cfg(not(target_arch = "wasm32"))]
+    #[tokio::test(flavor = "multi_thread")]
+    async fn download_again_if_etag_is_different() {
+        download_again_if_etag_is_different_internal().await
+    }
+    #[cfg(target_arch = "wasm32")]
+    #[tokio::test()]
+    async fn download_again_if_etag_is_different() {
+        download_again_if_etag_is_different_internal().await
     }
 }

lib/virtual-net/src/lib.rs
@@ -573,6 +573,24 @@ pub trait VirtualTcpSocket: VirtualConnectedSocket + fmt::Debug + Send + Sync + 
     /// latency but increases encapsulation overhead.
     fn nodelay(&self) -> Result<bool>;
 
+    /// When KEEP_ALIVE is set the connection will periodically send
+    /// an empty data packet to the server to make sure the connection
+    /// stays alive.
+    fn set_keepalive(&mut self, keepalive: bool) -> Result<()>;
+
+    /// Indicates if the KEEP_ALIVE flag is set which means that the
+    /// socket will periodically send an empty data packet to keep
+    /// the connection alive.
+    fn keepalive(&self) -> Result<bool>;
+
+    /// When DONT_ROUTE is set the packet will be sent directly
+    /// to the interface without passing through the routing logic.
+    fn set_dontroute(&mut self, keepalive: bool) -> Result<()>;
+
+    /// Indicates if the packet will pass straight through to
+    /// the interface bypassing the routing logic.
+    fn dontroute(&self) -> Result<bool>;
+
     /// Returns the address (IP and Port) of the peer socket that this
     /// is conencted to
     fn addr_peer(&self) -> Result<SocketAddr>;

Cargo.toml
@@ -168,6 +168,10 @@ opt-level = 3
 opt-level = 3
 [profile.dev.package.weezl]
 opt-level = 3
+[profile.dev.package.sha256]
+opt-level = 3
+[profile.dev.package.digest]
+opt-level = 3
 
 [[bench]]
 name = "static_and_dynamic_functions"

lib/virtual-io/src/selector.rs
@@ -89,23 +89,25 @@ impl Selector {
                     return;
                 }
 
-                let interest = if event.is_readable() {
-                    InterestType::Readable
-                } else if event.is_writable() {
-                    InterestType::Writable
-                } else if event.is_read_closed() || event.is_write_closed() {
-                    InterestType::Closed
-                } else if event.is_error() {
-                    InterestType::Error
-                } else {
-                    continue;
-                };
-                tracing::trace!(token = ?token, interest = ?interest, "host epoll");
-
                 // Otherwise this is a waker we need to wake
                 let s = event.token().0 as *mut HandlerWrapper;
                 let mut handler = ManuallyDrop::new(unsafe { Box::from_raw(s) });
-                handler.0.interest(interest);
+                if event.is_readable() {
+                    tracing::trace!(token = ?token, interest = ?InterestType::Readable, "host epoll");
+                    handler.0.interest(InterestType::Readable);
+                }
+                if event.is_writable() {
+                    tracing::trace!(token = ?token, interest = ?InterestType::Writable, "host epoll");
+                    handler.0.interest(InterestType::Writable);
+                }
+                if event.is_read_closed() || event.is_write_closed() {
+                    tracing::trace!(token = ?token, interest = ?InterestType::Closed, "host epoll");
+                    handler.0.interest(InterestType::Closed);
+                }
+                if event.is_error() {
+                    tracing::trace!(token = ?token, interest = ?InterestType::Error, "host epoll");
+                    handler.0.interest(InterestType::Error);
+                }
             }
         }
     }

lib/virtual-net/src/client.rs
@@ -1370,6 +1370,36 @@ impl VirtualTcpSocket for RemoteSocket {
         }
     }
 
+    fn set_keepalive(&mut self, keep_alive: bool) -> Result<()> {
+        self.io_socket_fire_and_forget(RequestType::SetKeepAlive(keep_alive))
+    }
+
+    fn keepalive(&self) -> Result<bool> {
+        match InlineWaker::block_on(self.io_socket(RequestType::GetKeepAlive)) {
+            ResponseType::Err(err) => Err(err),
+            ResponseType::Flag(val) => Ok(val),
+            res => {
+                tracing::debug!("invalid response to get nodelay request - {res:?}");
+                Err(NetworkError::IOError)
+            }
+        }
+    }
+
+    fn set_dontroute(&mut self, dont_route: bool) -> Result<()> {
+        self.io_socket_fire_and_forget(RequestType::SetDontRoute(dont_route))
+    }
+
+    fn dontroute(&self) -> Result<bool> {
+        match InlineWaker::block_on(self.io_socket(RequestType::GetDontRoute)) {
+            ResponseType::Err(err) => Err(err),
+            ResponseType::Flag(val) => Ok(val),
+            res => {
+                tracing::debug!("invalid response to get nodelay request - {res:?}");
+                Err(NetworkError::IOError)
+            }
+        }
+    }
+
     fn addr_peer(&self) -> Result<SocketAddr> {
         match InlineWaker::block_on(self.io_socket(RequestType::GetAddrPeer)) {
             ResponseType::Err(err) => Err(err),

lib/virtual-net/src/host.rs
@@ -10,6 +10,8 @@ use derivative::Derivative;
 use std::io::{Read, Write};
 use std::mem::MaybeUninit;
 use std::net::{IpAddr, Ipv4Addr, Ipv6Addr, Shutdown, SocketAddr};
+#[cfg(not(target_os = "windows"))]
+use std::os::fd::AsRawFd;
 use std::sync::Arc;
 use std::time::Duration;
 use tokio::runtime::Handle;
@@ -62,6 +64,8 @@ impl VirtualNetworking for LocalNetworking {
                     stream: mio::net::TcpListener::from_std(sock),
                     selector: self.selector.clone(),
                     handler_guard: None,
+                    no_delay: None,
+                    keep_alive: None,
                 })
             })
             .map_err(io_err_into_net_error)?;
@@ -94,11 +98,8 @@ impl VirtualNetworking for LocalNetworking {
         if let Ok(p) = stream.peer_addr() {
             peer = p;
         }
-        Ok(Box::new(LocalTcpStream::new(
-            self.selector.clone(),
-            stream,
-            peer,
-        )))
+        let socket = Box::new(LocalTcpStream::new(self.selector.clone(), stream, peer));
+        Ok(socket)
     }
 
     async fn resolve(
@@ -127,6 +128,8 @@ pub struct LocalTcpListener {
     stream: mio::net::TcpListener,
     selector: Arc<Selector>,
     handler_guard: Option<InterestGuard>,
+    no_delay: Option<bool>,
+    keep_alive: Option<bool>,
 }
 
 impl VirtualTcpListener for LocalTcpListener {
@@ -138,6 +141,12 @@ impl VirtualTcpListener for LocalTcpListener {
                     .ok();
                 let mut socket = LocalTcpStream::new(self.selector.clone(), stream, addr);
                 socket.set_first_handler_writeable();
+                if let Some(no_delay) = self.no_delay {
+                    socket.set_nodelay(no_delay).ok();
+                }
+                if let Some(keep_alive) = self.keep_alive {
+                    socket.set_keepalive(keep_alive).ok();
+                }
                 Ok((Box::new(socket), addr))
             }
             Err(NetworkError::WouldBlock) => Err(NetworkError::WouldBlock),
@@ -240,6 +249,66 @@ impl VirtualTcpSocket for LocalTcpStream {
 
     fn nodelay(&self) -> Result<bool> {
         self.stream.nodelay().map_err(io_err_into_net_error)
+    }
+
+    fn set_keepalive(&mut self, keepalive: bool) -> Result<()> {
+        socket2::SockRef::from(&self.stream)
+            .set_keepalive(true)
+            .map_err(io_err_into_net_error)?;
+        Ok(())
+    }
+
+    fn keepalive(&self) -> Result<bool> {
+        let ret = socket2::SockRef::from(&self.stream)
+            .keepalive()
+            .map_err(io_err_into_net_error)?;
+        Ok(ret)
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    fn set_dontroute(&mut self, val: bool) -> Result<()> {
+        let val = val as libc::c_int;
+        let payload = &val as *const libc::c_int as *const libc::c_void;
+        let err = unsafe {
+            libc::setsockopt(
+                self.stream.as_raw_fd(),
+                libc::SOL_SOCKET,
+                libc::SO_DONTROUTE,
+                payload,
+                std::mem::size_of::<libc::c_int>() as libc::socklen_t,
+            )
+        };
+        if err == -1 {
+            return Err(io_err_into_net_error(std::io::Error::last_os_error()));
+        }
+        Ok(())
+    }
+    #[cfg(target_os = "windows")]
+    fn set_dontroute(&mut self, val: bool) -> Result<()> {
+        Err(NetworkError::Unsupported)
+    }
+
+    #[cfg(not(target_os = "windows"))]
+    fn dontroute(&self) -> Result<bool> {
+        let mut payload: MaybeUninit<libc::c_int> = MaybeUninit::uninit();
+        let mut len = std::mem::size_of::<libc::c_int>() as libc::socklen_t;
+        let err = unsafe {
+            libc::getsockopt(
+                self.stream.as_raw_fd(),
+                libc::SOL_SOCKET,
+                libc::SO_DONTROUTE,
+                payload.as_mut_ptr().cast(),
+                &mut len,
+            )
+        };
+        if err == -1 {
+            return Err(io_err_into_net_error(std::io::Error::last_os_error()));
+        }
+        Ok(unsafe { payload.assume_init() != 0 })
+    }
+    #[cfg(target_os = "windows")]
+    fn dontroute(&self) -> Result<bool> {
+        Err(NetworkError::Unsupported)
     }
 
     fn addr_peer(&self) -> Result<SocketAddr> {

commit db73b08dc2fc039ca4f59f407f290493b5b214a2

fix: Fixed some compilation errors introduced by #4204 that assume `tokio::task::block_in_place()` always exists

lib/wasix/src/lib.rs
@@ -786,3 +786,18 @@ fn mem_error_to_wasi(err: MemoryAccessError) -> Errno {
         _ => Errno::Unknown,
     }
 }
+
+/// Run a synchronous function that would normally be blocking.
+///
+/// When the `sys-thread` feature is enabled, this will call
+/// [`tokio::task::block_in_place()`]. Otherwise, it calls the function
+/// immediately.
+pub(crate) fn block_in_place<Ret>(thunk: impl FnOnce() -> Ret) -> Ret {
+    cfg_if::cfg_if! {
+        if #[cfg(feature = "sys-thread")] {
+            tokio::task::block_in_place(thunk)
+        } else {
+            thunk()
+        }
+    }
+}

lib/wasix/src/runtime/package_loader/builtin_loader.rs
@@ -271,10 +271,7 @@ impl FileSystemCache {
     async fn lookup(&self, hash: &WebcHash) -> Result<Option<Container>, Error> {
         let path = self.path(hash);
 
-        #[cfg(target_arch = "wasm32")]
-        let container = Container::from_disk(&path);
-        #[cfg(not(target_arch = "wasm32"))]
-        let container = tokio::task::block_in_place(|| Container::from_disk(&path));
+        let container = crate::block_in_place(|| Container::from_disk(&path));
         match container {
             Ok(c) => Ok(Some(c)),
             Err(ContainerError::Open { error, .. })

lib/wasix/src/runtime/resolver/filesystem_source.rs
@@ -23,17 +23,9 @@ impl Source for FileSystemSource {
             _ => return Err(QueryError::Unsupported),
         };
 
-        #[cfg(target_arch = "wasm32")]
-        let webc_sha256 = WebcHash::for_file(&path)
+        let webc_sha256 = crate::block_in_place(|| WebcHash::for_file(&path))
             .with_context(|| format!("Unable to hash \"{}\"", path.display()))?;
-        #[cfg(not(target_arch = "wasm32"))]
-        let webc_sha256 = tokio::task::block_in_place(|| WebcHash::for_file(&path))
-            .with_context(|| format!("Unable to hash \"{}\"", path.display()))?;
-        #[cfg(target_arch = "wasm32")]
-        let container = Container::from_disk(&path)
-            .with_context(|| format!("Unable to parse \"{}\"", path.display()))?;
-        #[cfg(not(target_arch = "wasm32"))]
-        let container = tokio::task::block_in_place(|| Container::from_disk(&path))
+        let container = crate::block_in_place(|| Container::from_disk(&path))
             .with_context(|| format!("Unable to parse \"{}\"", path.display()))?;
 
         let url = crate::runtime::resolver::utils::url_from_file_path(&path)

lib/wasix/src/runtime/resolver/web_source.rs
@@ -240,20 +240,12 @@ impl Source for WebSource {
             .await
             .context("Unable to get the locally cached file")?;
 
-        #[cfg(target_arch = "wasm32")]
-        let webc_sha256 = WebcHash::for_file(&local_path)
-            .with_context(|| format!("Unable to hash \"{}\"", local_path.display()))?;
-        #[cfg(not(target_arch = "wasm32"))]
-        let webc_sha256 = tokio::task::block_in_place(|| WebcHash::for_file(&local_path))
+        let webc_sha256 = crate::block_in_place(|| WebcHash::for_file(&local_path))
             .with_context(|| format!("Unable to hash \"{}\"", local_path.display()))?;
 
         // Note: We want to use Container::from_disk() rather than the bytes
         // our HTTP client gave us because then we can use memory-mapped files
-        #[cfg(target_arch = "wasm32")]
-        let container = Container::from_disk(&local_path)
-            .with_context(|| format!("Unable to load \"{}\"", local_path.display()))?;
-        #[cfg(not(target_arch = "wasm32"))]
-        let container = tokio::task::block_in_place(|| Container::from_disk(&local_path))
+        let container = crate::block_in_place(|| Container::from_disk(&local_path))
             .with_context(|| format!("Unable to load \"{}\"", local_path.display()))?;
         let pkg = PackageInfo::from_manifest(container.manifest())
             .context("Unable to determine the package's metadata")?;

lib/wasix/src/state/builder.rs
@@ -13,8 +13,6 @@ use wasmer::{AsStoreMut, Instance, Module, Store};
 
 #[cfg(feature = "journal")]
 use crate::journal::{DynJournal, SnapshotTrigger};
-#[cfg(feature = "sys")]
-use crate::PluggableRuntime;
 use crate::{
     bin_factory::{BinFactory, BinaryPackage},
     capabilities::Capabilities,
@@ -800,7 +798,7 @@ impl WasiEnvBuilder {
             #[cfg(feature = "sys-thread")]
             {
                 #[allow(unused_mut)]
-                let mut runtime = PluggableRuntime::new(Arc::new(crate::runtime::task_manager::tokio::TokioTaskManager::default()));
+                let mut runtime = crate::runtime::PluggableRuntime::new(Arc::new(crate::runtime::task_manager::tokio::TokioTaskManager::default()));
                 #[cfg(feature = "journal")]
                 for journal in self.journals.clone() {
                     runtime.add_journal(journal);