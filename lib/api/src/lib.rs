8caa0c1db8fc5440d64e27dad31c4fb18ed05860
M.Amin Rayej <m.amin.rayej@gmail.com>
2024-01-29 22:40:13 +0330

    make the deprecation message more clear

--- lib/api/src/lib.rs
+++ lib/api/src/lib.rs
@@ -453,25 +453,25 @@ pub mod sys;
 pub use sys::*;
 
 #[cfg(feature = "sys")]
-#[deprecated(note = "Use sys::Artifact instead")]
+#[deprecated(note = "wasmer::Artifact is deprecated, use wasmer::sys::Artifact instead")]
 /// A compiled wasm module, ready to be instantiated.
 pub type Artifact = sys::Artifact;
 #[cfg(feature = "sys")]
-#[deprecated(note = "Use sys::EngineBuilder instead")]
+#[deprecated(note = "wasmer::EngineBuilder is deprecated, use wasmer::sys::EngineBuilder instead")]
 /// The Builder contents of `Engine`
 pub type EngineBuilder = sys::EngineBuilder;
 #[cfg(feature = "sys")]
-#[deprecated(note = "Use sys::Features instead")]
+#[deprecated(note = "wasmer::Features is deprecated, use wasmer::sys::Features instead")]
 /// Controls which experimental features will be enabled.
 pub type Features = sys::Features;
 #[cfg(feature = "sys")]
-#[deprecated(note = "Use sys::BaseTunables instead")]
+#[deprecated(note = "wasmer::BaseTunables is deprecated, use wasmer::sys::BaseTunables instead")]
 /// Tunable parameters for WebAssembly compilation.
 /// This is the reference implementation of the `Tunables` trait,
 /// used by default.
 pub type BaseTunables = sys::BaseTunables;
 #[cfg(feature = "sys")]
-#[deprecated(note = "Use sys::VMConfig instead")]
+#[deprecated(note = "wasmer::VMConfig is deprecated, use wasmer::sys::VMConfig instead")]
 /// Configuration for the the runtime VM
 /// Currently only the stack size is configurable
 pub type VMConfig = sys::VMConfig;
