3afe1e70357c157599cb92febcf1d03c0ad580b3
Christoph Herzog <chris@theduke.at>
2024-02-25 15:03:05 +0100

    test: Small compatibility tweaks and improvements to wasi-fyi tests

--- tests/wasi-fyi/test.sh
+++ tests/wasi-fyi/test.sh
@@ -1,5 +1,12 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -ueo pipefail
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# get absolute path of the root dir
+ROOT_DIR="$( cd "${DIR}/.." && pwd )"
+
+cd "$DIR"
 
 bash build.sh
 
@@ -7,6 +14,8 @@ status=0
 
 # Define skip list as an array
 SKIP_LIST=("ported_readlink.wasm" "fs_create_dir-existing-directory.wasm" "fs_rename-directory.wasm" "fs_rename-file.wasm")
+
+cd $DIR
 
 # List and process .foo files
 for file in *.wasm; do
@@ -19,4 +28,3 @@ for file in *.wasm; do
 done
 
 exit $status
-
