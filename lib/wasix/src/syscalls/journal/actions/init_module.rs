329f9729c5d60bd675cdaa4faf4f810e4fc256fd
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2024-02-26 18:15:03 +1100

    Fixed one of the lints that was missed

--- lib/wasix/src/syscalls/journal/actions/init_module.rs
+++ lib/wasix/src/syscalls/journal/actions/init_module.rs
@@ -1,6 +1,7 @@
 use super::*;
 
 impl<'a, 'c> JournalSyscallPlayer<'a, 'c> {
+    #[allow(clippy::result_large_err)]
     pub(crate) unsafe fn action_init_module(
         &mut self,
         wasm_hash: [u8; 8],
