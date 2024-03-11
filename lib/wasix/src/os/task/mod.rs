b8ee57bce419c950e0901cc167372190f6079983
Johnathan Sharratt <johnathan.sharratt@gmail.com>
2024-03-11 19:12:22 +1100

    Linting fix

--- lib/wasix/src/os/task/mod.rs
+++ lib/wasix/src/os/task/mod.rs
@@ -1,11 +1,11 @@
 //! OS task management for processes and threads.
 
+pub mod backoff;
 pub mod control_plane;
 pub mod process;
 pub mod signal;
 mod task_join_handle;
 pub mod thread;
-pub mod backoff;
 
 pub use task_join_handle::{
     OwnedTaskStatus, TaskJoinHandle, TaskStatus, TaskTerminatedError, VirtualTaskHandle,
