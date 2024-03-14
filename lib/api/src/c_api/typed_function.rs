844f9e793701f6d6321fc273e70e334acc697c3f
Edoardo Marangoni <edoardo@wasmer.io>
2024-03-14 18:09:47 +0100

    feat: implement `call` for `TypedFunction`

--- lib/api/src/c_api/typed_function.rs
+++ lib/api/src/c_api/typed_function.rs
@@ -7,6 +7,14 @@
 //! let add_one = instance.exports.get_function("function_name")?;
 //! let add_one_native: TypedFunction<i32, i32> = add_one.typed().unwrap();
 //! ```
+use crate::as_c::result_to_value;
+use crate::bindings::{
+    wasm_extern_as_func, wasm_func_call, wasm_func_t, wasm_global_set, wasm_global_t,
+    wasm_val_vec_new, wasm_val_vec_new_uninitialized,
+};
+
+use crate::as_c::param_from_c;
+use crate::c_api::externals::function::Function;
 use crate::c_api::trap::Trap;
 use crate::native_type::NativeWasmTypeInto;
 use crate::Value;
@@ -19,96 +27,66 @@ use wasmer_types::RawValue;
 macro_rules! impl_native_traits {
     (  $( $x:ident ),* ) => {
         #[allow(unused_parens, non_snake_case)]
-        impl<$( $x , )* Rets> TypedFunction<( $( $x ),* ), Rets>
-        where
-            $( $x: FromToNativeWasmType, )*
-            Rets: WasmTypeList,
-        {
-            /// Call the typed func and return results.
-            #[allow(clippy::too_many_arguments)]
-            pub fn call(&self, mut store: &mut impl AsStoreMut, $( $x: $x, )* ) -> Result<Rets, RuntimeError> where
-            $( $x: FromToNativeWasmType + NativeWasmTypeInto, )*
-            {
-                unimplemented!();
-                // // let store_ptr = Value::I64(store.as_store_mut().as_raw() as _).as_jsvalue(store);
-                // #[allow(unused_unsafe)]
-                // let params_list: Vec<_> = unsafe {
-                //     vec![ $( {
-                //         let raw = $x.into_raw(store);
-                //         let value = Value::from_raw(&mut store, $x::WASM_TYPE, raw);
-                //         value.as_jsvalue(store)
-                //     } ),* ]
-                // };
+         impl<$( $x , )* Rets> TypedFunction<( $( $x ),* ), Rets>
+         where
+             $( $x: FromToNativeWasmType, )*
+             Rets: WasmTypeList,
+         {
+             /// Call the typed func and return results.
+             #[allow(clippy::too_many_arguments)]
+             pub fn call(&self, mut store: &mut impl AsStoreMut, $( $x: $x, )* ) -> Result<Rets, RuntimeError> where
+             $( $x: FromToNativeWasmType + NativeWasmTypeInto, )*
+             {
+                 // // let store_ptr = Value::I64(store.as_store_mut().as_raw() as _).as_jsvalue(store);
+                 #[allow(unused_unsafe)]
+                 let params_list: Vec<_> = unsafe {
+                     vec![ $( {
+                         let raw = $x.into_raw(store);
+                         let value = Value::from_raw(&mut store, $x::WASM_TYPE, raw);
+                         result_to_value(&value)
+                     } ),* ]
+                 };
 
-                // let store_mut = store.as_store_mut();
-                // let context = store_mut.jsc().context();
+                 let mut params = unsafe {std::mem::zeroed()};
 
-                // let global = context.get_global_object();
-                // let store_ptr = store_mut.as_raw() as usize;
-                // global.set_property(&context, "__store_ptr".to_string(), JSValue::number(&context, store_ptr as _)).unwrap();
+                 unsafe {
+                     wasm_val_vec_new(&mut params, params_list.len(), params_list.as_ptr());
+                 }
 
-                // let results = {
-                //     let mut r;
-                //     // TODO: This loop is needed for asyncify. It will be refactored with https://github.com/wasmerio/wasmer/issues/3451
-                //     loop {
+                 let mut results = unsafe {std::mem::zeroed()};
 
-                //         let store_mut = store.as_store_mut();
-                //         let context = store_mut.jsc().context();
-                //         r = self.func.0.handle.function
-                //             .call(
-                //                 &context,
-                //                 None,
-                //                 &params_list,
-                //             );
+                 unsafe {
+                     wasm_val_vec_new_uninitialized(&mut results, Rets::wasm_types().len())
+                 }
+                 
+                 let func = unsafe { wasm_extern_as_func(self.func.to_vm_extern()) };
 
-                //         // let store_mut = store.as_store_mut();
-                //         if let Some(callback) = store_mut.inner.on_called.take() {
-                //             match callback(store_mut) {
-                //                 Ok(wasmer_types::OnCalledAction::InvokeAgain) => { continue; }
-                //                 Ok(wasmer_types::OnCalledAction::Finish) => { break; }
-                //                 Ok(wasmer_types::OnCalledAction::Trap(trap)) => { let err = Err(RuntimeError::user(trap));
-                //                     return err;
-                //                 },
-                //                 Err(trap) => {
-                //                     return Err(RuntimeError::user(trap))
-                //                 },
-                //             }
-                //         }
-                //         break;
-                //     }
-                //     let store_mut = store.as_store_mut();
-                //     let context = store_mut.jsc().context();
-                //     r.map_err(|e| Trap::from_jsvalue(context, e))?
-                // };
-                // let mut rets_list_array = Rets::empty_array();
-                // let mut_rets = rets_list_array.as_mut() as *mut [RawValue] as *mut RawValue;
-                // let store_mut = store.as_store_mut();
-                // let context = store_mut.jsc().context();
-                // match Rets::size() {
-                //     0 => {},
-                //     1 => unsafe {
-                //         let ty = Rets::wasm_types()[0];
-                //         let val = param_from_js(&context, &ty, &results);
-                //         *mut_rets = val.as_raw(&mut store);
-                //     }
-                //     _n => {
-                //         if !results.is_array(&context) {
-                //             panic!("Expected results to be an array.")
-                //         }
-                //         let results = results.to_object(&context).unwrap();
-                //         for (i, ret_type) in Rets::wasm_types().iter().enumerate() {
-                //             let store_mut = store.as_store_mut();
-                //             let context = store_mut.jsc().context();
-                //             let ret = results.get_property_at_index(&context, i as _).unwrap();
-                //             unsafe {
-                //                 let val = param_from_js(&context, &ret_type, &ret);
-                //                 let slot = mut_rets.add(i);
-                //                 *slot = val.as_raw(&mut store);
-                //             }
-                //         }
-                //     }
-                // }
-                // Ok(unsafe { Rets::from_array(store, rets_list_array) })
+                 unsafe { wasm_func_call(func, &params, &mut results); }
+
+                let mut rets_list_array = Rets::empty_array();
+                let mut_rets = rets_list_array.as_mut() as *mut [RawValue] as *mut RawValue;
+
+                match Rets::size() {
+                    0 => {},
+                    1 => unsafe {
+                        let val = (*results.data.wrapping_add(0)).clone();
+                        let val = param_from_c(&val);
+                        *mut_rets = val.as_raw(&mut store);
+                    }
+                    _n => {
+                        for (i, ret_type) in Rets::wasm_types().iter().enumerate() {
+                            unsafe {
+                                let val = (*results.data.wrapping_add(i)).clone();
+                                let val = param_from_c(&val);
+                                let slot = mut_rets.add(i);
+                                *slot = val.as_raw(&mut store);
+                            }
+                        }
+                    }
+                }
+
+                Ok(unsafe { Rets::from_array(store, rets_list_array) })
+
             }
         }
     };
