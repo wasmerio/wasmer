df806d4d824c8e87f9894c3e163fc7354e609c9b
Edoardo Marangoni <ecmm@anche.no>
2024-03-20 12:02:15 +0100

    fix(argus): explicit env filter

--- tests/wasmer-argus/src/main.rs
+++ tests/wasmer-argus/src/main.rs
@@ -2,10 +2,14 @@ mod argus;
 
 use argus::*;
 use clap::Parser;
+use tracing_subscriber::{fmt, prelude::*, EnvFilter};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
-    tracing_subscriber::fmt::fmt().init();
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .init();
 
     let config = ArgusConfig::parse();
 
