f956017d3008948492f9f106527a6d74d787cd6f
Edoardo Marangoni <ecmm@anche.no>
2024-03-21 20:31:23 +0100

    feat: implement table functions

--- lib/api/src/c_api/externals/table.rs
+++ lib/api/src/c_api/externals/table.rs
@@ -1,5 +1,7 @@
 use crate::bindings::{
-    wasm_table_type, wasm_tabletype_element, wasm_tabletype_limits, wasm_tabletype_t,
+    wasm_extern_as_ref, wasm_limits_t, wasm_ref_t, wasm_table_new, wasm_table_type,
+    wasm_tabletype_element, wasm_tabletype_limits, wasm_tabletype_new, wasm_tabletype_t,
+    wasm_val_t, wasm_valtype_new,
 };
 use crate::c_api::bindings::wasm_table_as_extern;
 use crate::c_api::vm::{VMExtern, VMExternTable, VMFunction, VMTable};
@@ -13,47 +15,37 @@ pub struct Table {
     pub(crate) handle: VMTable,
 }
 
-// Table can't be Send in js because it dosen't support `structuredClone`
-// https://developer.mozilla.org/en-US/docs/Web/API/structuredClone
-// unsafe impl Send for Table {}
-
 impl Table {
+    pub(crate) fn type_to_wamr(ty: TableType) -> *mut wasm_tabletype_t {
+        let valtype = unsafe { wasm_valtype_new(as_c::type_to_c(&ty.ty)) };
+
+        let limits = Box::into_raw(Box::new(wasm_limits_t {
+            min: ty.minimum,
+            max: match ty.maximum {
+                Some(v) => v,
+                None => 0,
+            },
+        }));
+
+        unsafe { wasm_tabletype_new(valtype, limits) }
+    }
+
     pub fn new(
         store: &mut impl AsStoreMut,
         ty: TableType,
         init: Value,
     ) -> Result<Self, RuntimeError> {
-        unimplemented!();
-        // let store_mut = store.as_store_mut();
-        // let engine = store_mut.engine();
-        // let context = engine.0.context();
+        let store_mut = store.as_store_mut();
+        let engine = store_mut.engine();
 
-        // let mut descriptor = JSObject::new(&context);
-        // descriptor.set_property(
-        //     &context,
-        //     "initial".to_string(),
-        //     JSValue::number(&context, ty.minimum.into()),
-        // );
-        // if let Some(max) = ty.maximum {
-        //     descriptor.set_property(
-        //         &context,
-        //         "maximum".to_string(),
-        //         JSValue::number(&context, max.into()),
-        //     );
-        // }
-        // descriptor.set_property(
-        //     &context,
-        //     "element".to_string(),
-        //     JSValue::string(&context, "anyfunc".to_string()),
-        // );
+        let wasm_tablety = Self::type_to_wamr(ty);
+        let init: wasm_val_t = as_c::result_to_value(&init);
 
-        // let js_table = engine
-        //     .0
-        //     .wasm_table_type()
-        //     .construct(&context, &[descriptor.to_jsvalue()])
-        //     .map_err(|e| <JSValue as Into<RuntimeError>>::into(e))?;
-        // let vm_table = VMTable::new(js_table, ty);
-        // Ok(Self::from_vm_extern(store, vm_table))
+        Ok(Self {
+            handle: unsafe {
+                wasm_table_new(store_mut.inner.store.inner, wasm_tablety, init.of.ref_)
+            },
+        })
     }
 
     pub fn to_vm_extern(&self) -> VMExtern {
@@ -68,7 +60,13 @@ impl Table {
         TableType {
             ty: unsafe { as_c::valtype_to_type(table_type) },
             minimum: unsafe { (*table_limits).min },
-            maximum: unsafe { Some((*table_limits).max) },
+            maximum: unsafe {
+                if (*table_limits).max == 0 {
+                    None
+                } else {
+                    Some((*table_limits).max)
+                }
+            },
         }
     }
 
