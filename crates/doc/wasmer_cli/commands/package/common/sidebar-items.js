6b7ad31908aca0450b16cad7a1040c425800f284
xdoardo <xdoardo@users.noreply.github.com>
2024-05-09 15:52:14 +0000

    deploy: b8f4bee845f748171da22094d392149d0a63d3be

--- crates/doc/wasmer_cli/commands/package/common/sidebar-items.js
+++ crates/doc/wasmer_cli/commands/package/common/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["PackageSpecifier","Tag"],"fn":["get_manifest","into_specifier","invalidate_graphql_query_cache","login_user","on_error","upload"],"mod":["macros","wait"]};
+window.SIDEBAR_ITEMS = {"fn":["get_manifest","invalidate_graphql_query_cache","login_user","on_error","upload"],"mod":["macros","wait"]};
