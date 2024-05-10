fedaac546e8d6766c7bfeb9f79bfb8b2b192957c
syrusakbary <syrusakbary@users.noreply.github.com>
2024-05-10 14:54:50 +0000

    deploy: d326250000209873641523eb10951b1e8beb9130

--- crates/doc/wasmer_cli/opts/sidebar-items.js
+++ crates/doc/wasmer_cli/opts/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"struct":["ApiOpts","ItemFormatOpts","ItemTableFormatOpts","ListFormatOpts","Login","WASMER_CACHE_DIR","WASMER_DIR","WasmerEnv"]};
+window.SIDEBAR_ITEMS = {"fn":["parse_registry_url"],"struct":["ApiOpts","ItemFormatOpts","ItemTableFormatOpts","ListFormatOpts","Login","WASMER_CACHE_DIR","WASMER_DIR","WasmerEnv"]};
