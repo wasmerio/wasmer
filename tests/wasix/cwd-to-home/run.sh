df8032ea5cad29322c4ac4673b6ddb3c3026348e
M.Amin Rayej <m.amin.rayej@gmail.com>
2024-05-31 01:42:57 +0330

    test more code paths

--- tests/wasix/cwd-to-home/run.sh
+++ tests/wasix/cwd-to-home/run.sh
@@ -2,6 +2,12 @@
 
 $CC $CFLAGS $LDFLAGS -o main.wasm main.c
 
-$WASMER run -q main.wasm --dir=. > output
+$WASMER -q run main.wasm --dir=. > output0
+$WASMER -q run . --dir=. > output1
+$WASMER -q package build --out cwd-to-home.webc . > /dev/null && $WASMER -q run cwd-to-home.webc --dir=. > output2
 
-diff -u output expected 1>/dev/null
+rm cwd-to-home.webc
+
+diff -u output0 expected 1>/dev/null && \
+diff -u output1 expected 1>/dev/null && \
+diff -u output2 expected 1>/dev/null
