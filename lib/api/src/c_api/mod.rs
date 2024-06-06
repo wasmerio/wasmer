9e7ff6c819fb3d24b0fa85797fcf89601b08430e
Edoardo Marangoni <ecmm@anche.no>
2024-06-06 17:41:56 +0200

    chore(api/wamr): Make linter happy

--- lib/api/src/c_api/mod.rs
+++ lib/api/src/c_api/mod.rs
@@ -1,6 +1,12 @@
 //! The JavascriptCore integration in the Wasmer API
 
-#![allow(unused, dead_code, non_upper_case_globals, non_snake_case, missing_docs)]
+#![allow(
+    unused,
+    dead_code,
+    non_upper_case_globals,
+    non_snake_case,
+    missing_docs
+)]
 
 #[cfg(all(feature = "std", feature = "core"))]
 compile_error!(
@@ -40,6 +46,5 @@ pub(crate) mod store;
 pub(crate) mod trap;
 pub(crate) mod typed_function;
 pub(crate) mod vm;
-
 
 pub use crate::bindings::*;
