ff04f57a563cc90b3578a72c77e8df3b3a9876ad
syrusakbary <syrusakbary@users.noreply.github.com>
2024-06-14 07:28:22 +0000

    deploy: d085cdb3b18e9ce7d3f785de4561e6ac1afef016

--- crates/doc/wasmer_compiler/engine/unwind/systemv/sidebar-items.js
+++ crates/doc/wasmer_compiler/engine/unwind/systemv/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["__deregister_frame","__register_frame"],"struct":["UnwindRegistry"]};
+window.SIDEBAR_ITEMS = {"fn":["__deregister_frame","__register_frame","using_libunwind"],"struct":["UnwindRegistry"]};
