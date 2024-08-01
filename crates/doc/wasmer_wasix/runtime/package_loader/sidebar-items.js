626b326b26ebef2e2ecb7ed810749d2e4978583e
theduke <theduke@users.noreply.github.com>
2024-08-01 20:05:07 +0000

    deploy: ecfb8039837755e30e898a4946e83f7cbeb8c825

--- crates/doc/wasmer_wasix/runtime/package_loader/sidebar-items.js
+++ crates/doc/wasmer_wasix/runtime/package_loader/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"fn":["load_package_tree"],"mod":["builtin_loader","load_package_tree","types","unsupported"],"struct":["BuiltinPackageLoader","UnsupportedPackageLoader"],"trait":["PackageLoader"]};
+window.SIDEBAR_ITEMS = {"fn":["load_package_tree"],"mod":["builtin_loader","load_package_tree","types","unsupported"],"struct":["UnsupportedPackageLoader"],"trait":["PackageLoader"]};
