20a107f8f09cfa5dec9c98d296ae165f364832b0
Damir Vodenicarevic <damipator@gmail.com>
2024-08-13 09:51:01 +0200

    Add module-level middleware errors Adds the ability to return errors from module-level middleware. This allows a middleware to cancel compilation cleanly.

--- lib/middlewares/src/metering.rs
+++ lib/middlewares/src/metering.rs
@@ -154,7 +154,7 @@ impl<F: Fn(&Operator) -> u64 + Send + Sync + 'static> ModuleMiddleware for Meter
     }
 
     /// Transforms a `ModuleInfo` struct in-place. This is called before application on functions begins.
-    fn transform_module_info(&self, module_info: &mut ModuleInfo) {
+    fn transform_module_info(&self, module_info: &mut ModuleInfo) -> Result<(), MiddlewareError> {
         let mut global_indexes = self.global_indexes.lock().unwrap();
 
         if global_indexes.is_some() {
@@ -192,7 +192,9 @@ impl<F: Fn(&Operator) -> u64 + Send + Sync + 'static> ModuleMiddleware for Meter
         *global_indexes = Some(MeteringGlobalIndexes(
             remaining_points_global_index,
             points_exhausted_global_index,
-        ))
+        ));
+
+        Ok(())
     }
 }
 
