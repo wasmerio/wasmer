34d0235a727afca79268adb6dc16cd6fae587512
theduke <theduke@users.noreply.github.com>
2024-08-15 05:23:13 +0000

    deploy: 4dddcf24fa63fa50f6a9e361378f3133316e110a

---
+++ crates/doc/wasmer_journal/concrete/compacting_transaction/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["CompactingTransactionJournal","CompactingTransactionJournalRx","CompactingTransactionJournalTx"]};
---
+++ crates/doc/wasmer_journal/concrete/transaction/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"struct":["State","TransactionJournal","TransactionJournalRx","TransactionJournalTx"]};
