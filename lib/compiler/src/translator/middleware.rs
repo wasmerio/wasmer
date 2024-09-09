46fc76381c2cecc60ec17e9317a4949e0c314826
Edoardo Marangoni <ecmm@anche.no>
2024-09-09 18:43:58 +0200

    fix(compiler): Use `default` features

--- lib/compiler/src/translator/middleware.rs
+++ lib/compiler/src/translator/middleware.rs
@@ -116,7 +116,7 @@ impl<'a: 'b, 'b> Extend<&'b Operator<'a>> for MiddlewareReaderState<'a> {
 impl<'a> MiddlewareBinaryReader<'a> {
     /// Constructs a `MiddlewareBinaryReader` with an explicit starting offset.
     pub fn new_with_offset(data: &'a [u8], original_offset: usize) -> Self {
-        let inner = BinaryReader::new(data, original_offset, WasmFeatures::all());
+        let inner = BinaryReader::new(data, original_offset, WasmFeatures::default());
         Self {
             state: MiddlewareReaderState {
                 inner,
