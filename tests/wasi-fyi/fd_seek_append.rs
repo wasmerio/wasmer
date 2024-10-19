2e7ec563918879344e645e07bca6c40b7e20f136
Yage Hu <me@huyage.dev>
2024-10-19 10:26:47 -0400

    Use std for test
    Signed-off-by: Yage Hu <me@huyage.dev>

--- tests/wasi-fyi/fd_seek_append.rs
+++ tests/wasi-fyi/fd_seek_append.rs
@@ -1,32 +1,15 @@
-use std::os::fd::AsRawFd;
-
-#[link(wasm_import_module = "wasi_snapshot_preview1")]
-extern "C" {
-    pub fn fd_seek(fd: i32, offset: i64, whence: i32, filesize: i32) -> i32;
-}
-
-const ERRNO_SUCCESS: i32 = 0;
-const WHENCE_SET: i32 = 0;
+use std::io::{Seek as _, SeekFrom};
 
 fn main() {
-    unsafe {
-        let offset = 100u64;
-        let mut new_offset = 0u64;
-        let f = std::fs::OpenOptions::new()
-            .read(true)
-            .write(true)
-            .create(true)
-            .append(true)
-            .open("/fyi/fs_seek_append.dir/file")
-            .unwrap();
-        let errno = fd_seek(
-            f.as_raw_fd(),
-            offset as i64,
-            WHENCE_SET,
-            &mut new_offset as *mut u64 as usize as i32,
-        );
+    let offset = 100u64;
+    let mut f = std::fs::OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .append(true)
+        .open("/fyi/fs_seek_append.dir/file")
+        .unwrap();
+    let new_offset = f.seek(SeekFrom::Start(offset)).unwrap();
 
-        assert_eq!(errno, ERRNO_SUCCESS);
-        assert_eq!(offset, new_offset);
-    }
+    assert_eq!(offset, new_offset);
 }
