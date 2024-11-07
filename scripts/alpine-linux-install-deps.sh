f6fee0fd8c50ac2e1ba1d23c68ddea2e585f7308
edoardo <48774736+xdoardo@users.noreply.github.com>
2024-11-07 17:29:48 +0000

    Fix(ci): Download zlib dependency on musl builds

--- scripts/alpine-linux-install-deps.sh
+++ scripts/alpine-linux-install-deps.sh
@@ -4,4 +4,4 @@
 # This script is used by the CI!
 
 apk update
-apk add build-base bash musl-dev curl tar make libtool libffi-dev gcc automake autoconf git openssl-dev g++ pkgconfig llvm18-dev clang18-static llvm18-static
+apk add build-base bash musl-dev curl tar make libtool libffi-dev gcc automake autoconf git openssl-dev g++ pkgconfig llvm18-dev clang18-static llvm18-static zlib
