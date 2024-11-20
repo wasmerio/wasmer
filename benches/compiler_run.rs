c0c0bc14a09bc83a2b121f30425b82a46121c381
Edoardo Marangoni <ecmm@anche.no>
2024-11-20 17:34:37 +0100

    fix(benchmarks): Minor changes

--- benches/compiler_run.rs
+++ benches/compiler_run.rs
@@ -1,8 +1,9 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use wasmer::*;
+use wasmer::{sys::*, *};
 
 static BENCHMARKS_ARTIFACTS_BASE_URL: &str = "https://pub-53a226d993e144159d6f8b993fe0cbf3.r2.dev";
 
+#[allow(unreachable_code)]
 fn get_engine() -> Engine {
     #[cfg(feature = "llvm")]
     return LLVM::new().into();
@@ -30,7 +31,7 @@ pub fn run_fn(c: &mut Criterion, module: &[u8], name: &str, input: i64) {
             .unwrap();
 
         b.iter(|| {
-            func.call(&mut store, input);
+            _ = func.call(&mut store, input);
         })
     });
 }
