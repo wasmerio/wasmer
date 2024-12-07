57a3e27fe73df2d2966eea792ea6024e051aeb3b
maminrayej <maminrayej@users.noreply.github.com>
2024-12-07 16:41:38 +0000

    deploy: 0c8fb44ed30a21c7d26544d77bbba84b480540a5

--- crates/doc/trait.impl/wasmer_wasix/runtime/resolver/source/trait.Source.js
+++ crates/doc/trait.impl/wasmer_wasix/runtime/resolver/source/trait.Source.js
@@ -1,4 +1,4 @@
 (function() {var implementors = {
-"wasmer_cli":[["impl Source for <a class=\"struct\" href=\"wasmer_cli/commands/run/struct.MonitoringSource.html\" title=\"struct wasmer_cli::commands::run::MonitoringSource\">MonitoringSource</a>"]],
+"wasmer_cli":[["impl <a class=\"trait\" href=\"wasmer_wasix/runtime/resolver/source/trait.Source.html\" title=\"trait wasmer_wasix::runtime::resolver::source::Source\">Source</a> for <a class=\"struct\" href=\"wasmer_cli/commands/run/struct.MonitoringSource.html\" title=\"struct wasmer_cli::commands::run::MonitoringSource\">MonitoringSource</a>"]],
 "wasmer_wasix":[]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
