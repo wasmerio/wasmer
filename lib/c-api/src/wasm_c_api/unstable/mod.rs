5c5ffa780d6ba5da6d63b0aac7c698c12eee1505
Edoardo Marangoni <ecmm@anche.no>
2025-03-11 13:04:56 +0100

    fix(c_api): Remove feature-gate for `target_lexicon` mod

--- lib/c-api/src/wasm_c_api/unstable/mod.rs
+++ lib/c-api/src/wasm_c_api/unstable/mod.rs
@@ -5,7 +5,6 @@ pub mod middlewares;
 pub mod module;
 #[cfg(feature = "compiler")]
 pub mod parser;
-#[cfg(any(feature = "compiler", feature = "compiler-headless"))]
 pub mod target_lexicon;
 #[cfg(feature = "wasi")]
 pub mod wasi;
