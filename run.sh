41596bb0bcb7d2efb19729d493a738fe09e843c2
Roberto E. Vargas Caballero <k0ga@shike2.net>
2025-06-08 22:00:58 +0200

    build: Break in rust_begin_unwind()
    It is likely that we don't need core::panicking::panic anymore,
    because I guess rust_begin_unwind is called for every type
    pf panic, but it is not a bad idea to keep it anyway.

--- run.sh
+++ run.sh
@@ -9,6 +9,7 @@ qemu-riscv64-static -g 1024 target/riscv64gc-unknown-linux-gnu/debug/examples/ri
 
 (echo set debuginfod enabled on
  echo target remote localhost:1024
+ echo br rust_begin_unwind
  echo br core::panicking::panic
  cat -u /dev/tty) |
 rust-gdb target/riscv64gc-unknown-linux-gnu/debug/examples/riscv
