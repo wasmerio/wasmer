9e23032212abf5d8ed60ede5c050923ec31f06ef
Roberto E. Vargas Caballero <k0ga@shike2.net>
2025-06-08 22:00:35 +0200

    riscv64: Implement get_param_location()
    As we only care about one function receiving 2 integer parameters
    then we only cover the case of parameters in registers.

--- lib/compiler-singlepass/src/riscv_decl.rs
+++ lib/compiler-singlepass/src/riscv_decl.rs
@@ -12,7 +12,38 @@ use wasmer_types::{CompileError, Type};
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
 #[allow(clippy::upper_case_acronyms)]
 pub enum GPR {
-    // TODO: define integer registers X0-X31.
+    X0 = 0,
+    X1 = 1,
+    X2 = 2,
+    X3 = 3,
+    X4 = 4,
+    X5 = 5,
+    X6 = 6,
+    X7 = 7,
+    X8 = 8,
+    X9 = 9,
+    X10 = 10,
+    X11 = 11,
+    X12 = 12,
+    X13 = 13,
+    X14 = 14,
+    X15 = 15,
+    X16 = 16,
+    X17 = 17,
+    X18 = 18,
+    X19 = 19,
+    X20 = 20,
+    X21 = 21,
+    X22 = 22,
+    X23 = 23,
+    X24 = 24,
+    X25 = 25,
+    X26 = 26,
+    X27 = 27,
+    X28 = 28,
+    X29 = 29,
+    X30 = 30,
+    X31 = 31,
 }
 
 /// Floating-point registers.
