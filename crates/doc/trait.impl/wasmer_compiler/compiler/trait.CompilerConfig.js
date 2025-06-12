d581ef6fb785c5179f97d7d33875f0a54c3db03b
syrusakbary <syrusakbary@users.noreply.github.com>
2025-06-12 15:16:03 +0000

    deploy: a6d17196fb9f613e4d3f4a6ff621c9178ae3d0dd

--- crates/doc/trait.impl/wasmer_compiler/compiler/trait.CompilerConfig.js
+++ crates/doc/trait.impl/wasmer_compiler/compiler/trait.CompilerConfig.js
@@ -2,5 +2,5 @@
 "wasmer":[],
 "wasmer_compiler_cranelift":[["impl CompilerConfig for <a class=\"struct\" href=\"wasmer_compiler_cranelift/config/struct.Cranelift.html\" title=\"struct wasmer_compiler_cranelift::config::Cranelift\">Cranelift</a>"]],
 "wasmer_compiler_llvm":[["impl CompilerConfig for <a class=\"struct\" href=\"wasmer_compiler_llvm/config/struct.LLVM.html\" title=\"struct wasmer_compiler_llvm::config::LLVM\">LLVM</a>"]],
-"wasmer_compiler_singlepass":[["impl <a class=\"trait\" href=\"wasmer_compiler/compiler/trait.CompilerConfig.html\" title=\"trait wasmer_compiler::compiler::CompilerConfig\">CompilerConfig</a> for <a class=\"struct\" href=\"wasmer_compiler_singlepass/config/struct.Singlepass.html\" title=\"struct wasmer_compiler_singlepass::config::Singlepass\">Singlepass</a>"]]
+"wasmer_compiler_singlepass":[["impl CompilerConfig for <a class=\"struct\" href=\"wasmer_compiler_singlepass/config/struct.Singlepass.html\" title=\"struct wasmer_compiler_singlepass::config::Singlepass\">Singlepass</a>"]]
 };if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
