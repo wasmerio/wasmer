a36241ee52e82ad00b252e66297bd55adaa8f278
syrusakbary <syrusakbary@users.noreply.github.com>
2025-07-10 09:04:14 +0000

    deploy: 65b0d5ce6ae052a72ea4bf86223161195c78416d

--- crates/doc/trait.impl/wasmer_compiler/compiler/trait.Compiler.js
+++ crates/doc/trait.impl/wasmer_compiler/compiler/trait.Compiler.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["wasmer_compiler_cranelift",[["impl Compiler for <a class=\"struct\" href=\"wasmer_compiler_cranelift/compiler/struct.CraneliftCompiler.html\" title=\"struct wasmer_compiler_cranelift::compiler::CraneliftCompiler\">CraneliftCompiler</a>"]]],["wasmer_compiler_llvm",[["impl Compiler for <a class=\"struct\" href=\"wasmer_compiler_llvm/compiler/struct.LLVMCompiler.html\" title=\"struct wasmer_compiler_llvm::compiler::LLVMCompiler\">LLVMCompiler</a>"]]],["wasmer_compiler_singlepass",[["impl Compiler for <a class=\"struct\" href=\"wasmer_compiler_singlepass/compiler/struct.SinglepassCompiler.html\" title=\"struct wasmer_compiler_singlepass::compiler::SinglepassCompiler\">SinglepassCompiler</a>"]]]]);
+    var implementors = Object.fromEntries([["wasmer_compiler_cranelift",[["impl Compiler for <a class=\"struct\" href=\"wasmer_compiler_cranelift/compiler/struct.CraneliftCompiler.html\" title=\"struct wasmer_compiler_cranelift::compiler::CraneliftCompiler\">CraneliftCompiler</a>"]]],["wasmer_compiler_llvm",[["impl Compiler for <a class=\"struct\" href=\"wasmer_compiler_llvm/compiler/struct.LLVMCompiler.html\" title=\"struct wasmer_compiler_llvm::compiler::LLVMCompiler\">LLVMCompiler</a>"]]],["wasmer_compiler_singlepass",[["impl <a class=\"trait\" href=\"wasmer_compiler/compiler/trait.Compiler.html\" title=\"trait wasmer_compiler::compiler::Compiler\">Compiler</a> for <a class=\"struct\" href=\"wasmer_compiler_singlepass/compiler/struct.SinglepassCompiler.html\" title=\"struct wasmer_compiler_singlepass::compiler::SinglepassCompiler\">SinglepassCompiler</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[241,212,248]}
+//{"start":57,"fragment_lengths":[241,212,377]}
