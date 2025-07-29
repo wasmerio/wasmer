25932b890f98d4a6ffa7c1baeb56338e08a90dd0
Martin Liska <martin.liska@hey.com>
2025-07-29 18:28:43 +0200

    Add table with documentation of the registers

--- lib/compiler-singlepass/src/riscv_decl.rs
+++ lib/compiler-singlepass/src/riscv_decl.rs
@@ -11,6 +11,46 @@ use std::{collections::BTreeMap, slice::Iter};
 use wasmer_types::target::CallingConvention;
 use wasmer_types::{CompileError, Type};
 
+/*
++-----+-----------+-------------------------------+-------------------+
+| Reg | ABI Name  | Description                   | Saved by Callee   |
++-----+-----------+-------------------------------+-------------------+
+| x0  | zero      | hardwired zero                | -                 |
+| x1  | ra        | return address                | -R                |
+| x2  | sp        | stack pointer                 | -E                |
+| x3  | gp        | global pointer                | -                 |
+| x4  | tp        | thread pointer                | -                 |
+| x5  | t0        | temporary register 0          | -R                |
+| x6  | t1        | temporary register 1          | -R                |
+| x7  | t2        | temporary register 2          | -R                |
+| x8  | s0/fp     | saved register 0/frame pointer| -E                |
+| x9  | s1        | saved register 1              | -E                |
+| x10 | a0        | function arg 0/return value 0 | -R                |
+| x11 | a1        | function arg 1/return value 1 | -R                |
+| x12 | a2        | function argument 2           | -R                |
+| x13 | a3        | function argument 3           | -R                |
+| x14 | a4        | function argument 4           | -R                |
+| x15 | a5        | function argument 5           | -R                |
+| x16 | a6        | function argument 6           | -R                |
+| x17 | a7        | function argument 7           | -R                |
+| x18 | s2        | saved register 2              | -E                |
+| x19 | s3        | saved register 3              | -E                |
+| x20 | s4        | saved register 4              | -E                |
+| x21 | s5        | saved register 5              | -E                |
+| x22 | s6        | saved register 6              | -E                |
+| x23 | s7        | saved register 7              | -E                |
+| x24 | s8        | saved register 8              | -E                |
+| x25 | s9        | saved register 9              | -E                |
+| x26 | s10       | saved register 10             | -E                |
+| x27 | s11       | saved register 11             | -E                |
+| x28 | t3        | temporary register 3          | -R                |
+| x29 | t4        | temporary register 4          | -R                |
+| x30 | t5        | temporary register 5          | -R                |
+| x31 | t6        | temporary register 6          | -R                |
++-----+-----------+-------------------------------+-------------------+
+Legend: -R = caller-saved, -E = callee-saved, - = not saved
+*/
+
 /// General-purpose registers.
 #[repr(u8)]
 #[derive(Copy, Clone, Debug, Eq, PartialEq, Ord, PartialOrd, Hash)]
