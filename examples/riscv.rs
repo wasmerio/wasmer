f2dc4e9e886ebb98a6503f02c15b610a996bb33d
Martin Liska <martin.liska@hey.com>
2025-07-29 17:43:42 +0200

    Extend the test for an extra parameter

--- examples/riscv.rs
+++ examples/riscv.rs
@@ -16,10 +16,12 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wasm_bytes = wat2wasm(
         r#"
     (module
-    (type $sum_t (func (param i32 i32) (result i32)))
-    (func $sum_f (type $sum_t) (param $x i32) (param $y i32) (result i32)
+    (type $sum_t (func (param i32 i32 i32) (result i32)))
+    (func $sum_f (type $sum_t) (param $x i32) (param $y i32) (param $z i32) (result i32)
     local.get $x
     local.get $y
+    i32.add
+    local.get $z
     i32.add)
     (export "sum" (func $sum_f)))
     "#
@@ -41,17 +43,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Option 1
     println!("Calling `sum` function...");
-    let args = [Value::I32(1), Value::I32(2)];
+    let args = [Value::I32(1), Value::I32(10), Value::I32(100)];
     let result = sum.call(&mut store, &args)?;
     println!("Results: {:?}", result);
-    assert_eq!(result.to_vec(), vec![Value::I32(3)]);
+    assert_eq!(result.to_vec(), vec![Value::I32(111)]);
 
     // Option 2
-    let sum_typed: TypedFunction<(i32, i32), i32> = sum.typed(&mut store)?;
+    let sum_typed: TypedFunction<(i32, i32, i32), i32> = sum.typed(&mut store)?;
     println!("Calling `sum` function (natively)...");
-    let result = sum_typed.call(&mut store, 1, 2)?;
+    let result = sum_typed.call(&mut store, 1, 2, 3)?;
     println!("Results: {:?}", result);
-    assert_eq!(result, 3);
+    assert_eq!(result, 6);
 
     Ok(())
 }
