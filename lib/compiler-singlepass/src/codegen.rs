9724e868b389dbe222f77bda4c0c741983bd01a1
Martin Liska <martin.liska@hey.com>
2025-10-20 14:42:07 +0200

    Revert "implement Operator::Unreachable via direct call to wasmer_vm_raise_trap"
    This reverts commit ec96631942eae17726f07b008ed40c1b79be462c.

--- lib/compiler-singlepass/src/codegen.rs
+++ lib/compiler-singlepass/src/codegen.rs
@@ -739,7 +739,7 @@ impl<'a, M: Machine> FuncGen<'a, M> {
         Ok(I2O1 { loc_a, loc_b, ret })
     }
 
-    fn _mark_trappable(&mut self) {
+    fn mark_trappable(&mut self) {
         let state_diff_id = self.get_state_diff();
         let offset = self.machine.assembler_get_offset().0;
         self.fsm.trappable_offsets.insert(
@@ -784,37 +784,6 @@ impl<'a, M: Machine> FuncGen<'a, M> {
         cb: F,
         params: I,
         params_type: J,
-    ) -> Result<(), CompileError> {
-        self.emit_call_native_impl(cb, params, params_type, true)
-    }
-
-    /// Emits a Native ABI call sequence without passing vmctx as the first parameter.
-    ///
-    /// The caller MUST NOT hold any temporary registers allocated by `acquire_temp_gpr` when calling
-    /// this function.
-    fn emit_call_native_without_context<
-        I: Iterator<Item = Location<M::GPR, M::SIMD>>,
-        J: Iterator<Item = WpType>,
-        F: FnOnce(&mut Self) -> Result<(), CompileError>,
-    >(
-        &mut self,
-        cb: F,
-        params: I,
-        params_type: J,
-    ) -> Result<(), CompileError> {
-        self.emit_call_native_impl(cb, params, params_type, false)
-    }
-
-    fn emit_call_native_impl<
-        I: Iterator<Item = Location<M::GPR, M::SIMD>>,
-        J: Iterator<Item = WpType>,
-        F: FnOnce(&mut Self) -> Result<(), CompileError>,
-    >(
-        &mut self,
-        cb: F,
-        params: I,
-        params_type: J,
-        include_ctx_param: bool,
     ) -> Result<(), CompileError> {
         // Values pushed in this function are above the shadow region.
         self.state.stack_values.push(MachineValue::ExplicitShadow);
@@ -874,7 +843,7 @@ impl<'a, M: Machine> FuncGen<'a, M> {
         // Calculate stack offset.
         for (i, _param) in params.iter().enumerate() {
             args.push(self.machine.get_param_location(
-                if include_ctx_param { 1 } else { 0 } + i,
+                1 + i,
                 params_size[i],
                 &mut stack_offset,
                 calling_convention,
@@ -956,15 +925,13 @@ impl<'a, M: Machine> FuncGen<'a, M> {
             }
         }
 
-        if include_ctx_param {
-            // Put vmctx as the first parameter.
-            self.machine.move_location(
-                Size::S64,
-                Location::GPR(self.machine.get_vmctx_reg()),
-                self.machine
-                    .get_simple_param_location(0, calling_convention),
-            )?; // vmctx
-        }
+        // Put vmctx as the first parameter.
+        self.machine.move_location(
+            Size::S64,
+            Location::GPR(self.machine.get_vmctx_reg()),
+            self.machine
+                .get_simple_param_location(0, calling_convention),
+        )?; // vmctx
 
         if stack_padding > 0 {
             self.machine.adjust_stack(stack_padding as u32)?;
@@ -4027,28 +3994,9 @@ impl<'a, M: Machine> FuncGen<'a, M> {
                 )?;
             }
             Operator::Unreachable => {
-                self.machine.move_location(
-                    Size::S64,
-                    Location::Memory(
-                        self.machine.get_vmctx_reg(),
-                        self.vmoffsets
-                            .vmctx_builtin_function(VMBuiltinFunctionIndex::get_raise_trap_index())
-                            as i32,
-                    ),
-                    Location::GPR(self.machine.get_grp_for_call()),
-                )?;
-
-                self.emit_call_native_without_context(
-                    |this| {
-                        this.machine
-                            .emit_call_register(this.machine.get_grp_for_call())
-                    },
-                    // [trap_code]
-                    [Location::Imm32(TrapCode::UnreachableCodeReached as u32)]
-                        .iter()
-                        .cloned(),
-                    [WpType::I32].iter().cloned(),
-                )?;
+                self.mark_trappable();
+                self.machine
+                    .emit_illegal_op(TrapCode::UnreachableCodeReached)?;
                 self.unreachable_depth = 1;
             }
             Operator::Return => {
