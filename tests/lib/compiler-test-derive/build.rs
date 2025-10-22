0bfae0371f8f224fb0b3fc45f4f04f2edaec0c39
Martin Liska <martin.liska@hey.com>
2025-10-22 11:12:05 +0200

    export variables also in compiler-test-derive/build.rs for unit tests

--- tests/lib/compiler-test-derive/build.rs
+++ tests/lib/compiler-test-derive/build.rs
@@ -1,3 +1,17 @@
 fn main() {
-    println!("cargo:rerun-if-changed=../../ignores.txt");
+    println!(
+        "cargo:rustc-env=CFG_TARGET_OS={}",
+        std::env::var("CARGO_CFG_TARGET_OS")
+            .expect("CARGO_CFG_TARGET_OS must be provided by cargo")
+    );
+    println!(
+        "cargo:rustc-env=CFG_TARGET_ARCH={}",
+        std::env::var("CARGO_CFG_TARGET_ARCH")
+            .expect("CARGO_CFG_TARGET_ARCH must be provided by cargo")
+    );
+    println!(
+        "cargo:rustc-env=CFG_TARGET_ENV={}",
+        std::env::var("CARGO_CFG_TARGET_ENV")
+            .expect("CARGO_CFG_TARGET_ENV must be provided by cargo")
+    );
 }
