0217b7d248a80575718781d1f614899affa0ce3a
marxin <marxin@users.noreply.github.com>
2025-10-30 14:16:42 +0000

    deploy: 9533eb20ea5585f48fe962f2fb52910e24ed0cae

--- crates/doc/trait.impl/std/io/trait.Read.js
+++ crates/doc/trait.impl/std/io/trait.Read.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["virtual_fs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"virtual_fs/pipe/struct.Pipe.html\" title=\"struct virtual_fs::pipe::Pipe\">Pipe</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.87.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"virtual_fs/pipe/struct.PipeRx.html\" title=\"struct virtual_fs::pipe::PipeRx\">PipeRx</a>"]]],["wasmer_wasix",[]]]);
+    var implementors = Object.fromEntries([["virtual_fs",[["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"virtual_fs/pipe/struct.Pipe.html\" title=\"struct virtual_fs::pipe::Pipe\">Pipe</a>"],["impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.88.0/std/io/trait.Read.html\" title=\"trait std::io::Read\">Read</a> for <a class=\"struct\" href=\"virtual_fs/pipe/struct.PipeRx.html\" title=\"struct virtual_fs::pipe::PipeRx\">PipeRx</a>"]]],["wasmer_wasix",[]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
