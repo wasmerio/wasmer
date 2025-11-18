1bbee7aa0bf90a437e1e24142df48229d788dc0e
Zebreus <zebreus@zebre.us>
2025-11-18 14:48:15 +0100

    Improve error handling in context_switch

--- lib/wasix/src/syscalls/wasix/context_switch.rs
+++ lib/wasix/src/syscalls/wasix/context_switch.rs
@@ -21,13 +21,28 @@ pub fn context_switch(
     mut ctx: FunctionEnvMut<WasiEnv>,
     target_context_id: u64,
 ) -> impl Future<Output = Result<Errno, RuntimeError>> + Send + 'static + use<> {
+    let sync_part = inner_context_switch(ctx, target_context_id);
+    async move {
+        match sync_part {
+            Ok(fut) => fut.await,
+            Err(res) => res,
+        }
+    }
+}
+
+/// Helper function that allows us to return from the synchronous part early
+fn inner_context_switch(
+    mut ctx: FunctionEnvMut<WasiEnv>,
+    target_context_id: u64,
+) -> Result<
+    impl Future<Output = Result<Errno, RuntimeError>> + Send + 'static + use<>,
+    Result<Errno, RuntimeError>,
+> {
     // TODO: Should we call do_pending_operations here?
     match WasiEnv::do_pending_operations(&mut ctx) {
         Ok(()) => {}
         Err(e) => {
-            // TODO: Move this to the end to only need a single async block
-            panic!("Error in do_pending_operations");
-            // return async move {Err(RuntimeError::user(Box::new(e)))}
+            return Err(Err(RuntimeError::user(Box::new(e))));
         }
     }
 
@@ -38,43 +53,53 @@ pub fn context_switch(
         .swap(target_context_id, Ordering::SeqCst);
 
     if own_context_id == target_context_id {
-        panic!("Switching to self is not allowed");
+        tracing::trace!("Switching context {own_context_id} to itself, which is a no-op");
+        return Err(Ok(Errno::Success));
     }
 
-    let (sender, receiver) = oneshot::channel::<Result<(), RuntimeError>>();
-    let wait_for_unblock = receiver.unwrap_or_else(|_canceled| {
-        // TODO: Handle canceled properly
-        todo!("Context was canceled. Cleanup not implemented yet so we just panic");
-    });
-
-    // let this_one = contexts.get_mut(&current_context_id).unwrap();
-    // let receiver_promise = this_one.suspend();
+    let (sender, wait_for_unblock) = oneshot::channel::<Result<(), RuntimeError>>();
 
     let maybe_old_value = data.contexts.insert(own_context_id, sender);
     if maybe_old_value.is_some() {
+        // This should never happen, and if it does, it is an error in WASIX
         panic!(
-            "Context ID {} was already suspended when switching context to {}",
-            own_context_id, target_context_id
+            "Context {own_context_id} is already suspended but tried to switch to context {target_context_id}. How did we get here?",
         );
     }
 
     // Unblock the other context
-    let unblock_target = data
-        .contexts
-        .remove(&target_context_id)
-        .map(|(_id, val)| val)
-        .expect("Context to switch to does not exist");
-    unblock_target
-        .send(Ok(()))
-        .expect("Failed to unblock target context, this should not happen");
+    let Some(unblock_target) = data.contexts.remove(&target_context_id).map(|(_, val)| val) else {
+        tracing::trace!(
+            "Context {own_context_id} tried to switch to context {target_context_id} but it does not exist or is not suspended"
+        );
+        return Err(Ok(Errno::Inval));
+    };
+    let Ok(_) = unblock_target.send(Ok(())) else {
+        // This should never happen, and if it does, it is an error in WASIX
+        // TODO: Handle cancellation properly
+        panic!(
+            "Context {own_context_id} failed to unblock target context {target_context_id}. This should never happen"
+        );
+    };
 
     let current_context_id = data.current_context_id.clone();
-    async move {
+    Ok(async move {
         let result = wait_for_unblock.await;
-
         current_context_id.store(own_context_id, Ordering::SeqCst);
+
+        let result = match result {
+            Ok(v) => v,
+            Err(canceled) => {
+                tracing::trace!(
+                    "Context {own_context_id} was canceled while it was suspended: {}",
+                    canceled
+                );
+                // TODO: Handle cancellation properly
+                panic!("Sender was dropped: {canceled}");
+            }
+        };
 
         // If we get relayed a trap, propagate it. Other wise return success
         result.and(Ok(Errno::Success))
-    }
+    })
 }
