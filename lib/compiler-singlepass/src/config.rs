2ca47c584abf09b6df4ba880bda8ff2f74865512
Martin Liška <martin.liska@hey.com>
2025-11-18 01:06:47 -0800

    Singlepass: add multivalue support (#5869)

--- lib/compiler-singlepass/src/config.rs
+++ lib/compiler-singlepass/src/config.rs
@@ -45,9 +45,7 @@ impl CompilerConfig for Singlepass {
 
     /// Gets the supported features for this compiler in the given target
     fn supported_features_for_target(&self, _target: &Target) -> Features {
-        let mut features = Features::default();
-        features.multi_value(false);
-        features
+        Features::default()
     }
 
     /// Pushes a middleware onto the back of the middleware chain.
