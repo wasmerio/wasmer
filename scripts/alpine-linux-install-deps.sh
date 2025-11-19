4e7c375d76476b4999dc279cbf1afec52f6cbe0c
Martin Liska <martin.liska@hey.com>
2025-11-19 18:43:50 +0100

    use clang 21

--- scripts/alpine-linux-install-deps.sh
+++ scripts/alpine-linux-install-deps.sh
@@ -5,4 +5,4 @@
 
 apk update
 apk add bash mold make curl cmake ninja clang21 zstd-static llvm21-dev clang21-static llvm21-static ncurses-static zlib-static
-ln -s /usr/bin/clang-18 /usr/bin/clang
+ln -s /usr/bin/clang-21 /usr/bin/clang
