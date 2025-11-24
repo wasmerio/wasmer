84eeff23a9159cbeb638fcf28b71a35b8caa43a8
Zebreus <zebreus@zebre.us>
2025-11-24 14:44:10 +0100

    Add WASIX test for context switching

---
+++ lib/wasix/tests/context-switching/main.c
@@ -0,0 +1,54 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include <errno.h>
+#include <wasix/context.h>
+
+wasix_context_id_t context1;
+wasix_context_id_t context2;
+
+char* message = "Uninitialized\n";
+int stop = 0;
+int counter = 0;
+
+void test1(void) {
+    while (1) {
+        wasix_context_switch(context2);
+        if (stop == 1) {
+            wasix_context_switch(context_main_context);
+        }
+        counter++;
+        printf("%s", message);
+    }
+}
+
+void test2(void) {
+    printf("Starting test2\n");
+
+    message = "Switch 1\n";
+    wasix_context_switch(context1);
+
+    message = "Switch 2\n";
+    wasix_context_switch(context1);
+
+    message = "Switch 3\n";
+    wasix_context_switch(context1);
+
+    message = "Switch 4\n";
+    wasix_context_switch(context1);
+
+    stop = 1;
+    wasix_context_switch(context1);
+
+    exit(50);
+}
+
+int main() {
+    wasix_context_new(&context1, test1);
+    wasix_context_new(&context2, test2);
+    wasix_context_switch(context1);
+    
+    assert(counter == 4);
+
+    return 0;
+}
