968996a53c3fbec41ca6ea2d1417830a8840350b
Zebreus <zebreus@zebre.us>
2025-11-25 15:45:37 +0100

    Update context-switching tests to the new names

--- lib/wasix/tests/context_switching/switching_in_threads.c
+++ lib/wasix/tests/context_switching/switching_in_threads.c
@@ -1,70 +1,70 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <assert.h>
 #include <errno.h>
 #include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <wasix/context.h>
 
 #ifndef NULL
-#define NULL ((void*)0)
+#define NULL ((void *)0)
 #endif
 
 wasix_context_id_t context1;
 wasix_context_id_t context2;
 
-char* message = "Uninitialized\n";
+char *message = "Uninitialized\n";
 int stop = 0;
 int counter = 0;
 
 void test1(void) {
-    while (1) {
-        wasix_context_switch(context2);
-        if (stop == 1) {
-            wasix_context_switch(context_main_context);
-        }
-        counter++;
-        printf("%s", message);
+  while (1) {
+    wasix_context_switch(context2);
+    if (stop == 1) {
+      wasix_context_switch(context_main_context);
     }
+    counter++;
+    printf("%s", message);
+  }
 }
 
 void test2(void) {
-    printf("Starting test2\n");
+  printf("Starting test2\n");
 
-    message = "Switch 1\n";
-    wasix_context_switch(context1);
+  message = "Switch 1\n";
+  wasix_context_switch(context1);
 
-    message = "Switch 2\n";
-    wasix_context_switch(context1);
+  message = "Switch 2\n";
+  wasix_context_switch(context1);
 
-    message = "Switch 3\n";
-    wasix_context_switch(context1);
+  message = "Switch 3\n";
+  wasix_context_switch(context1);
 
-    message = "Switch 4\n";
-    wasix_context_switch(context1);
+  message = "Switch 4\n";
+  wasix_context_switch(context1);
 
-    stop = 1;
-    wasix_context_switch(context1);
+  stop = 1;
+  wasix_context_switch(context1);
 
-    exit(50);
+  exit(50);
 }
 
-void* abort_in_thread(void* arg) {
-    wasix_context_new(&context1, test1);
-    wasix_context_new(&context2, test2);
-    wasix_context_switch(context1);
+void *abort_in_thread(void *arg) {
+  wasix_context_create(&context1, test1);
+  wasix_context_create(&context2, test2);
+  wasix_context_switch(context1);
 
-    return NULL;
+  return NULL;
 }
 
 int main() {
-    pthread_t thread;
-    pthread_create(&thread, NULL, abort_in_thread, NULL);
-    pthread_join(thread, NULL);
+  pthread_t thread;
+  pthread_create(&thread, NULL, abort_in_thread, NULL);
+  pthread_join(thread, NULL);
 
-    if(counter != 4) {
-        printf("Error: expected counter to be 4 but it is %d\n", counter);
-        exit(1);
-    }
+  if (counter != 4) {
+    printf("Error: expected counter to be 4 but it is %d\n", counter);
+    exit(1);
+  }
 
-    return 0;
+  return 0;
 }
