0bf91fb039e1db062a8306e01569b2bf88012190
Zebreus <zebreus@zebre.us>
2025-11-25 15:45:37 +0100

    Update context-switching tests to the new names

--- lib/wasix/tests/context_switching/switching_to_a_deleted_context.c
+++ lib/wasix/tests/context_switching/switching_to_a_deleted_context.c
@@ -1,7 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <assert.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <wasix/context.h>
 
 wasix_context_id_t context1;
@@ -10,37 +10,38 @@ wasix_context_id_t context2;
 int counter = 0;
 
 void test1(void) {
-    counter += 1;
-    wasix_context_switch(context_main_context);
+  counter += 1;
+  wasix_context_switch(context_main_context);
 }
 
 // Required because switching with the real main currently segfaults
 void test_main(void) {
-    wasix_context_new(&context1, test1);
-    wasix_context_new(&context2, test1);
+  wasix_context_create(&context1, test1);
+  wasix_context_create(&context2, test1);
 
-    // Assert that test1 increments the context.
-    assert(counter == 0);
-    wasix_context_switch(context1);
-    assert(counter == 1);
+  // Assert that test1 increments the context.
+  assert(counter == 0);
+  wasix_context_switch(context1);
+  assert(counter == 1);
 
-    // Assert that calling the deleteed context again fails
-    wasix_context_delete(context1);
-    wasix_context_switch(context1);
-    assert(counter == 1);
+  // Assert that calling the destroyed context again fails
+  wasix_context_destroy(context1);
+  wasix_context_switch(context1);
+  assert(counter == 1);
 
-    // Assert that switching to a context that was deleteed before the first switch works
-    wasix_context_delete(context2);
-    wasix_context_switch(context2);
-    assert(counter == 1);
+  // Assert that switching to a context that was destroyed before the first
+  // switch works
+  wasix_context_destroy(context2);
+  wasix_context_switch(context2);
+  assert(counter == 1);
 
-    exit(0);
+  exit(0);
 }
 
 int main() {
-    wasix_context_id_t test_main_context;
-    wasix_context_new(&test_main_context, test_main);
-    wasix_context_switch(test_main_context);
+  wasix_context_id_t test_main_context;
+  wasix_context_create(&test_main_context, test_main);
+  wasix_context_switch(test_main_context);
 
-    return 0;
+  return 0;
 }
--- lib/wasix/tests/context_switching/switching_with_main.c
+++ lib/wasix/tests/context_switching/switching_with_main.c
@@ -1,7 +1,7 @@
-#include <stdlib.h>
-#include <stdio.h>
 #include <assert.h>
 #include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <wasix/context.h>
 
 wasix_context_id_t context1;
@@ -11,28 +11,27 @@ int stop = 0;
 int counter = 0;
 
 void test1(void) {
-    while (1) {
-        wasix_context_switch(context_main_context);
-        if (stop == 1) {
-            wasix_context_switch(context_main_context);
-            break;
-        }
-        counter++;
+  while (1) {
+    wasix_context_switch(context_main_context);
+    if (stop == 1) {
+      wasix_context_switch(context_main_context);
+      break;
     }
+    counter++;
+  }
 }
 
 int main() {
-    wasix_context_new(&context1, test1);
-    wasix_context_switch(context1);
+  wasix_context_create(&context1, test1);
+  wasix_context_switch(context1);
 
+  wasix_context_switch(context1);
+  wasix_context_switch(context1);
+  wasix_context_switch(context1);
+  wasix_context_switch(context1);
+  stop = 1;
+  wasix_context_switch(context1);
 
-    wasix_context_switch(context1);
-    wasix_context_switch(context1);
-    wasix_context_switch(context1);
-    wasix_context_switch(context1);
-    stop = 1;
-    wasix_context_switch(context1);
-
-    assert(counter == 4);
-    return 0;
+  assert(counter == 4);
+  return 0;
 }
