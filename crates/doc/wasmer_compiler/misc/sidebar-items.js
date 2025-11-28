690c012a022e47be2e216d46469b2efd146cd450
marxin <marxin@users.noreply.github.com>
2025-11-28 12:08:18 +0000

    deploy: 47ef166f6856a5dd8cefd5d8706da269bb756832

--- crates/doc/wasmer_compiler/misc/sidebar-items.js
+++ crates/doc/wasmer_compiler/misc/sidebar-items.js
@@ -1,1 +1,1 @@
-window.SIDEBAR_ITEMS = {"enum":["CompiledKind"],"fn":["function_kind_to_filename","sanitize_filename","types_to_signature"]};
+window.SIDEBAR_ITEMS = {"enum":["CompiledKind"],"fn":["function_kind_to_filename","sanitize_filename","save_assembly_to_file","types_to_signature"]};
