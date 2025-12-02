4eda17378adac58044c1fc876a729d7e7fb42557
Martin Liska <martin.liska@hey.com>
2025-12-02 11:07:10 +0100

    add missing package

--- scripts/alpine-linux-install-deps.sh
+++ scripts/alpine-linux-install-deps.sh
@@ -4,4 +4,4 @@
 # This script is used by the CI!
 
 apk update
-apk add bash mold make curl cmake ninja clang21 zstd-static llvm21-dev clang21-static llvm21-static ncurses-static zlib-static tar libxml2
+apk add bash mold make curl cmake ninja clang21 zstd-static llvm21-dev clang21-static llvm21-static ncurses-static zlib-static tar libxml2-static
