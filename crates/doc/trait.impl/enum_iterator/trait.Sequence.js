5ce321e8cb227eb903a5ae3be1de15561308f721
marxin <marxin@users.noreply.github.com>
2025-12-03 10:54:16 +0000

    deploy: 12e4295b5c627aa42cb9721c9ee48c7afc4611fb

---
+++ crates/doc/trait.impl/enum_iterator/trait.Sequence.js
@@ -0,0 +1,9 @@
+(function() {
+    var implementors = Object.fromEntries([["wasmer_types",[["impl Sequence for <a class=\"enum\" href=\"wasmer_types/enum.LibCall.html\" title=\"enum wasmer_types::LibCall\">LibCall</a>"]]]]);
+    if (window.register_implementors) {
+        window.register_implementors(implementors);
+    } else {
+        window.pending_implementors = implementors;
+    }
+})()
+//{"start":57,"fragment_lengths":[147]}
--- crates/doc/trait.impl/syn/parse/trait.Parse.js
+++ crates/doc/trait.impl/syn/parse/trait.Parse.js
@@ -1,5 +1,5 @@
 (function() {
-    var implementors = Object.fromEntries([["macro_wasmer_universal_test",[["impl <a class=\"trait\" href=\"https://docs.rs/syn/2.0.108/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"struct\" href=\"macro_wasmer_universal_test/struct.MaybeItemFn.html\" title=\"struct macro_wasmer_universal_test::MaybeItemFn\">MaybeItemFn</a>"]]]]);
+    var implementors = Object.fromEntries([["macro_wasmer_universal_test",[["impl <a class=\"trait\" href=\"https://docs.rs/syn/2.0.111/syn/parse/trait.Parse.html\" title=\"trait syn::parse::Parse\">Parse</a> for <a class=\"struct\" href=\"macro_wasmer_universal_test/struct.MaybeItemFn.html\" title=\"struct macro_wasmer_universal_test::MaybeItemFn\">MaybeItemFn</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
