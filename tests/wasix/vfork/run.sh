721b9fadfab7caed130fd721188ae1c020aefd3f
Zebreus <zebreus@zebre.us>
2025-12-22 15:40:04 +0100

    Adjust the test setup

--- tests/wasix/vfork/run.sh
+++ tests/wasix/vfork/run.sh
@@ -1,13 +1,11 @@
 #!/usr/bin/env bash
 set -e
 
-# Compile the main program with setjmp/longjmp based vfork
-WASIXCC_WASM_EXCEPTIONS=yes WASIXCC_PIC=yes wasixcc main.c -o main-eh.wasm -Wl,-pie
-
 echo ""
 echo "Note: runtime errors reported from wasmer for this test are expected"
 
 # Test the asyncify based vfork implementation
+WASIXCC_WASM_EXCEPTIONS=no WASIXCC_PIC=no wasixcc main.c -o main.wasm
 
 $WASMER_RUN main.wasm --dir . -- successful_exec
 $WASMER_RUN main.wasm --dir . -- successful_execlp
@@ -16,11 +14,13 @@ $WASMER_RUN main.wasm --dir . -- cloexec
 $WASMER_RUN main.wasm --dir . -- nested_vfork
 $WASMER_RUN main.wasm --dir . -- exiting_child
 $WASMER_RUN main.wasm --dir . -- trapping_child
-$WASMER_RUN main.wasm --dir . -- exit_before_exec || true
 # This test is triggering undefined behaviour
+$WASMER_RUN main.wasm --dir . -- exit_before_exec || true
+# This test is triggering undefined behaviour as well
 $WASMER_RUN main.wasm --dir . -- trap_before_exec || true
 
 # Test the setjmp/longjmp based vfork implementation
+WASIXCC_WASM_EXCEPTIONS=yes WASIXCC_PIC=yes wasixcc main.c -o main-eh.wasm -Wl,-pie
 
 $WASMER_RUN main-eh.wasm --dir . -- successful_exec
 $WASMER_RUN main-eh.wasm --dir . -- successful_execlp
@@ -29,7 +29,7 @@ $WASMER_RUN main-eh.wasm --dir . -- cloexec
 $WASMER_RUN main-eh.wasm --dir . -- nested_vfork
 $WASMER_RUN main-eh.wasm --dir . -- exiting_child
 $WASMER_RUN main-eh.wasm --dir . -- trapping_child
-# This test is triggering undefined behaviour as well
-$WASMER_RUN main-eh.wasm --dir . -- exit_before_exec || true
 # This test is triggering undefined behaviour
+$WASMER_RUN main-eh.wasm --dir . -- exit_before_exec || true
+# This test is triggering undefined behaviour as well
 $WASMER_RUN main-eh.wasm --dir . -- trap_before_exec || true
