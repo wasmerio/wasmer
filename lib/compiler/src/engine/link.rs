a7980d969f2302aa76e668e4fb1dde106bbca5ee
Martin Liska <martin.liska@hey.com>
2025-12-22 17:52:57 +0100

    use i32

--- lib/compiler/src/engine/link.rs
+++ lib/compiler/src/engine/link.rs
@@ -404,18 +404,18 @@ fn apply_relocation(
         },
         RelocationKind::Add4 => unsafe {
             let (reloc_address, reloc_abs) = r.for_address(body, target_func_address as u64);
-            let value = read_unaligned(reloc_address as *mut u32);
+            let value = read_unaligned(reloc_address as *mut i32);
             write_unaligned(
-                reloc_address as *mut u32,
-                u32::try_from(dbg!(reloc_abs)).unwrap().wrapping_add(value),
+                reloc_address as *mut i32,
+                i32::try_from(reloc_abs).unwrap().wrapping_add(value),
             );
         },
         RelocationKind::Sub4 => unsafe {
             let (reloc_address, reloc_abs) = r.for_address(body, target_func_address as u64);
-            let value = read_unaligned(reloc_address as *mut u32);
+            let value = read_unaligned(reloc_address as *mut i32);
             write_unaligned(
-                reloc_address as *mut u32,
-                u32::try_from(dbg!(reloc_abs)).unwrap().wrapping_sub(value),
+                reloc_address as *mut i32,
+                i32::try_from(reloc_abs).unwrap().wrapping_sub(value),
             );
         },
         kind => panic!("Relocation kind unsupported in the current architecture: {kind}"),
