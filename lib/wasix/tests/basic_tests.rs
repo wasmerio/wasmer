b74bf630ad5db0154f9cb2d5d3f0290b1eb17787
Zebreus <zebreus@zebre.us>
2026-01-14 17:53:12 +0100

    Start migrating tests from wasix-tests

---
+++ lib/wasix/tests/basic_tests.rs
@@ -0,0 +1,14 @@
+//! Basic tests from wasix-tests directory
+//! 
+//! These tests verify fundamental functionality:
+//! - helloworld: Basic printf and return 0
+
+mod wasixcc_test_utils;
+use wasixcc_test_utils::{run_build_script, run_wasm};
+
+#[test]
+fn test_helloworld() {
+    let wasm_path = run_build_script(file!(), "helloworld").unwrap();
+    let test_dir = wasm_path.parent().unwrap();
+    run_wasm(&wasm_path, test_dir).unwrap();
+}
