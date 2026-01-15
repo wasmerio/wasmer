7ffb191f78dfcaae3e729a5cbd3bee0e408eaa35
Zebreus <zebreus@zebre.us>
2026-01-15 14:00:27 +0100

    Add ffi tests

---
+++ lib/wasix/tests/ffi_tests.rs
@@ -0,0 +1,26 @@
+mod wasixcc_test_utils;
+use wasixcc_test_utils::{run_build_script, run_wasm};
+
+#[test]
+fn simple_ffi_call() {
+    let wasm = run_build_script(file!(), "simple-ffi-call").unwrap();
+    run_wasm(&wasm, wasm.parent().unwrap()).unwrap();
+}
+
+#[test]
+fn complex_ffi_call() {
+    let wasm = run_build_script(file!(), "complex-ffi-call").unwrap();
+    run_wasm(&wasm, wasm.parent().unwrap()).unwrap();
+}
+
+#[test]
+fn longdouble_ffi_call() {
+    let wasm = run_build_script(file!(), "longdouble-ffi-call").unwrap();
+    run_wasm(&wasm, wasm.parent().unwrap()).unwrap();
+}
+
+#[test]
+fn simple_ffi_closure() {
+    let wasm = run_build_script(file!(), "simple-ffi-closure").unwrap();
+    run_wasm(&wasm, wasm.parent().unwrap()).unwrap();
+}
