71a5ac88bc23bd91872702fccd1539210f6dbccb
marxin <marxin@users.noreply.github.com>
2026-01-20 09:08:45 +0000

    deploy: 012eab3d6249e2a6e404350f7f54ca2417c8b860

--- crates/doc/trait.impl/wasmer_compiler/compiler/trait.CompilerConfig.js
+++ crates/doc/trait.impl/wasmer_compiler/compiler/trait.CompilerConfig.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["wasmer",[]],["wasmer_compiler_cranelift",[["impl CompilerConfig for <a class=\"struct\" href=\"wasmer_compiler_cranelift/struct.Cranelift.html\" title=\"struct wasmer_compiler_cranelift::Cranelift\">Cranelift</a>"]]],["wasmer_compiler_llvm",[["impl CompilerConfig for <a class=\"struct\" href=\"wasmer_compiler_llvm/struct.LLVM.html\" title=\"struct wasmer_compiler_llvm::LLVM\">LLVM</a>"]]],["wasmer_compiler_singlepass",[["impl <a class=\"trait\" href=\"wasmer_compiler/compiler/trait.CompilerConfig.html\" title=\"trait wasmer_compiler::compiler::CompilerConfig\">CompilerConfig</a> for <a class=\"struct\" href=\"wasmer_compiler_singlepass/struct.Singlepass.html\" title=\"struct wasmer_compiler_singlepass::Singlepass\">Singlepass</a>"]]]]);
+    var implementors = Object.fromEntries([["wasmer",[]],["wasmer_compiler_cranelift",[["impl CompilerConfig for <a class=\"struct\" href=\"wasmer_compiler_cranelift/struct.Cranelift.html\" title=\"struct wasmer_compiler_cranelift::Cranelift\">Cranelift</a>"]]],["wasmer_compiler_llvm",[["impl CompilerConfig for <a class=\"struct\" href=\"wasmer_compiler_llvm/struct.LLVM.html\" title=\"struct wasmer_compiler_llvm::LLVM\">LLVM</a>"]]],["wasmer_compiler_singlepass",[["impl CompilerConfig for <a class=\"struct\" href=\"wasmer_compiler_singlepass/struct.Singlepass.html\" title=\"struct wasmer_compiler_singlepass::Singlepass\">Singlepass</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[13,205,175,352]}
+//{"start":57,"fragment_lengths":[13,205,175,211]}
