2111f4126c52d84ab1e9c0e26e6f133049ef83c3
Martin Liska <martin.liska@hey.com>
2026-01-23 09:57:53 +0100

    Release 7.0.0-rc.1

--- scripts/update-version.py
+++ scripts/update-version.py
@@ -3,8 +3,8 @@
 import os
 import re
 
-PREVIOUS_VERSION = "7.0.0-alpha.1"
-NEXT_VERSION = "7.0.0-alpha.2"
+PREVIOUS_VERSION = "7.0.0-alpha.2"
+NEXT_VERSION = "7.0.0-rc.1"
 
 
 def make_prerelease_version(version: str) -> str:
