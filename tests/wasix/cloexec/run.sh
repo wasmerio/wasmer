25685f13093afdde08b86346b2b6ccc27cff8fa3
Zebreus <zebreus@zebre.us>
2026-02-12 11:45:33 +0100

    Add a test for CLOEXEC on pipes

--- tests/wasix/cloexec/run.sh
+++ tests/wasix/cloexec/run.sh
@@ -1,7 +1,13 @@
-set -e
+set -ex
 
-# step 1: tests involving the flag itself
-$WASMER_RUN main.wasm -- flag_tests
+wasixcc main.c -o main.wasm
 
-# step 2: tests involving exec
-$WASMER_RUN main.wasm --volume . -- exec_tests
+# Test 1: fcntl FD_CLOEXEC flag manipulation
+timeout -s 9 -f -v 5 -- $WASMER_RUN main.wasm -- flag_tests
+
+# Test 2: O_CLOEXEC with open() + fork/exec
+timeout -s 9 -f -v 5 -- $WASMER_RUN main.wasm --volume . -- exec_tests
+
+# Test 3: pipe2(O_CLOEXEC) should set FD_CLOEXEC
+# BUG: pipe2() does not set FD_CLOEXEC when O_CLOEXEC flag is passed
+timeout -s 9 -f -v 5 -- $WASMER_RUN main.wasm -- pipe2_cloexec_test
