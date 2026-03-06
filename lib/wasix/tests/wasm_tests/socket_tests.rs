b57a2ad5dca3556607dbaabe016eeb4ba67fc21d
Artem Yarulin <artem.yarulin@kapteko.com>
2026-03-06 09:37:50 +0200

    wasix(tests): Migrate c-wasi-tests to the new runner, migrate some of wasix tests to that as well

--- lib/wasix/tests/wasm_tests/socket_tests.rs
+++ lib/wasix/tests/wasm_tests/socket_tests.rs
@@ -1,4 +1,4 @@
-use super::{run_build_script, run_wasm_with_result};
+use super::{run_build_script, run_wasm, run_wasm_with_result};
 use wasmer::Engine;
 use wasmer::sys::{Features, LLVM, NativeEngineExt, Target};
 
@@ -15,4 +15,10 @@ fn test_pipe_send_recv_compat() {
         stdout,
         String::from_utf8_lossy(&result.stderr)
     );
+}
+
+#[test]
+fn test_socket_pair() {
+    let wasm = run_build_script(file!(), "socket-pair").unwrap();
+    run_wasm(&wasm, wasm.parent().unwrap()).unwrap();
 }
