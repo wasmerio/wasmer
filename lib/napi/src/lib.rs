77d5f21b3b535c6af67b7b920690b2170022ff30
Christoph Herzog <chris@theduke.at>
2026-03-17 00:04:46 +0100

    fix(napi): Fix host/guest buffer handling

--- lib/napi/src/lib.rs
+++ lib/napi/src/lib.rs
@@ -6,7 +6,7 @@ mod snapi;
 mod wasix;
 
 pub use ctx::{NapiCtx, NapiCtxBuilder, NapiLimits, NapiRuntimeHooks, NapiSession};
-pub(crate) use env::RuntimeEnv;
+pub(crate) use env::{GuestBackingStoreMapping, HostBufferCopy, RuntimeEnv};
 
 pub fn module_needs_napi(module: &wasmer::Module) -> bool {
     NapiCtx::module_needs_napi(module)
