276d299302ffd6f5eb000aa48670e64510e5db5f
Martin Liska <martin.liska@hey.com>
2026-03-20 14:10:47 +0100

    simplify NAPI interface constants

--- lib/napi/src/lib.rs
+++ lib/napi/src/lib.rs
@@ -5,8 +5,6 @@ mod snapi;
 #[cfg(feature = "wasix")]
 mod wasix;
 
-use std::sync::LazyLock;
-
 pub use ctx::{NapiCtx, NapiCtxBuilder, NapiLimits, NapiRuntimeHooks, NapiSession};
 pub(crate) use env::{GuestBackingStoreMapping, HostBufferCopy, RuntimeEnv};
 
@@ -14,10 +12,5 @@ pub fn module_needs_napi(module: &wasmer::Module) -> bool {
     NapiCtx::module_needs_napi(module)
 }
 
-const NAPI_CURRENT_VERSION: u64 = 10;
-const NAPI_EXTENSION_WASMER_CURRENT_VERSION: u64 = 0;
-
-pub static NAPI_INTERFACE_NAME: LazyLock<String> =
-    LazyLock::new(|| format!("napi_v{NAPI_CURRENT_VERSION}"));
-pub static NAPI_EXTENSION_WASMER_INTERFACE_NAME: LazyLock<String> =
-    LazyLock::new(|| format!("napi_extension_wasmer_v{NAPI_EXTENSION_WASMER_CURRENT_VERSION}"));
+pub static NAPI_INTERFACE_NAME: &str = "napi_v10";
+pub static NAPI_EXTENSION_WASMER_INTERFACE_NAME: &str = "napi_extension_wasmer_v0";
