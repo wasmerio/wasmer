9f94fdb7aa55e9693ebaffc3a80e7e9c0aa74a04
Martin Liska <martin.liska@hey.com>
2026-04-01 13:22:19 +0200

    drop FuncEnvironment trait

--- lib/compiler-cranelift/src/translator/mod.rs
+++ lib/compiler-cranelift/src/translator/mod.rs
@@ -7,7 +7,7 @@ mod func_translator;
 mod translation_utils;
 mod unwind;
 
-pub use self::func_environ::{FuncEnvironment, GlobalVariable, TargetEnvironment};
+pub use self::func_environ::{GlobalVariable, ReturnMode};
 pub use self::func_translator::FuncTranslator;
 pub use self::translation_utils::{
     irlibcall_to_libcall, irreloc_to_relocationkind, signature_to_cranelift_ir,
