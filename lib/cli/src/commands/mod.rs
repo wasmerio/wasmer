285f16703c0cc6a0b23f5f57f38a628c364a21dc
Martin Liška <martin.liska@hey.com>
2026-04-21 14:44:45 +0200

    feat: drop x86_64-darwin target (#6506)

--- lib/cli/src/commands/mod.rs
+++ lib/cli/src/commands/mod.rs
@@ -385,7 +385,6 @@ enum Cmd {
     //
     // - "x86_64-linux-gnu"
     // - "aarch64-linux-gnu"
-    // - "x86_64-apple-darwin"
     // - "arm64-apple-darwin"
     // #[cfg(any(feature = "static-artifact-create", feature = "wasmer-artifact-create"))]
     // #[clap(name = "create-exe", verbatim_doc_comment)]
@@ -417,7 +416,6 @@ enum Cmd {
     ///
     /// - "x86_64-linux-gnu"
     /// - "aarch64-linux-gnu"
-    /// - "x86_64-apple-darwin"
     /// - "arm64-apple-darwin"
     // #[cfg(feature = "static-artifact-create")]
     // #[structopt(name = "create-obj", verbatim_doc_comment)]
