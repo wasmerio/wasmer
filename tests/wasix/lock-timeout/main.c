8128077995a6af4443a4190ca0842c509a694771
Fliqqr <filip.duris.2@gmail.com>
2026-05-05 14:01:43 +0200

    fix: clang format

--- tests/wasix/lock-timeout/main.c
+++ tests/wasix/lock-timeout/main.c
@@ -1,34 +1,33 @@
-#include <stdio.h>
-#include <pthread.h>
-#include <time.h>
 #include <errno.h>
+#include <pthread.h>
+#include <stdio.h>
+#include <time.h>
 
 pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-void *f(void *arg) {
-    (void)arg;
+void* f(void* arg) {
+  (void)arg;
 
-    struct timespec ts;
-    clock_gettime(CLOCK_REALTIME, &ts);
+  struct timespec ts;
+  clock_gettime(CLOCK_REALTIME, &ts);
 
-    ts.tv_nsec += 500 * 1000 * 1000; // 0.5 seconds
-    if (ts.tv_nsec >= 1000000000) {
-        ts.tv_sec += 1;
-        ts.tv_nsec -= 1000000000;
-    }
+  ts.tv_nsec += 500 * 1000 * 1000;  // 0.5 seconds
+  if (ts.tv_nsec >= 1000000000) {
+    ts.tv_sec += 1;
+    ts.tv_nsec -= 1000000000;
+  }
 
-    int result = pthread_mutex_timedlock(&lock, &ts);
+  int result = pthread_mutex_timedlock(&lock, &ts);
 
-    return NULL;
+  return NULL;
 }
 
 int main(int argc, char** argv) {
-    pthread_mutex_lock(&lock);
+  pthread_mutex_lock(&lock);
 
-    pthread_t thread;
-    pthread_create(&thread, NULL, f, NULL);
+  pthread_t thread;
+  pthread_create(&thread, NULL, f, NULL);
+  pthread_join(thread, NULL);
 
-    pthread_join(thread, NULL);
-
-    return 0;
+  return 0;
 }
