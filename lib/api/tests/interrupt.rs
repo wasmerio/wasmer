9b5b506f88b1af0f2540094b357dab983f9dcf09
Arshia <arshia001@live.com>
2026-05-15 19:59:36 +0400

    apparently, spellcheckers can't decide between interruptable and interruptible

--- lib/api/tests/interrupt.rs
+++ lib/api/tests/interrupt.rs
@@ -43,16 +43,16 @@ const INFINITE_ATOMIC_WAIT_WAT: &str = r#"
 
 #[test]
 fn test_interrupt_hot_loop() -> Result<()> {
-    test_interruptable(INFINITE_LOOP_WAT)
+    test_interruptible(INFINITE_LOOP_WAT)
 }
 
 #[test]
 fn test_interrupt_memory_wait() -> Result<()> {
-    test_interruptable(INFINITE_ATOMIC_WAIT_WAT)
+    test_interruptible(INFINITE_ATOMIC_WAIT_WAT)
 }
 
 // TODO: update/fix this as we implement more of the feature
-fn test_interruptable(wat: &'static str) -> Result<()> {
+fn test_interruptible(wat: &'static str) -> Result<()> {
     let barrier = Arc::new(Barrier::new(2));
     let interrupter_slot = Arc::new(Mutex::new(None));
 
