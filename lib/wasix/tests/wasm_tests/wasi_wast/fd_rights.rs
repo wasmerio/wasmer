320303ff90743718dddf8a6f93803bb36c279f8f
Sonu Kapoor <sonukapoor@gmail.com>
2026-05-29 18:11:38 -0400

    test(wasix): add missing fd_rights wasi-wast case

---
+++ lib/wasix/tests/wasm_tests/wasi_wast/fd_rights.rs
@@ -0,0 +1,28 @@
+//#DefaultMappedDirectories: false
+//#CurrentDirectory: /
+//#MappedDirectory: $temp:/tmp
+
+use std::io::{Read, Write};
+use std::path::PathBuf;
+
+fn main() {
+    let file = {
+        let mut base = PathBuf::from("tmp");
+
+        base.push("foo.txt");
+        base
+    };
+
+    let mut filehandle = std::fs::OpenOptions::new()
+        .read(false)
+        .write(true)
+        .create(true)
+        .open(&file)
+        .unwrap();
+    filehandle.write_all(b"test").unwrap();
+
+    let mut contents = String::new();
+    assert!(filehandle.read_to_string(&mut contents).is_err());
+
+    std::fs::remove_file(&file).unwrap();
+}
--- lib/wasix/tests/wasm_tests/wasi_wast/test_fs/README.md
+++ lib/wasix/tests/wasm_tests/wasi_wast/test_fs/README.md
@@ -4,6 +4,7 @@ This is a test "file system" used in some of the WASI integration tests.
 
 It's just a bunch of files in a tree.
 
-If you make changes here, please regenerate the tests with `make wasitests`!
+If you make changes here, update the expected output fixtures in the parent
+`wasi_wast` directory.
 
 The contents of `temp` are deleted before each run.  If you want to test making or mutating files, do it in that sub directory.
