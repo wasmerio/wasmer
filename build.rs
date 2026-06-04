3eec114702f314deb62eb6f043157d5d69f3dec7
Sonu Kapoor <sonukapoor@users.noreply.github.com>
2026-06-04 10:21:32 -0400

    test(wasix): port wasi-wast tests to wasm harness (#6648)
    This ports the old tests/wasi-wast coverage into the current WASIX wasm test harness under lib/wasix/tests/wasm_tests/wasi_wast.
    
    Resolves: #6646

--- build.rs
+++ build.rs
@@ -9,7 +9,7 @@ use std::fs;
 use std::path::PathBuf;
 use std::process::Command;
 use test_generator::test_directory_module;
-use test_generator::{Testsuite, test_directory, wasi_processor, wast_processor, with_test_module};
+use test_generator::{Testsuite, test_directory, wast_processor, with_test_module};
 
 fn main() -> anyhow::Result<()> {
     println!(
@@ -30,12 +30,6 @@ fn main() -> anyhow::Result<()> {
 
     // As rerun-if-changed doesn't support globs, we use another crate
     // to check changes in directories.
-    build_deps::rerun_if_changed_paths("tests/wasi-wast/wasi/unstable/*")
-        .expect("Can't get directory");
-    build_deps::rerun_if_changed_paths("tests/wasi-wast/wasi/snapshot1/*")
-        .expect("Can't get directory");
-    build_deps::rerun_if_changed_paths("tests/wasi-wast/wasi/nightly-2022-10-18/*")
-        .expect("Can't get directory");
     build_deps::rerun_if_changed_paths("tests/wast/spec/proposals/*").expect("Can't get directory");
 
     let out_dir = PathBuf::from(
@@ -76,46 +70,6 @@ fn main() -> anyhow::Result<()> {
         // Note: We need drop because we don't want to run `unwrap` or `expect` as
         // the command might fail, but we don't care about it's result.
         drop(Command::new("rustfmt").arg(&spectests_output).status());
-    }
-
-    // Wasitest test generation
-    {
-        let mut wasitests = Testsuite {
-            buffer: String::new(),
-            path: vec![],
-        };
-
-        with_test_module(&mut wasitests, "wasitests", |wasitests| {
-            for wasi_version in &["unstable", "snapshot1", "nightly_2022_10_18"] {
-                with_test_module(wasitests, wasi_version, |wasitests| {
-                    for (wasi_filesystem_test_name, wasi_filesystem_kind) in &[
-                        ("host_fs", "WasiFileSystemKind::Host"),
-                        ("mem_fs", "WasiFileSystemKind::InMemory"),
-                        ("tmp_fs", "WasiFileSystemKind::Tmp"),
-                        ("passthru_fs", "WasiFileSystemKind::PassthruMemory"),
-                        ("union_fs", "WasiFileSystemKind::UnionHostMemory"),
-                        ("root_fs", "WasiFileSystemKind::RootFileSystemBuilder"),
-                    ] {
-                        with_test_module(wasitests, wasi_filesystem_test_name, |wasitests| {
-                            test_directory(
-                                wasitests,
-                                format!("tests/wasi-wast/wasi/{wasi_version}"),
-                                |out, path| wasi_processor(out, path, wasi_filesystem_kind),
-                            )
-                        })?;
-                    }
-
-                    Ok(())
-                })?;
-            }
-
-            Ok(())
-        })?;
-
-        let wasitests_output = out_dir.join("generated_wasitests.rs");
-        fs::write(&wasitests_output, wasitests.buffer)?;
-
-        drop(Command::new("rustfmt").arg(&wasitests_output).status());
     }
 
     Ok(())
--- tests/lib/test-generator/src/processors.rs
+++ tests/lib/test-generator/src/processors.rs
@@ -26,36 +26,3 @@ pub fn wast_processor(_out: &mut Testsuite, p: PathBuf) -> Option<Test> {
         body,
     })
 }
-
-/// Given a Testsuite and a path, process the path in case is a WASI
-/// wasm file.
-pub fn wasi_processor(
-    _out: &mut Testsuite,
-    p: PathBuf,
-    wasi_filesystem_kind: &str,
-) -> Option<Test> {
-    let ext = p.extension()?;
-    // Only look at wast files.
-    if ext != "wast" {
-        return None;
-    }
-
-    let wasm_dir = {
-        let mut inner = p.clone();
-        inner.pop();
-        inner
-    };
-    let testname = extract_name(&p);
-
-    let body = format!(
-        "crate::run_wasi(config, r#\"{}\"#, \"{}\", crate::{})",
-        p.display(),
-        wasm_dir.display(),
-        wasi_filesystem_kind,
-    );
-
-    Some(Test {
-        name: testname,
-        body,
-    })
-}
