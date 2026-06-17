809f0e483a729dc7048a3bdcf9aa065fc188c1eb
Sadhbh Code <sonia.kolasinska.pro@gmail.com>
2026-06-17 14:04:22 +0100

    Put checks around calls

--- lib/wasix/tests/wasm_tests/socket/udp-readiness/main.c
+++ lib/wasix/tests/wasm_tests/socket/udp-readiness/main.c
@@ -10,20 +10,55 @@
 
 int main(void) {
   int fd = socket(AF_INET, SOCK_DGRAM | SOCK_NONBLOCK, 0);
+  if (fd < 0) {
+    perror("socket");
+    return 1;
+  }
 
   struct sockaddr_in addr = {0};
   addr.sin_family = AF_INET;
   addr.sin_addr.s_addr = htonl(INADDR_LOOPBACK);
   addr.sin_port = 0;
-  bind(fd, (struct sockaddr*)&addr, sizeof(addr));
+  if (bind(fd, (struct sockaddr*)&addr, sizeof(addr)) != 0) {
+    perror("bind");
+    close(fd);
+    return 1;
+  }
 
   socklen_t len = sizeof(addr);
-  getsockname(fd, (struct sockaddr*)&addr, &len);
+  if (getsockname(fd, (struct sockaddr*)&addr, &len) != 0) {
+    perror("getsockname");
+    close(fd);
+    return 1;
+  }
 
-  sendto(fd, "", 0, 0, (struct sockaddr*)&addr, sizeof(addr));
+  ssize_t sent =
+      sendto(fd, "", 0, 0, (struct sockaddr*)&addr, sizeof(addr));
+  if (sent != 0) {
+    fprintf(stderr, "expected zero-length sendto, got %zd errno=%d (%s)\n",
+            sent, errno, strerror(errno));
+    close(fd);
+    return 1;
+  }
 
   struct pollfd pfd = {.fd = fd, .events = POLLIN};
-  poll(&pfd, 1, 1000);
+  int ready = poll(&pfd, 1, 1000);
+  if (ready < 0) {
+    perror("poll");
+    close(fd);
+    return 1;
+  }
+  if (ready == 0) {
+    fprintf(stderr,
+            "poll did not report an already queued zero-length UDP datagram\n");
+    close(fd);
+    return 1;
+  }
+  if ((pfd.revents & POLLIN) == 0) {
+    fprintf(stderr, "poll did not report POLLIN, revents=0x%x\n", pfd.revents);
+    close(fd);
+    return 1;
+  }
 
   char byte;
   ssize_t n = recvfrom(fd, &byte, sizeof(byte), 0, NULL, NULL);
