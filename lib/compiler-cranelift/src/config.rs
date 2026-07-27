ea45b28f9c5284e8b234f1a7785f31a2a093e6bd
Martin Liska <martin.liska@hey.com>
2026-07-27 15:33:23 +0200

    fix(Cranelift): restore enable_multi_ret_implicit_sret on Windows

--- lib/compiler-cranelift/src/config.rs
+++ lib/compiler-cranelift/src/config.rs
@@ -244,6 +244,13 @@ impl Cranelift {
             .enable("use_colocated_libcalls")
             .expect("should be a valid flag");
 
+        if matches!(target.triple().operating_system, OperatingSystem::Windows) {
+            // For macOS and Linux we rely on the precise `ReturnAbi` calling conventions.
+            flags
+                .enable("enable_multi_ret_implicit_sret")
+                .expect("should be a valid flag");
+        }
+
         // Invert cranelift's default-on verification to instead default off.
         flags
             .set("enable_verifier", &self.enable_verifier.to_string())
