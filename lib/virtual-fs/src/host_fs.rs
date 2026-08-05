ec94ed0d9bc35f9bcc157ee2541952561eef10e6
kilyanni <github@nkilian.org>
2026-08-05 18:49:57 +0200

    feat(wasix): report stdin/out/err as Unknown if it isnt a tty

--- lib/virtual-fs/src/host_fs.rs
+++ lib/virtual-fs/src/host_fs.rs
@@ -616,6 +616,10 @@ impl VirtualFile for Stdout {
         Some(1)
     }
 
+    fn is_terminal(&self) -> bool {
+        std::io::IsTerminal::is_terminal(&std::io::stdout())
+    }
+
     fn poll_read_ready(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<usize>> {
         Poll::Ready(Ok(0))
     }
@@ -790,6 +794,10 @@ impl VirtualFile for Stderr {
         Some(2)
     }
 
+    fn is_terminal(&self) -> bool {
+        std::io::IsTerminal::is_terminal(&std::io::stderr())
+    }
+
     fn poll_read_ready(self: Pin<&mut Self>, _cx: &mut Context<'_>) -> Poll<io::Result<usize>> {
         Poll::Ready(Ok(0))
     }
@@ -905,6 +913,11 @@ impl VirtualFile for Stdin {
     fn get_special_fd(&self) -> Option<u32> {
         Some(0)
     }
+
+    fn is_terminal(&self) -> bool {
+        std::io::IsTerminal::is_terminal(&std::io::stdin())
+    }
+
     fn poll_read_ready(mut self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<io::Result<usize>> {
         {
             let read_buffer = self.read_buffer.lock().unwrap();
